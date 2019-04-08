45d49a57d5345c43455173c72d30a763ce4bf8b2
yubozhao <yubz86@gmail.com>
2019-04-08 13:27:49 -0700

    Update image handler

--- bentoml/handlers/image_handler.py
+++ bentoml/handlers/image_handler.py
@@ -22,6 +22,7 @@ import json
 import os
 import sys
 import cv2
+import numpy as np
 from werkzeug.utils import secure_filename
 from flask import request, Response, make_response
 from bentoml.handlers.base_handlers import RequestHandler, CliHandler
@@ -51,6 +52,17 @@ class ImageHandler(RequestHandler, CliHandler):
 
     @staticmethod
     def handle_request(request, func, options={}):
+        """
+        Handle http request that has image file/s.  It will convert image into a ndarray for the function to consume.
+
+        Args:
+            request: incoming request object.
+            func: function that will take ndarray as its arg.
+            options: configuration for handling request object.
+        Return:
+            response object
+        """
+
         if request.method == 'POST':
             try:
                 options = merge_dicts(default_options, options)
@@ -58,9 +70,10 @@ class ImageHandler(RequestHandler, CliHandler):
                     input_file = request.files[options.input_name]
                     file_name = secure_filename(input_file.filename)
 
-                    check_file_format(file_name, options.accept_file_extensions) 
+                    check_file_format(file_name, options['accept_file_extensions'])
 
-                    input_data = input_file.read()
+                    input_data_string = input_file.read()
+                    input_data = np.fromstring(input_data_string, np.uint8)
                 else:
                     raise NotImplementedError
 
@@ -76,21 +89,26 @@ class ImageHandler(RequestHandler, CliHandler):
             return make_response(500)
 
     @staticmethod
-    def handle_cli(args, func, options=None):
+    def handle_cli(args, func, options={}):
         options = merge_dicts(default_options, options)
         parser = generate_cli_default_parser()
         parsed_args = parser.parse_args(args)
+        file_path = parsed_args.input
 
         try:
-            check_file_format(parsed_args.input, options.accept_format_list)
-            image = cv2.imread(parsed_args.input)
-            output = func(content)
-            with open(parsed_args.input, 'rb') as content_file:
+            check_file_format(file_path, options['accept_file_extensions'])
+            if not os.path.isabs(file_path):
+                file_path = os.path.abspath(file_path)
 
-                if parsed_args.output == 'json' or not parsed_args.output:
-                    result = json.dumps(output)
-                    sys.stdout.write(result)
-                else:
-                    raise NotImplementedError
+            image = cv2.imread(file_path)
+            output = func(image)
+
+            if parsed_args.output == 'json' or not parsed_args.output:
+                result = json.dumps(output)
+                sys.stdout.write(result)
+            else:
+                raise NotImplementedError
+
         except Exception as e:
+            # TODO: handle exceptions
             raise NotImplementedError

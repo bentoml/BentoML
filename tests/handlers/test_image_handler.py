84074620632b9d306dcae98dd0c42c0530f22de2
yubozhao <yubz86@gmail.com>
2019-04-09 12:28:40 -0700

    update tests to work with new implmentation

--- tests/handlers/test_image_handler.py
+++ tests/handlers/test_image_handler.py
@@ -5,8 +5,8 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 import pytest
 
-from bentoml import BentoModel, load, api
-from bentoml.artifacts import PickleArtifact
+from bentoml import BentoService, load, api, artifacts
+from bentoml.artifact import PickleArtifact
 from bentoml.handlers import ImageHandler
 
 
@@ -15,10 +15,10 @@ class FakeImageModel(object):
         return image_ndarray.shape
 
 
-class ImageHandlerModel(BentoModel):
-    def config(self, artifacts, env):
-        artifacts.add(PickleArtifact('clf'))
-
+@artifacts([
+    PickleArtifact('clf')
+])
+class ImageHandlerModel(BentoService):
     @api(ImageHandler)
     def predict(self, input_data):
         return self.artifacts.clf.predict(input_data)
@@ -29,7 +29,7 @@ BASE_TEST_PATH = "/tmp/bentoml-test"
 
 def test_image_handler(capsys):
     fake_model = FakeImageModel()
-    ms = ImageHandlerModel(clf=fake_model)
+    ms = ImageHandlerModel.pack(clf=fake_model)
     api = ms.get_service_apis()[0]
 
     fake_args = ['--input=./tests/test_image_1.jpg']

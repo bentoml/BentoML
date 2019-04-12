61920334b31d1b80e14a88e29ce72bd162181d0f
Bozhao <yubz86@gmail.com>
2019-04-12 14:22:08 -0700

    update linter script, and fix linter for test files (#47)

--- tests/handlers/test_image_handler.py
+++ tests/handlers/test_image_handler.py
@@ -1,10 +1,15 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
-from bentoml import BentoService, api, artifacts
-from bentoml.artifact import PickleArtifact
-from bentoml.handlers import ImageHandler
+try:
+    from bentoml import BentoService, api, artifacts
+    from bentoml.artifact import PickleArtifact
+    from bentoml.handlers import ImageHandler
+except ImportError:
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+    from bentoml import BentoService, api, artifacts
+    from bentoml.artifact import PickleArtifact
+    from bentoml.handlers import ImageHandler
 
 
 class FakeImageModel(object):

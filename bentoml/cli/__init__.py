46189a2b9bc954803c518946c9d9d16b4005a0b7
Chaoyu Yang <paranoyang@gmail.com>
2019-04-11 22:53:32 -0700

    make condiational arg command cli work, improve help msg

--- bentoml/cli/__init__.py
+++ bentoml/cli/__init__.py
@@ -17,3 +17,85 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+import json
+
+import click
+
+from bentoml.archive import load
+from bentoml.server import BentoAPIServer
+from bentoml.cli.click_utils import DefaultCommandGroup, conditional_argument
+
+
+def create_bentoml_cli(installed_archive_path=None):
+
+    @click.group(cls=DefaultCommandGroup)
+    @click.version_option()
+    def bentoml_cli():
+        """
+        BentoML CLI tool
+        """
+
+    # Example Usage: bentoml API_NAME /SAVED_ARCHIVE_PATH --input=INPUT
+    @bentoml_cli.command(
+        default_command=True,
+        default_command_usage="API_NAME BENTO_ARCHIVE_PATH --input=INPUT",
+        default_command_display_name="<API_NAME>",
+        help="Run a API defined in saved BentoArchive with cli args as input",
+        context_settings=dict(
+            ignore_unknown_options=True,
+            allow_extra_args=True,
+        )
+    )
+    @click.argument('api-name', type=click.STRING)
+    @conditional_argument(installed_archive_path is None, 'archive-path', type=click.STRING)
+    @click.pass_context
+    def run(ctx, api_name, archive_path=installed_archive_path):
+        """
+        Run an API definied in the BentoService loaded from archive
+        """
+        model_service = load(archive_path)
+
+        try:
+            api = next((api for api in model_service.get_service_apis() if api.name == api_name))
+        except StopIteration:
+            raise ValueError("Can't find API '{}' in Service '{}'".format(api_name, model_service.name))
+
+        api.handler.handle_cli(ctx.args, api.func, api.options)
+
+
+    # Example Usage: bentoml info /SAVED_ARCHIVE_PATH
+    @bentoml_cli.command()
+    @conditional_argument(installed_archive_path is None, 'archive-path', type=click.STRING)
+    def info(archive_path=installed_archive_path):
+        """
+        List all APIs definied in the BentoService loaded from archive
+        """
+        model_service = load(archive_path)
+        service_apis = model_service.get_service_apis()
+        output = json.dumps(
+            dict(name=model_service.name, version=model_service.version,
+                 apis=[api.name for api in service_apis]), indent=2)
+        print(output)
+
+
+    # Example Usage: bentoml serve ./SAVED_ARCHIVE_PATH --port=PORT
+    @bentoml_cli.command()
+    @conditional_argument(installed_archive_path is None, 'archive-path', type=click.STRING)
+    @click.option('--port', type=click.INT, default=BentoAPIServer._DEFAULT_PORT)
+    def serve(port, archive_path=installed_archive_path):
+        """
+        Start REST API server hosting BentoService loaded from archive
+        """
+        model_service = load(archive_path)
+        server = BentoAPIServer(model_service, port=port)
+        server.start()
+
+    return bentoml_cli
+
+def cli():
+    _cli = create_bentoml_cli()
+    _cli()
+
+if __name__ == '__main__':
+    cli()
--- bentoml/cli/click_utils.py
+++ bentoml/cli/click_utils.py
@@ -28,13 +28,20 @@ class DefaultCommandGroup(click.Group):
 
     def command(self, *args, **kwargs):
         default_command = kwargs.pop('default_command', False)
+        default_command_usage = kwargs.pop('default_command_usage', '')
+        default_command_display_name = kwargs.pop('default_command_display_name', '<>')
+
         if default_command and not args:
-            kwargs['name'] = kwargs.get('name', '<>')
+            kwargs['name'] = kwargs.get('name', default_command_display_name)
         decorator = super(DefaultCommandGroup, self).command(*args, **kwargs)
 
         if default_command:
+            def default_command_format_usage(ctx, formatter):
+                formatter.write_usage(ctx.parent.command_path, default_command_usage)
+
             def new_decorator(f):
                 cmd = decorator(f)
+                cmd.format_usage = default_command_format_usage
                 self.default_command = cmd.name  # pylint:disable=attribute-defined-outside-init
                 return cmd
             return new_decorator

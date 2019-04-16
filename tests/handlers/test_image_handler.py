69f048b6a030750ab6e1128e57d612e47b078d2a
Chaoyu Yang <paranoyang@gmail.com>
2019-04-16 08:09:15 -0700

    improve image handler test

--- tests/handlers/test_image_handler.py
+++ tests/handlers/test_image_handler.py
@@ -1,16 +1,10 @@
 import os
 import sys
 
-try:
-    from bentoml import BentoService, api, artifacts
-    from bentoml.artifact import PickleArtifact
-    from bentoml.handlers import ImageHandler
-except ImportError:
-    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-    from bentoml import BentoService, api, artifacts
-    from bentoml.artifact import PickleArtifact
-    from bentoml.handlers import ImageHandler
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from bentoml import BentoService, api, artifacts
+from bentoml.artifact import PickleArtifact
+from bentoml.handlers import ImageHandler
 
 class TestImageModel(object):
 
@@ -26,16 +20,18 @@ class ImageHandlerModel(BentoService):
         return self.artifacts.clf.predict(input_data)
 
 
-BASE_TEST_PATH = "/tmp/bentoml-test"
-
-
-def test_image_handler(capsys):
+def test_image_handler(capsys, tmp_path):
     test_model = TestImageModel()
     ms = ImageHandlerModel.pack(clf=test_model)
     api = ms.get_service_apis()[0]
 
-    test_args = ['--input=./tests/test_image_1.jpg']
+    import cv2
+    import numpy as np
+    img_file = os.path.join(tmp_path, 'img.png')
+    cv2.imwrite(img_file, np.zeros((10, 10)))
+    
+    test_args = ['--input={}'.format(img_file)]
     api.handle_cli(test_args)
     out, err = capsys.readouterr()
 
-    assert out.strip().endswith('(360, 480, 3)')
+    assert out.strip().endswith('(10, 10, 3)')

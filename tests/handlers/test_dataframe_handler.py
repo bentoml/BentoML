183913c18c0a514c88f19711f6df9194583910b2
yubozhao <yubz86@gmail.com>
2019-04-23 13:21:18 -0700

    unit test dataframe handler instead

--- tests/handlers/test_dataframe_handler.py
+++ tests/handlers/test_dataframe_handler.py
@@ -2,24 +2,10 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from bentoml import BentoService, api, artifacts  # noqa: E402
-from bentoml.artifact import PickleArtifact  # noqa: E402
 from bentoml.handlers import DataframeHandler  # noqa: E402
 
-class TestDataframeModel(object):
 
-    def predict(self, df):
-        return df['name'][0]
-
-@artifacts([PickleArtifact('test_clf')])
-class DataframeHandlerModel(BentoService):
-
-    @api(DataframeHandler)
-    def predict(self, df):
-        return self.artifacts.test_clf.predict(df)
-
-
-def test_dataframe_handler(capsys, tmpdir):
+def test_dataframe_handle_cli(capsys, tmpdir):
     test_content = """
     [
       {
@@ -29,9 +15,7 @@ def test_dataframe_handler(capsys, tmpdir):
       }
     ]
     """
-    test_model = TestDataframeModel()
-    ms = DataframeHandlerModel.pack(test_clf=test_model)
-    api = ms.get_service_apis()[0]
+    handler = DataframeHandler()
 
     import json
     json_file = tmpdir.join('test.json')
@@ -39,6 +23,29 @@ def test_dataframe_handler(capsys, tmpdir):
         f.write(test_content)
 
     test_args = ['--input={}'.format(json_file)]
-    api.handle_cli(test_args)
+    handler.handle_cli(test_args)
     out, err = capsys.readouterr()
     assert out.strip().endswith('john')
+
+
+def test_dataframe_handle_aws_lambda_event():
+    test_content = """
+    {
+      "name": "john",
+      "game": "mario",
+      "city": "sf"
+    }
+    """
+    def test_func(json_obj):
+        return json_obj['name']
+
+    handler = DataframeHandler()
+    success_event_obj = {'headers': {'Content-Type': 'application/json'}, 'body': test_content}
+    success_response = handler.handle_aws_lambda_event(success_event_objevent_obj, test_func)
+
+    assert success_response.statusCode == 200
+    assert success_response.body == 'john'
+
+    error_event_obj = {'headers': {'Content-Type': 'this_will_fail'}, 'body': test_content}
+    error_response = handler.handle_aws_lambda_event(error_event_obj, test_func)
+    assert error_response.statusCode == 400

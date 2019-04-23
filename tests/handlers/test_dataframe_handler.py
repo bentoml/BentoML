4606cc58dcebdcd05114c204ceb7246fb7e130c4
yubozhao <yubz86@gmail.com>
2019-04-23 14:09:28 -0700

    update dataframehandler tests

--- tests/handlers/test_dataframe_handler.py
+++ tests/handlers/test_dataframe_handler.py
@@ -17,7 +17,7 @@ def test_dataframe_handle_cli(capsys, tmpdir):
     """
 
     def test_func(df):
-        return df[0]['name']
+        return df['name'][0]
 
     handler = DataframeHandler()
 
@@ -34,22 +34,24 @@ def test_dataframe_handle_cli(capsys, tmpdir):
 
 def test_dataframe_handle_aws_lambda_event():
     test_content = """
-    {
-      "name": "john",
-      "game": "mario",
-      "city": "sf"
-    }
+    [
+      {
+        "name": "john",
+        "game": "mario",
+        "city": "sf"
+      }
+    ]
     """
 
-    def test_func(json_obj):
-        return json_obj['name']
+    def test_func(df):
+        return df['name'][0]
 
     handler = DataframeHandler()
     success_event_obj = {'headers': {'Content-Type': 'application/json'}, 'body': test_content}
     success_response = handler.handle_aws_lambda_event(success_event_obj, test_func)
 
     assert success_response['statusCode'] == 200
-    assert success_response['body'] == 'john'
+    assert success_response['body'] == '"john"'
 
     error_event_obj = {'headers': {'Content-Type': 'this_will_fail'}, 'body': test_content}
     error_response = handler.handle_aws_lambda_event(error_event_obj, test_func)

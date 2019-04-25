4dfbdbda9b9bb6aad00aa819d9929e6c1ac430c7
Chaoyu <paranoyang@gmail.com>
2019-04-24 17:00:45 -0700

    Update script/lint.sh script (#118)
    * continue the execution even if one linter check fails
    
    * nicer cli output

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-set -e
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT
 
+echo "Running pylint on bentoml module.."
 pylint --rcfile="./pylintrc" bentoml
+
+echo "Running pylint on test module.."
 pylint --rcfile="./pylintrc" tests
+
+echo "Running pycodestyle on bentoml module.."
 pycodestyle bentoml
+
+echo "Running pycodestyle on test module.."
 pycodestyle tests
+
+echo "Done"

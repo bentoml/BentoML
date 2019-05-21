b83e208e37887cb49213655566b8cda5ddf17c21
Chaoyu <paranoyang@gmail.com>
2019-05-21 16:06:52 -0700

    bump version 0.2.0 (#140)

--- bentoml/version.py
+++ bentoml/version.py
@@ -14,4 +14,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.1.2'
+__version__ = '0.2.0'

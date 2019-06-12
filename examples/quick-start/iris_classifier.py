97a5e31249a54492da7dc69a53b64f56f853ae0c
Chaoyu <paranoyang@gmail.com>
2019-06-12 02:58:06 -0700

    Update quick start guide & readme page (#165)
    * add event label and use anonymous client id
    
    * moving quick start guide to notebook, make it run-all-able on google colab
    
    * add quick start guide notebook
    
    * Update readme.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

---
+++ examples/quick-start/iris_classifier.py
@@ -0,0 +1,15 @@
+from bentoml import BentoService, api, env, artifacts
+from bentoml.artifact import PickleArtifact
+from bentoml.handlers import DataframeHandler
+
+# You can also import your own python module here and BentoML will automatically
+# figure out the dependency chain and package all those python modules
+
+@artifacts([PickleArtifact('model')])
+@env(conda_pip_dependencies=["scikit-learn"])
+class IrisClassifier(BentoService):
+
+    @api(DataframeHandler)
+    def predict(self, df):
+        # arbitrary preprocessing or feature fetching code can be placed here 
+        return self.artifacts.model.predict(df)

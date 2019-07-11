7f9c450b21a31c2398290d0fb4514c89f835a41c
Chaoyu Yang <paranoyang@gmail.com>
2019-07-11 16:22:01 -0700

    update

--- examples/tf-keras-text-classification/text_classification_service.py
+++ examples/tf-keras-text-classification/text_classification_service.py
@@ -18,18 +18,28 @@ class TextClassificationService(BentoService):
         else:
             return self.artifacts.word_index["<UNK>"]
     
+    def preprocessing(self, text):
+        sequence = keras.preprocessing.text.text_to_word_sequence(text)
+        return list(map(self.word_to_index, sequence))
+    
     @api(JsonHandler)
     def predict(self, parsed_json):
-        """
-        """
-        text = parsed_json['text']
+        if type(parsed_json) == list:
+            input_data = list(map(self.preprocessing, parsed_json))
         
-        sequence = keras.preprocessing.text.hashing_trick(
-            text,
-            256,
-            hash_function=self.word_to_index,
-            filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
-            lower=True,
-            split=' ')
+        else: # expecting type(parsed_json) == dict:
+            input_data = [self.preprocessing(parsed_json['text'])]
 
-        return self.artifacts.model.predict(np.expand_dims(sequence, 0))
+#             input_data = self.preprocessing(parsed_json['text'])
+#             input_data = np.expand_dims(input_data, 0)
+
+        input_data = keras.preprocessing.sequence.pad_sequences(input_data,
+                                                                value=self.artifacts.word_index["<PAD>"],
+                                                                padding='post',
+                                                                maxlen=80)
+#         input_data = keras.preprocessing.sequence.pad_sequences(input_data,
+#             value=self.artifacts.word_index["<PAD>"],
+#             padding='post',
+#             maxlen=256)
+
+        return self.artifacts.model.predict_classes(input_data)

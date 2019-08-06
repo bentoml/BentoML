cb54f704e01b2d3969f97e964d332b57174242cc
Chaoyu Yang <paranoyang@gmail.com>
2019-08-06 15:26:36 -0700

    updates

--- bentoml/_version.py
+++ bentoml/_version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "'v'"
     cfg.parentdir_prefix = ""
     cfg.versionfile_source = "bentoml/_version.py"
     cfg.verbose = False

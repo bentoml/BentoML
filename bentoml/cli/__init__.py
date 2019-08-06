60ee5f1e57c38ee921e51af2103d227e9c79fc24
yubozhao <yubz86@gmail.com>
2019-08-05 22:23:58 -0700

    Setup deployment as subcommands

--- bentoml/cli/__init__.py
+++ bentoml/cli/__init__.py
@@ -30,7 +30,10 @@ from bentoml.archive import load, load_service_api, load_bentoml_config
 from bentoml.server import BentoAPIServer, get_docs
 from bentoml.server.gunicorn_server import GunicornBentoServer
 from bentoml.cli.click_utils import BentoMLCommandGroup, conditional_argument, _echo
-from bentoml.cli.deployment import add_deployment_commands
+from bentoml.cli.deployment import (
+    add_legacy_deployment_commands,
+    add_deployment_sub_commands,
+)
 from bentoml.cli.config import get_configuration_sub_command
 from bentoml.utils import Path
 from bentoml.utils.log import configure_logging
@@ -290,7 +293,8 @@ def create_bentoml_cli():
     # Commands created here aren't mean to be used from generated service archive. They
     # are used as part of BentoML cli commands only.
 
-    add_deployment_commands(_cli)
+    add_legacy_deployment_commands(_cli)
+    add_deployment_sub_commands(_cli)
     config_sub_command = get_configuration_sub_command()
     _cli.add_command(config_sub_command)
     return _cli
--- bentoml/cli/deployment.py
+++ bentoml/cli/deployment.py
@@ -17,10 +17,23 @@ from __future__ import division
 from __future__ import print_function
 
 import click
+import grpc
 
+from bentoml.config import config
 from bentoml.deployment.serverless import ServerlessDeployment
 from bentoml.deployment.sagemaker import SagemakerDeployment
 from bentoml.cli.click_utils import _echo, CLI_COLOR_ERROR, CLI_COLOR_SUCCESS
+from bentoml.proto.deployment_pb2 import (
+    ApplyDeploymentRequest,
+    DeleteDeploymentRequest,
+    GetDeploymentRequest,
+    DescribeDeploymentRequest,
+    ListDeploymentsRequest,
+    Deployment,
+    DeploymentSpec,
+)
+from bentoml.proto.status_pb2 import Status
+from bentoml.proto.yatai_service_pb2_grpc import YataiStub
 from bentoml.utils.usage_stats import track_cli
 
 SERVERLESS_PLATFORMS = ["aws-lambda", "aws-lambda-py2", "gcp-function"]
@@ -28,7 +41,7 @@ SERVERLESS_PLATFORMS = ["aws-lambda", "aws-lambda-py2", "gcp-function"]
 # pylint: disable=unused-variable
 
 
-def add_deployment_commands(cli):
+def add_legacy_deployment_commands(cli):
 
     # Example usage: bentoml deploy /ARCHIVE_PATH --platform=aws-lambda
     @cli.command(
@@ -212,4 +225,105 @@ def add_deployment_commands(cli):
 
         deployment.check_status()
 
+    return cli
+
+def create_yatai_stub():
+    channel = grpc.insecure_channel(config.get('yatai', 'url'))
+    return YataiStub(channel)
+
+
+def add_deployment_sub_commands(cli):
+    @click.group()
+    def deployment():
+        pass
+
+    @deployment.command(
+        help="Apply deployment configuration to target deployment platform",
+        short_help="Apply deployment configuration",
+    )
+    @click.argument("archive-path", type=click.STRING)
+    @click.option("--namespace", type=click.STRING, help="Deployment's namespace")
+    @click.option("--name", type=click.STRING, help="Deployment's name")
+    @click.option("--labels", type=click.STRING)
+    @click.option("--annotations", type=click.STRING)
+    @click.option(
+        "--platform",
+        type=click.Choice(
+            ["aws-lambda", "gcp-function", "aws-sagemaker", "kubernetes", "custom"]
+        ),
+        required=True,
+        help="Target platform that Bento archive is going to deployed to",
+    )
+    @click.option(
+        "--region",
+        type=click.STRING,
+        help="Target region inside the cloud provider that will be deployed to",
+    )
+    @click.pass_context
+    def apply(
+        ctx,
+        archive_path,
+        platform,
+        region=None,
+        name=None,
+        namespace=None,
+        labels=None,
+        annotations=None,
+        api_name=None,
+    ):
+        stub = create_yatai_stub()
+        deployment = ''
+        result = stub.DeleteDeployment(deployment)
+        if result.status.status_code != Status.OK:
+            _echo('wrong', CLI_COLOR_ERROR)
+        else:
+            _echo('good')
+
+    @deployment.command()
+    @click.option("--name", type=click.STRING, help="Deployment's name", required=True)
+    def delete(name):
+        stub = create_yatai_stub()
+        result = stub.DeleteDeployment(deployment_name=name)
+        if result.status.status_code != Status.OK:
+            _echo('wrong', CLI_COLOR_ERROR)
+        else:
+            _echo('good')
+
+    @deployment.command()
+    @click.option("--name", type=click.STRING, help="Deployment's name", required=True)
+    def get(name):
+        stub = create_yatai_stub()
+        result = stub.GetDeployment(deployment_name=name)
+        if result.status.status_code != Status.OK:
+            _echo('wrong', CLI_COLOR_ERROR)
+        else:
+            _echo('good')
+
+    @deployment.command()
+    @click.option("--name", type=click.STRING, help="Deployment's name", required=True)
+    @click.option("--namespace", type=click.STRING, help="Deployment's namespace")
+    def describe(name):
+        stub = create_yatai_stub()
+        result = stub.DescribeDeployment(deployment_name=name)
+        if result.status.status_code != Status.OK:
+            _echo("wrong", CLI_COLOR_ERROR)
+        else:
+            _echo('something good')
+
+
+    @deployment.command()
+    @click.option("--limit", type=click.INT, help="")
+    @click.option("--offset", type=click.INT, help="")
+    @click.option("--filter", type=click.STRING, help="")
+    @click.option("--labels", type=click.STRING, help="")
+    def list(limit=None, offset=None, filter=None, labels=None):
+        stub = create_yatai_stub()
+        result = stub.ListDeployments(limit=limit, offset=offset, filter=filter, labels=labels)
+        if result.status.status_code != Status.OK:
+            _echo('something wrong', CLI_COLOR_ERROR)
+        else:
+            for deployment in result.deployments:
+                _echo('deployment name: ' + deployment.name)
+
+    cli.add_command(deployment)
     return cli

42816047422d9556b43792a99242dff430194431
yubozhao <yubz86@gmail.com>
2019-08-13 18:10:42 -0700

    add default worker count for sagemaker

--- bentoml/deployment/sagemaker/templates.py
+++ bentoml/deployment/sagemaker/templates.py
@@ -82,9 +82,12 @@ import subprocess
 import os
 import signal
 import sys
+import multiprocessing
+
+default_worker_count = (multiprocessing.cpu_count() // 2) + 1
 
 model_server_timeout = os.environ.get('MODEL_SERVER_TIMEOUT', 60)
-model_server_workers = int(os.environ.get('MODEL_SERVER_WORKERS', get_gunicorn_worker_count()))
+model_server_workers = int(os.environ.get('MODEL_SERVER_WORKERS', default_worker_count))
 
 def sigterm_handler(nginx_pid, gunicorn_pid):
     try:

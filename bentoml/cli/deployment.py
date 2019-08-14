02fceb08498cfd372dbbadb9a077ab6f8eb0a3b9
yubozhao <yubz86@gmail.com>
2019-08-14 15:41:06 -0700

    remove import argparse

--- bentoml/cli/deployment.py
+++ bentoml/cli/deployment.py
@@ -17,7 +17,6 @@ from __future__ import division
 from __future__ import print_function
 
 import click
-import argparse
 
 from bentoml.deployment.serverless import ServerlessDeployment
 from bentoml.deployment.sagemaker import SagemakerDeployment

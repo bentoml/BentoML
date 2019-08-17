80330711a8c1419fbee643894095a2a9b0d6c01e
yubozhao <yubz86@gmail.com>
2019-08-17 11:50:46 -0700

    Include artifacts, apis info for bentoml.yml

--- examples/deploy-with-sagemaker/sentiment_lr_model.py
+++ examples/deploy-with-sagemaker/sentiment_lr_model.py
@@ -4,7 +4,7 @@ from bentoml.artifact import PickleArtifact
 from bentoml.handlers import DataframeHandler
 
 @bentoml.artifacts([PickleArtifact('sentiment_lr')])
-@bentoml.env(conda_dependencies=["scikit-learn", "pandas"])
+@bentoml.env(pip_dependencies=["scikit-learn", "pandas"])
 class SentimentLRModel(bentoml.BentoService):
     
     @bentoml.api(DataframeHandler, typ='series')

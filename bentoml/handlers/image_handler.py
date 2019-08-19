bb8b1a49650181034b2e91aecfb1d15afb5a7fe5
Jakub Bartczuk <bartczukkuba@gmail.com>
2019-08-19 20:20:04 +0200

    ImageHandler - handling multiple images (#265)
    * ImageHandler - handling multiple images
    
    * code style fixes
    
    * swagger schema change

--- bentoml/handlers/image_handler.py
+++ bentoml/handlers/image_handler.py
@@ -71,7 +71,7 @@ class ImageHandler(BentoHandler):
         accept_multiple_files=False,
         pilmode="RGB",
     ):
-        self.input_name = input_name
+        self.input_name = input_name if type(input_name) is tuple else (input_name,)
         self.pilmode = pilmode
         self.accept_file_extensions = accept_file_extensions or [
             ".jpg",
@@ -88,7 +88,8 @@ class ImageHandler(BentoHandler):
                 "schema": {
                     "type": "object",
                     "properties": {
-                        self.input_name: {"type": "string", "format": "binary"}
+                        filename: {"type": "string", "format": "binary"}
+                        for filename in self.input_name
                     },
                 }
             },
@@ -132,10 +133,27 @@ class ImageHandler(BentoHandler):
                 )
 
             input_data = imread(input_stream, pilmode=self.pilmode)
+            result = func(input_data)
         else:
-            return Response(response="Only support single file input", status=400)
+            input_files = [request.files.get(filename) for filename in self.input_name]
+            if input_files:
+                file_names = [secure_filename(file.filename) for file in input_files]
+                for file_name in file_names:
+                    check_file_format(file_name, self.accept_file_extensions)
+                input_streams = [
+                    BytesIO(input_file.read()) for
+                    input_file in input_files
+                ]
+            else:
+                raise ValueError(
+                    "BentoML#ImageHandler unexpected HTTP request: %s" % request
+                )
+            input_data = tuple(
+                imread(input_stream, pilmode=self.pilmode)
+                for input_stream in input_streams
+            )
+            result = func(*input_data)
 
-        result = func(input_data)
         result = get_output_str(result, request.headers.get("output", "json"))
         return Response(response=result, status=200, mimetype="application/json")
 

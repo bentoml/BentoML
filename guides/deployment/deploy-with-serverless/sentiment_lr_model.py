3f5f3106dff6d07b2d57c557ed3e989c087d5950
yubozhao <yubz86@gmail.com>
2019-09-17 11:58:17 -0700

    update serverless deployment guide notebook

--- guides/deployment/deploy-with-serverless/sentiment_lr_model.py
+++ guides/deployment/deploy-with-serverless/sentiment_lr_model.py
@@ -4,7 +4,7 @@ from bentoml.artifact import PickleArtifact
 from bentoml.handlers import DataframeHandler
 
 @bentoml.artifacts([PickleArtifact('sentiment_lr')])
-@bentoml.env(requirements_txt='./requirements.txt')
+@bentoml.env(pip_dependencies=['sklearn', 'numpy', 'pandas'])
 class SentimentLRModel(bentoml.BentoService):
     
     @bentoml.api(DataframeHandler, typ='series')

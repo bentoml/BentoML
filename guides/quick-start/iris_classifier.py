fe43338ff7c5642866c288ce44dbb0cbcdfdf670
Chaoyu Yang <paranoyang@gmail.com>
2019-10-07 17:50:17 -0700

    Update quick-start guide

--- guides/quick-start/iris_classifier.py
+++ guides/quick-start/iris_classifier.py
@@ -2,14 +2,10 @@ from bentoml import BentoService, api, env, artifacts
 from bentoml.artifact import SklearnModelArtifact
 from bentoml.handlers import DataframeHandler
 
-# You can also import your own python module here and BentoML will automatically
-# figure out the dependency chain and package all those python modules
-
 @artifacts([SklearnModelArtifact('model')])
 @env(pip_dependencies=["scikit-learn"])
 class IrisClassifier(BentoService):
 
     @api(DataframeHandler)
     def predict(self, df):
-        # arbitrary preprocessing or feature fetching code can be placed here 
         return self.artifacts.model.predict(df)

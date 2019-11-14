9c0b0edd631844df0eab559565b937045bce6521
yubozhao <yubz86@gmail.com>
2019-11-13 17:08:23 -0800

    WIP

--- bentoml/deployment/aws_lambda/__init__.py
+++ bentoml/deployment/aws_lambda/__init__.py
@@ -20,12 +20,20 @@ import os
 
 from ruamel.yaml import YAML
 
-from bentoml.deployment.aws_lambda.utils import ensure_sam_available_or_raise, \
-    call_sam_command
+from bentoml.deployment.aws_lambda.utils import (
+    ensure_sam_available_or_raise,
+    upload_artifacts_to_s3,
+    init_sam_project,
+    lambda_build,
+    lambda_deploy,
+    lambda_package,
+    generate_framework_layers,
+)
 from bentoml.deployment.operator import DeploymentOperatorBase
 from bentoml.deployment.utils import (
     exception_to_return_status,
     ensure_deploy_api_name_exists_in_bento,
+    ensure_docker_available_or_raise,
 )
 from bentoml.exceptions import BentoMLException
 from bentoml.proto.deployment_pb2 import (
@@ -39,26 +47,48 @@ from bentoml.utils.tempdir import TempDirectory
 from bentoml.yatai.status import Status
 
 
-def generate_function_resource():
-    return {}
+def generate_function_resource(api_name, artifact_bucket_name):
+    return {
+        'Type': 'AWS::Serverless::Function',
+        'Properties': {
+            'Runtime': 'python3.7',
+            'CodeUri': '',
+            'Handler': 'app.{}'.format(api_name),
+            'FunctionName': api_name,
+            'Timeout': 60,
+            'MemorySize': 3008,
+            'Events': {
+                'Api': {
+                    'Type': 'Api',
+                    'Properties': {'Path': '/{}'.format(api_name), 'Method': 'get'},
+                }
+            },
+            'Policies': [{'S3ReadPolicy': {'BucketName': artifact_bucket_name}}],
+        },
+    }
 
 
-def generate_aws_lambda_template_config(project_dir, api_names):
+def generate_aws_lambda_template_config(project_dir, api_names, s3_bucket_name):
     template_file_path = os.path.join(project_dir, 'template.yaml')
     yaml = YAML()
     sam_config = {
         'AWSTemplateFormatVersion': '2010-09-09',
         'Transform': 'AWS::Serverless-2016-10-31',
-        'Globals': {
-            'Function': {
-                'Timeout': 180,
+        'Globals': {'Function': {'Timeout': 180, 'Runtime': 'python3.7'}},
+        'Resources': {},
+        # Output section from cloud formation
+        'Outputs': {
+            'EndpointUrl': {
+                'Value': '!Sub "https://${ServerlessRestApi}.'
+                'execute-api.${AWS::Region}}.amazonaws.com/Prod"',
+                'Description': 'Url for endpoint',
             }
         },
-        'Resources': {},
-        'Outputs': {}
     }
     for api_name in api_names:
-        sam_config['Resources'][api_name] = generate_function_resource()
+        sam_config['Resources'][api_name] = generate_function_resource(
+            api_name, s3_bucket_name
+        )
 
     yaml.dump(sam_config, Path(template_file_path))
 
@@ -67,6 +97,7 @@ class AwsLambdaDeploymentOperator(DeploymentOperatorBase):
     def apply(self, deployment_pb, yatai_service, prev_deployment):
         try:
             ensure_sam_available_or_raise()
+            ensure_docker_available_or_raise()
             deployment_spec = deployment_pb.spec
             aws_config = deployment_spec.aws_lambda_operator_config
 
@@ -81,7 +112,7 @@ class AwsLambdaDeploymentOperator(DeploymentOperatorBase):
                     'BentoML currently only support local repository'
                 )
             else:
-                bento_path = bento_pb.bento.uri.uri
+                bento_archive_path = bento_pb.bento.uri.uri
             bento_service_metadata = bento_pb.bento.bento_service_metadata
 
             api_names = (
@@ -92,10 +123,32 @@ class AwsLambdaDeploymentOperator(DeploymentOperatorBase):
             ensure_deploy_api_name_exists_in_bento(
                 [api.name for api in bento_service_metadata.apis], api_names
             )
+            artifact_bucket_name = '{name}-{bento_name}-{bento_version}'.format(
+                name=deployment_pb.name,
+                bento_name=deployment_spec.bento_name,
+                bento_version=deployment_spec.bento_version,
+            )
+            upload_artifacts_to_s3(
+                aws_config.region,
+                artifact_bucket_name,
+                bento_archive_path,
+                deployment_spec.bento_name,
+            )
             with TempDirectory() as lambda_project_dir:
-                upload_artifacts_to_s3()
-                call_sam_command(['package'])
-                call_sam_command(['deploy'])
+                generate_aws_lambda_template_config(
+                    lambda_project_dir, api_names, artifact_bucket_name
+                )
+                init_sam_project(
+                    lambda_project_dir,
+                    bento_archive_path,
+                    deployment_spec.bento_name,
+                    api_names,
+                )
+                lambda_package(lambda_project_dir, 's3_location')
+                lambda_deploy(
+                    lambda_project_dir,
+                    stack_name=deployment_pb.namespace + '-' + deployment_pb.name,
+                )
 
             res_deployment_pb = Deployment(state=DeploymentState())
             res_deployment_pb.CopyFrom(deployment_pb)
--- bentoml/deployment/aws_lambda/utils.py
+++ bentoml/deployment/aws_lambda/utils.py
@@ -16,11 +16,38 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import shutil
 import subprocess
 import logging
 
+import boto3
+
 from bentoml.exceptions import BentoMLException, BentoMLMissingDependencyException
 
+UNZIP_REQUIREMENTS_PY = """\
+import os
+import shutil
+import sys
+import zipfile
+
+
+pkgdir = '/tmp/sls-py-req'
+
+sys.path.append(pkgdir)
+
+if not os.path.exists(pkgdir):
+    tempdir = '/tmp/_temp-sls-py-req'
+    if os.path.exists(tempdir):
+        shutil.rmtree(tempdir)
+
+    default_layer_root = '/opt'
+    lambda_root = os.getcwd() if os.environ.get('IS_LOCAL') == 'true' else default_layer_root
+    zip_requirements = os.path.join(lambda_root, '.requirements.zip')
+
+    zipfile.ZipFile(zip_requirements, 'r').extractall(tempdir)
+    os.rename(tempdir, pkgdir)  # Atomic
+"""
 
 logger = logging.getLogger(__name__)
 
@@ -45,10 +72,123 @@ def ensure_sam_available_or_raise():
         )
 
 
-def call_sam_command(command):
+def cleanup_build_files(project_dir, api_name):
+    build_dir = os.path.join(project_dir, '.aws-sam/build/{}'.format(api_name))
+    subprocess.check_output(['rm', '-rf', './{*.egg-info,*.dist-info}'])
+
+
+def call_sam_command(command, project_dir):
     command = ['sam'] + command
-    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+    with subprocess.Popen(
+        command, cwd=project_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    ) as proc:
         stdout = proc.stdout.read().decode('utf-8')
         logger.debug('SAM cmd output: %s', stdout)
     return stdout
+
+
+def lambda_build(project_dir, api_names):
+    call_sam_command(['build', '-u'], project_dir)
+    for api_name in api_names:
+        cleanup_build_files(project_dir, api_name)
+
+
+def lambda_package(project_dir, s3_bucket_name):
+    call_sam_command(
+        [
+            'package',
+            '--force-upload',
+            '--s3-bucket',
+            s3_bucket_name,
+            '--template-file',
+            'template.yaml',
+        ],
+        project_dir,
+    )
+
+
+def lambda_deploy(project_dir, stack_name):
+    call_sam_command(
+        ['deploy', '--template-file', 'template.yaml', '--stack-name', stack_name],
+        project_dir,
+    )
+
+
+def upload_artifacts_to_s3(region, bucket_name, bento_archive_path, bento_name):
+    artifacts_path = os.path.join(bento_archive_path, bento_name, 'artifacts')
+    s3_client = boto3.client('s3', region)
+
+    try:
+        for file_name in os.listdir(artifacts_path):
+            s3_client.upload_file(
+                os.path.join(artifacts_path, file_name),
+                bucket_name,
+                '/artifacts/{}'.format(file_name),
+            )
+    except Exception as error:
+        raise BentoMLException(str(error))
+
+
+def create_dependency_layer(sam_project_path, s3_bucket_name):
+    pass
+
+
+def init_sam_project(sam_project_path, bento_archive_path, bento_name, api_names):
+    # Copy requirements.txt
+    requirement_txt_path = os.path.join(bento_archive_path, 'requirements.txt')
+    shutil.copy(requirement_txt_path, sam_project_path)
+
+    # Copy bundled pip dependencies
+    bundled_dep_path = os.path.join(bento_archive_path, 'bundled_pip_dependencies')
+    if os.path.isdir(bundled_dep_path):
+        shutil.copytree(
+            bundled_dep_path, os.path.join(sam_project_path, 'bundled_pip_dependencies')
+        )
+        bundled_files = os.listdir(
+            os.path.join(sam_project_path, 'bundled_pip_dependencies')
+        )
+        has_bentoml_bundle = False
+        for index, bundled_file_name in enumerate(bundled_files):
+            bundled_files[index] = '\n./bundled_pip_dependencies/{}'.format(
+                bundled_file_name
+            )
+            # If file name start with `BentoML-`, assuming it is a
+            # bentoml targz bundle
+            if bundled_file_name.startswith('BentoML-'):
+                has_bentoml_bundle = True
+
+        with open(
+            os.path.join(sam_project_path, 'requirements.txt'), 'r+'
+        ) as requirement_file:
+            required_modules = requirement_file.readlines()
+            if has_bentoml_bundle:
+                # Assuming bentoml is always the first one in
+                # requirements.txt. We are removing it
+                required_modules = required_modules[1:]
+            required_modules = required_modules + bundled_files
+            # Write from beginning of the file, instead of appending to
+            # the end.
+            requirement_file.seek(0)
+            requirement_file.writelines(required_modules)
+
+    lambda_build()
+    cleanup_build_files()
+
+    # Copy bento_service_model
+    model_path = os.path.join(bento_archive_path, bento_name)
+    shutil.copytree(model_path, os.path.join(sam_project_path, bento_name))
+
+    # remove the artifacts dir. Artifacts will be upload to S3
+    shutil.rmtree(os.path.join(sam_project_path, bento_name, 'artifacts'))
+
+    # generate app.py
+    with open(os.path.join(sam_project_path, 'app.py'), 'w') as f:
+        f.write('')
+        for api_name in api_names:
+            api_content = ''
+            f.write(api_content)
+
+    # generate unzip_requirements.py
+    with open(os.path.join(sam_project_path, 'unzip_requirements.py'), 'w') as f:
+        f.write(UNZIP_REQUIREMENTS_PY)
 

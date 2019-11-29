9b16235ee778c79aae0e01e578056a11e51be9ab
hrmthw <hrmthw.troy@gmail.com>
2019-11-29 17:56:17 +0800

    tf_tensor_handler test

--- bentoml/handlers/tensorflow_tensor_handler.py
+++ bentoml/handlers/tensorflow_tensor_handler.py
@@ -22,10 +22,46 @@ from bentoml.handlers.base_handlers import BentoHandler
 class TensorflowTensorHandler(BentoHandler):
     """
     Tensor handlers for Tensorflow models
+    ref: 
+        * https://www.tensorflow.org/tfx/serving/api_rest#predict_api
+        * https://github.com/tensorflow/serving/blob/91adea9716b57dd58427714427b944fbe9b3f89e/tensorflow_serving/model_servers/tensorflow_model_server_test.py#L425
     """
 
+    @property
+    def request_schema(self):
+        default = {"application/json": {"schema": {"type": "object"}}}
+        if self.input_dtypes is None:
+            return default
+
+        if isinstance(self.input_dtypes, dict):
+            return {
+                "application/json": {  # For now, only declare JSON on docs.
+                    "schema": {
+                        "type": "object",
+                        "properties": {
+                            k: {"type": "array", "items": {"type": self._get_type(v)}}
+                            for k, v in self.input_dtypes.items()
+                        },
+                    }
+                }
+            }
+
+        return default
+
     def handle_request(self, request, func):
-        raise NotImplementedError
+        if request.content_type == "application/json":
+            parsed_json = json.loads(request.data.decode("utf-8"))
+        else:
+            return make_response(
+                jsonify(
+                    message="Request content-type must be 'application/json'"
+                    "for this BentoService API"
+                ),
+                400,
+            )
+        result = func(parsed_json)
+        result = get_output_str(result, request.headers.get("output", "json"))
+        return Response(response=result, status=200, mimetype="application/json")
 
     def handle_cli(self, args, func):
         raise NotImplementedError

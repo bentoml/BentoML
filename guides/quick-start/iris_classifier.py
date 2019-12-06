7a51bfe3d32ecdb2a96317bf3d350dc8dd8b7ad2
yubozhao <yubz86@gmail.com>
2019-12-06 14:09:35 -0800

    undo clean cells of quick start guide

--- guides/quick-start/iris_classifier.py
+++ guides/quick-start/iris_classifier.py
@@ -3,7 +3,7 @@ from bentoml.artifact import SklearnModelArtifact
 from bentoml.handlers import DataframeHandler
 
 @artifacts([SklearnModelArtifact('model')])
-@env(pip_dependencies=["scikit-learn==0.20.3"])
+@env(pip_dependencies=["scikit-learn"])
 class IrisClassifier(BentoService):
 
     @api(DataframeHandler)

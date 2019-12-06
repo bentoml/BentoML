b5ec7d771d10db624c7b63423ebc55055e5f185d
yubozhao <yubz86@gmail.com>
2019-12-06 12:20:51 -0800

    lock scikit-learn version for quick-start guide

--- guides/quick-start/iris_classifier.py
+++ guides/quick-start/iris_classifier.py
@@ -3,7 +3,7 @@ from bentoml.artifact import SklearnModelArtifact
 from bentoml.handlers import DataframeHandler
 
 @artifacts([SklearnModelArtifact('model')])
-@env(pip_dependencies=["scikit-learn"])
+@env(pip_dependencies=["scikit-learn==0.20.3"])
 class IrisClassifier(BentoService):
 
     @api(DataframeHandler)

dbc5584e5a54fb84a7a29fc066b13d431710d034
hrmthw <hrmthw.troy@gmail.com>
2019-12-11 16:32:19 +0800

    fix tf_tensor_handler test

--- tests/handlers/test_tf_tensor_handler.py
+++ tests/handlers/test_tf_tensor_handler.py
@@ -1,95 +1,53 @@
-import pytest
-from collections import namedtuple
-import pandas as pd
+import sys
+import json
+import base64
+import math
 import numpy as np
-import tensorflow as tf
-
-from bentoml.handlers import TensorflowTensorHandler
 
 try:
-    from unittest.mock import Mock
+    from unittest.mock import Mock, MagicMock
 except ImportError:
-    from mock import Mock
+    from mock import Mock, MagicMock
 
 
-TestCase = namedtuple("TestCase", ("input", "spec", "output"))
+def mock_tensorflow_module():
+    class MockTensor:
+        def __init__(self, _input):
+            self.input = _input
+    
+        def numpy(self):
+            if isinstance(self.input, (list, tuple)):
+                return np.array(self.input, dtype=object)
+            return self.input
+    
+        def __eq__(self, dst):
+            return self.input == dst.input
+        
+    class MockConstant(MockTensor):
+        pass
 
-TEST_CASES = [
-    TestCase(  # row format, list of 2 tensors each of [1, 2] shape
-        {'instances': [ [[1, 2]], [[3, 4]] ]},
-        tf.TensorSpec(dtype=tf.int32, shape=(2, 1, 2)),
-        tf.constant([ [[1, 2]], [[3, 4]] ]),
-    ),
-    TestCase( # with non RFC7159 values
-        input={
-            "instances": [[1.0, -np.Infinity, pd.Nan, np.Infinity]]
-        },
-        tf.TensorSpec(dtype=tf.float32, shape=(1, 4)),
-        output=tf.constant([[1.0, -np.Infinity, pd.Nan, np.Infinity]], dtype=tf.float32)
-    ),
-    # TestCase( # with specific input name
-    #     input={
-    #         "instances": [
-    #             {
-    #                 "sensor_readings": [ 1.0, -np.Infinity, pd.Nan, np.Infinity ],
-    #             }
-    #         ]
-    #     },
-    #     tf.TensorSpec(dtype=tf.float32, shape=None),
-    #     output={"predictions": [1.0, -np.Infinity, pd.Nan, np.Infinity]},
-    # ),
-    # TestCase( # with binary
-    #     input={
-    #         "signature_name": "classify_objects",
-    #         "instances": [
-    #             {
-    #                 "image": { "b64": "aW1hZ2UgYnl0ZXM=" },
-    #                 "caption": "seaside"
-    #             },
-    #             {
-    #             "image": { "b64": "YXdlc29tZSBpbWFnZSBieXRlcw==" },
-    #             "caption": "mountains"
-    #             }
-    #         ]
-    #     },
-    #     tf.TensorSpec(dtype=tf.float32, shape=None),
-    #     output={},
-    # ),
-    # TestCase( # columnar format
-    #     input={
-    #         "inputs": {
-    #             "tag": ["foo", "bar"],
-    #             "signal": [[1, 2, 3, 4, 5], [3, 4, 1, 2, 5]],
-    #             "sensor": [[[1, 2], [3, 4]], [[4, 5], [6, 8]]]
-    #         }
-    #     },
-    #     tf.TensorSpec(dtype=tf.float32, shape=None),
-    #     output={},
-    # ),
-    # TestCase( # List of 3 scalar tensors
-    #     {'instances': ["foo", "bar", "baz"]},
-    #     tf.TensorSpec(dtype=None, shape=None),
-    #     tf.constant(["foo", "bar", "baz"])
-    # ),
-    # TestCase( # multiple named inputs
-    #     input={
-    #         "signature_name": "",
-    #         "instances": [
-    #             {
-    #                 "tag": "foo",
-    #                 "signal": [1, 2, 3, 4, 5],
-    #                 "sensor": [[1, 2], [3, 4]]
-    #             },
-    #             {
-    #                 "tag": "bar",
-    #                 "signal": [[3, 4, 1, 2, 5]],
-    #                 "sensor": [[4, 5], [6, 8]]
-    #             }
-    #         ]
-    #     },
-    #     test_function=lambda d: d,
-    #     output={},
-    # ),
+    sys.modules['tensorflow'] = MagicMock()
+    
+    import tensorflow as tf
+    tf.__version__ = "2.0"
+    tf.Tensor = tf.compat.v2.Tensor = MockTensor
+    tf.constant = tf.compat.v2.constant = MockConstant
+    
+
+mock_tensorflow_module()
+
+
+STR_BYTES = b"hello world"
+STR = STR_BYTES.decode("utf-8")
+STR_B64 = base64.b64encode(STR_BYTES).decode()
+
+BIN_BYTES = b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR"
+BIN_B64 = base64.b64encode(BIN_BYTES).decode()
+
+COMMON_TEST_CASES = [
+    {'instances': [[[1, 2]], [[3, 4]]]},
+    {"instances": [[1.0, -float('inf'), float('inf')]]},
+    {"instances": float('nan')},
 ]
 
 
@@ -97,22 +55,45 @@ def test_tf_tensor_handle_request():
     '''
     ref: https://www.tensorflow.org/tfx/serving/api_rest#request_format_2
     '''
+    from bentoml.handlers import TensorflowTensorHandler
 
     request = Mock()
     request.headers = {}
     request.content_type = 'application/json'
 
-    for input_data, spec, output in TEST_CASES:
-        handler = TensorflowTensorHandler(spec=spec)
-        request.data = input_data
+    handler = TensorflowTensorHandler()
+
+    for input_data in COMMON_TEST_CASES:
+        request.data = json.dumps(input_data).encode('utf-8')
         result = handler.handle_request(request, lambda i: i)
         predictions = json.loads(result.get_data().decode('utf-8'))['predictions']
-        assert predictions == output.numpy().tolist()
+        assert (input_data['instances'] == predictions
+                or math.isnan(input_data['instances']) and math.isnan(predictions))
+    
+    # test str b64
+    input_data = {"instances": {"b64": STR_B64}}
+    request.data = json.dumps(input_data).encode("utf8")
+    result = handler.handle_request(request, lambda i: i)
+    predictions = json.loads(result.get_data().decode('utf-8'))['predictions']
+    assert STR == predictions
 
+    input_data = {"instances": [{"b64": STR_B64}]}
+    request.data = json.dumps(input_data).encode("utf8")
+    result = handler.handle_request(request, lambda i: i)
+    predictions = json.loads(result.get_data().decode('utf-8'))['predictions']
+    assert [STR] == predictions
 
-# def test_tf_tensor_handle_cli(capsys, tmpdir):
-#     pass
+    # test bin b64
+    input_data = {"instances": {"b64": BIN_B64}}
+    request.data = json.dumps(input_data).encode("utf8")
+    result = handler.handle_request(request, lambda i: i)
+    predictions = json.loads(result.get_data().decode('utf-8'))['predictions']
+    assert {"b64": BIN_B64} == predictions
 
+    input_data = {"instances": [{"b64": BIN_B64}]}
+    request.data = json.dumps(input_data).encode("utf8")
+    result = handler.handle_request(request, lambda i: i)
+    predictions = json.loads(result.get_data().decode('utf-8'))['predictions']
+    assert [{"b64": BIN_B64}] == predictions
 
-# def test_tf_tensor_handle_aws_lambda_event():
-#     pass
+test_tf_tensor_handle_request()

104698db9e9dee4ff0e4dd17a15f2157efda5b1c
Bozhao <yubz86@gmail.com>
2019-12-20 12:57:02 -0800

    Improve Lambda deployment - properly handle stacks at "dead" status before deploying (#448)
    * move const out of func
    
    * improve lambda deploy func
    
    * Add debugging info

--- bentoml/deployment/aws_lambda/utils.py
+++ bentoml/deployment/aws_lambda/utils.py
@@ -95,6 +95,7 @@ def call_sam_command(command, project_dir, region):
     # We are passing region as part of the param, due to sam cli is not currently
     # using the region that passed in each command.  Set the region param as
     # AWS_DEFAULT_REGION for the subprocess call
+    logger.debug('Setting envar "AWS_DEFAULT_REGION" to %s for subprocess call', region)
     copied_env = os.environ.copy()
     copied_env['AWS_DEFAULT_REGION'] = region
 
@@ -144,6 +145,13 @@ def lambda_package(project_dir, aws_region, s3_bucket_name, deployment_prefix):
 
 
 def lambda_deploy(project_dir, aws_region, stack_name):
+    # if the stack name exists and the state is in rollback_complete or
+    # other 'bad' state, we will delete the stack first, and then deploy
+    # it
+    logger.debug('Ensure stack "%s" is ready to deploy', stack_name)
+    ensure_is_ready_to_deploy_to_cloud_formation(stack_name, aws_region)
+    logger.debug('Stack "%s"is ready to deploy', stack_name)
+
     template_file = os.path.join(project_dir, '.aws-sam', 'build', 'packaged.yaml')
     return_code, stdout, stderr = call_sam_command(
         [
@@ -347,5 +355,46 @@ def reduce_bundle_size_and_upload_extra_resources_to_s3(
             s3_bucket,
             os.path.join(deployment_prefix, 'requirements.tar'),
         )
+    except ClientError as e:
+        raise BentoMLException(str(e))
+
+
+# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/\
+# using-cfn-describing-stacks.html
+FAILED_CLOUDFORMATION_STACK_STATUS = [
+    'CREATE_FAILED',
+    # Ongoing creation of one or more stacks with an expected StackId
+    # but without any templates or resources.
+    'REVIEW_IN_PROGRESS',
+    'ROLLBACK_FAILED',
+    # This status exists only after a failed stack creation.
+    'ROLLBACK_COMPLETE',
+    # Ongoing removal of one or more stacks after a failed stack
+    # creation or after an explicitly cancelled stack creation.
+    'ROLLBACK_IN_PROGRESS',
+]
+
+
+def ensure_is_ready_to_deploy_to_cloud_formation(stack_name, region):
+    try:
+        cf_client = boto3.client('cloudformation', region)
+        logger.debug('Checking stack description')
+        describe_formation_result = cf_client.describe_stacks(StackName=stack_name)
+        result_stacks = describe_formation_result.get('Stacks')
+        if len(result_stacks):
+            logger.debug('Stack "%s" exists', stack_name)
+            stack_result = result_stacks[0]
+            if stack_result['StackStatus'] in [
+                'ROLLBACK_COMPLETE',
+                'ROLLBACK_FAILED',
+                'ROLLBACK_IN_PROGRESS',
+            ]:
+                logger.debug(
+                    'Stack "%s" is in a "bad" status(%s), deleting the stack '
+                    'before deployment',
+                    stack_name,
+                    stack_result['StackStatus']
+                )
+                cf_client.delete_stack(StackName=stack_name)
     except ClientError as e:
         raise BentoMLException(str(e))

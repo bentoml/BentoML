094b8d68cd3210135a15ee08c3a0d0681b635043
yubozhao <yubz86@gmail.com>
2020-03-03 11:24:29 -0800

    update e2e tests to work with pytest

---
+++ e2e_tests/basic_bento_service_examples.py
@@ -0,0 +1,16 @@
+from bentoml import BentoService, api
+from bentoml.handlers import JsonHandler
+
+
+class BasicBentoService(BentoService):
+    @api(JsonHandler)
+    def predict(self, data):
+        return 'cat'
+
+
+class UpdatedBasicBentoService(BentoService):
+    @api(JsonHandler)
+    def predict(self, data):
+        return 'dog'
+
+
--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -2,6 +2,8 @@ import logging
 import pytest
 from sklearn import svm, datasets
 
+from e2e_tests.basic_bento_service_examples import BasicBentoService, \
+    UpdatedBasicBentoService
 from e2e_tests.iris_classifier_example import IrisClassifier
 from e2e_tests.cli_operations import delete_bento
 
@@ -22,5 +24,27 @@ def iris_clf_service():
     iris_clf_service.save()
 
     bento_name = f'{iris_clf_service.name}:{iris_clf_service.version}'
+    yield bento_name
+    delete_bento(bento_name)
+
+
+@pytest.fixture()
+def basic_bentoservice_v1():
+    logger.debug('Creating iris classifier BentoService bundle..')
+    bento_svc = BasicBentoService()
+    bento_svc.save()
+
+    bento_name = f'{bento_svc.name}:{bento_svc.version}'
+    yield bento_name
+    delete_bento(bento_name)
+
+
+@pytest.fixture()
+def basic_bentoservice_v2():
+    logger.debug('Creating iris classifier BentoService bundle..')
+    bento_svc = UpdatedBasicBentoService()
+    bento_svc.save()
+
+    bento_name = f'{bento_svc.name}:{bento_svc.version}'
     yield bento_name
     delete_bento(bento_name)

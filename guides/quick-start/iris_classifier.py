238c5198fd2a5d81974a6c76fb4dd7342a7ead25
Chaoyu <paranoyang@gmail.com>
2020-05-06 01:06:41 -0700

    [DOCS] Update quickstart notebook  (#632)

--- guides/quick-start/iris_classifier.py
+++ guides/quick-start/iris_classifier.py
@@ -1,11 +1,12 @@
-from bentoml import BentoService, api, env, artifacts
-from bentoml.artifact import SklearnModelArtifact
+from bentoml import env, artifacts, api, BentoService
 from bentoml.handlers import DataframeHandler
+from bentoml.artifact import SklearnModelArtifact
 
-@artifacts([SklearnModelArtifact('model')])
 @env(auto_pip_dependencies=True)
+@artifacts([SklearnModelArtifact('model')])
 class IrisClassifier(BentoService):
 
     @api(DataframeHandler)
     def predict(self, df):
+        # Optional pre-processing, post-processing code goes here
         return self.artifacts.model.predict(df)

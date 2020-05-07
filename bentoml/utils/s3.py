8b9b21ffa5737729a6aa028f0eedd7a3b82da12c
Bozhao <yubz86@gmail.com>
2020-05-06 19:42:53 -0700

    Fix create s3 bucket issue for us-east-1 region (#633)
    * Fix create s3 bucket for us-east-1 region issue
    
    * remove prints
    
    * update

--- bentoml/utils/s3.py
+++ bentoml/utils/s3.py
@@ -71,10 +71,30 @@ def create_s3_bucket_if_not_exists(bucket_name, region):
     except ClientError as error:
         if error.response and error.response['Error']['Code'] == 'NoSuchBucket':
             logger.debug('Creating s3 bucket: %s in region %s', bucket_name, region)
-            s3_client.create_bucket(
-                Bucket=bucket_name,
-                CreateBucketConfiguration={'LocationConstraint': region},
-            )
+
+            # NOTE: boto3 will raise ClientError(InvalidLocationConstraint) if
+            # `LocationConstraint` is set to `us-east-1` region.
+            # https://github.com/boto/boto3/issues/125.
+            # This issue still show up in  boto3 1.13.4(May 6th 2020)
+            try:
+                s3_client.create_bucket(
+                    Bucket=bucket_name,
+                    CreateBucketConfiguration={'LocationConstraint': region},
+                )
+            except ClientError as s3_error:
+                if (
+                    s3_error.response
+                    and s3_error.response['Error']['Code']
+                    == 'InvalidLocationConstraint'
+                ):
+                    logger.debug(
+                        'Special s3 region: %s, will attempt create bucket without '
+                        '`LocationConstraint`',
+                        region
+                    )
+                    s3_client.create_bucket(Bucket=bucket_name)
+                else:
+                    raise s3_error
         else:
             raise error
 

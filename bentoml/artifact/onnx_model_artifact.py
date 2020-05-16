5b6bc277b71e876a1436993a2f410d895daed3fe
yubozhao <yubz86@gmail.com>
2020-05-16 14:55:30 -0700

    Update onnx_model_artifact.py

--- bentoml/artifact/onnx_model_artifact.py
+++ bentoml/artifact/onnx_model_artifact.py
@@ -18,11 +18,28 @@ from __future__ import print_function
 
 import logging
 import os
+import pathlib
+import shutil
 
 from bentoml.artifact import BentoServiceArtifact, BentoServiceArtifactWrapper
 from bentoml.exceptions import MissingDependencyException, InvalidArgument
 
 logger = logging.getLogger(__name__)
+
+
+def _is_path_like(p):
+    return isinstance(p, (str, bytes, pathlib.PurePath, os.PathLike))
+
+
+def _load_onnx_saved_model(path):
+    try:
+        import onnxruntime
+    except ImportError:
+        raise MissingDependencyException(
+            'onnxruntime package is required for inferencing using ONNX runtime'
+        )
+    model = onnxruntime.InferenceSession(path)
+    return model
 
 
 class OnnxModelArtifact(BentoServiceArtifact):
@@ -36,128 +53,78 @@ class OnnxModelArtifact(BentoServiceArtifact):
 
     def __init__(self, name, backend='onnx'):
         super(OnnxModelArtifact, self).__init__(name)
-        self.model_extension = '.onnx'
         self.backend = backend
-        # throw if training_framework is empty
 
     def _model_file_path(self, base_path):
         return os.path.join(base_path, self.name + '.onnx')
 
-    def pack(self, model, training_framework=None, initial_types=None):  # pylint:disable=arguments-differ
-        return _OnnxModelArtifactWrapper(
-            self, model, training_framework, initial_types
-        )
+    def pack(self, obj):  # pylint:disable=arguments-differ
+        if _is_path_like(obj):
+            return _ExportedOnnxModelArtifact(self, obj)
+        return _OnnxModelArtifactWrapper(self, obj)
 
     def load(self, path):
         if self.backend == 'onnx':
-            inference_model = OnnxRuntimeSession(self._model_file_path(path))
+            inference_model = _load_onnx_saved_model(self._model_file_path(path))
         else:
-            raise NotImplementedError('not now')
+            raise NotImplementedError('not now bro')
         return self.pack(inference_model)
 
     @property
     def pip_dependencies(self):
-        dependencies = ['onnx']
+        dependencies = []
         if self.backend == 'onnx':
             dependencies.append('onnxruntime')
         return dependencies
 
 
 class _OnnxModelArtifactWrapper(BentoServiceArtifactWrapper):
-    def __init__(self, spec, model, training_framework, initial_types=None):
+    def __init__(self, spec, model):
         super(_OnnxModelArtifactWrapper, self).__init__(spec)
-        try:
-            import onnx
-        except ImportError:
-            raise MissingDependencyException(
-                "onnx package is required to use OnnxModelArtifact"
-            )
-        # check instance
         self._model = model
-        self.training_framework = training_framework
-        self.initial_types = initial_types
 
     def get(self):
         return self._model
 
     def save(self, dst):
         try:
-            import onnxmltools
+            import onnx
         except ImportError:
-            raise MissingDependencyException(
-                "onnx package is required to use OnnxModelArtifact"
-            )
-
-        logger.info(self.training_framework)
-        logger.info(self.initial_types)
-        if self.training_framework == 'scikit-learn':
-            try:
-                import skl2onnx
-            except ImportError:
-                raise MissingDependencyException(
-                    'skl2onnx package is required for converting sk model to ONNX model'
-                )
-            converted_model = onnxmltools.convert_sklearn(
-                model=self._model, initial_types=self.initial_types
-            )
-        elif self.training_framework == 'pytorch':
-            raise NotImplementedError
-        elif self.training_framework == 'tensorflow':
-            try:
-                import tf2onnx
-            except ImportError:
-                raise MissingDependencyException(
-                    'tf2onnx is required for converting Tensorflow model to ONNX model'
-                )
-            converted_model = onnxmltools.convert_tensorflow(
-                frozen_graph_def=self._model
-            )
-        elif self.training_framework == 'keras':
-            converted_model = onnxmltools.convert_keras(
-                model=self._model, initial_types=self.initial_types
-            )
-        elif self.training_framework == 'lightgbm':
-            converted_model = onnxmltools.convert_lightgbm(
-                model=self._model, initial_types=self.initial_types
-            )
-        elif self.training_framework == 'xgboost':
-            converted_model = onnxmltools.convert_xgboost(
-                model = self._model, initial_types=self.initial_types
-            )
-        # not release on pypi yet
-        # elif self.training_framework == 'h2o':
-        #     converted_model = onnxmltools.convert_h2o(
-        #         model=self._model, initial_types=self.initial_types
-        #     )
-        elif self.training_framework == 'coreml':
-            try:
-                import coremltools
-            except ImportError:
-                raise MissingDependencyException(
-                    'coremltools is required for converting CoreML model to ONNX model'
-                )
-            converted_model = onnxmltools.convert_coreml(
-                model=self._model, initial_types=self.initial_types
-            )
-        else:
-            raise InvalidArgument('training framework not supported')
-
-        return onnxmltools.utils.save_model(converted_model, self.spec._model_file_path(dst))
+            raise MissingDependencyException('TODO')
+        if not isinstance(self._model, onnx.ModelProto):
+            raise InvalidArgument('TODO')
+        return onnx.save(self._model, self.spec._model_file_path(dst))
 
 
-class OnnxRuntimeSession:
-    def __init__(self, model_path):
-        try:
-            import onnxruntime
-        except ImportError:
-            raise MissingDependencyException(
-                'onnxruntime package is required for inferencing using ONNX runtime'
-            )
+class _ExportedOnnxModelArtifact(BentoServiceArtifactWrapper):
+    def __init__(self, spec, path):
+        super(_ExportedOnnxModelArtifact, self).__init__(spec)
 
-        self.model_path = model_path
-        self.session = onnxruntime.InferenceSession(self.model_path)
-        self.input_name = self.session.get_inputs()[0].name
-        self.output_name = self.session.get_outputs()[0].name
+        self.path = path
+        self.model = None
 
-    def predict(self, input_data):
-        return self.session.run([self.output_name], {self.input_name: input_data})
+    def save(self, dst):
+        shutil.copyfile(self.path, self.spec._model_file_path(dst))
+
+    def get(self):
+        if not self.model:
+            self.model = _load_onnx_saved_model(self.spec._model_file_path(self.path))
+        return self.model
+
+
+# class OnnxRuntimeSession:
+#     def __init__(self, model_path):
+#         try:
+#             import onnxruntime
+#         except ImportError:
+#             raise MissingDependencyException(
+#                 'onnxruntime package is required for inferencing using ONNX runtime'
+#             )
+#
+#         self.model_path = model_path
+#         self.session = onnxruntime.InferenceSession(self.model_path)
+#         self.input_name = self.session.get_inputs()[0].name
+#         self.output_name = self.session.get_outputs()[0].name
+#
+#     def predict(self, input_data):
+#         return self.session.run([self.output_name], {self.input_name: input_data})

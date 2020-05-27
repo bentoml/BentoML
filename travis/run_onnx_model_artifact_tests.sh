ed9e14c3974dc5c02b06f83c75f88a7b23471149
Bozhao <yubz86@gmail.com>
2020-05-27 09:39:56 -0700

    [CI] Include onnx artifact test on travis (#726)
    * Include artifact tests (onnx) for travis-ci
    
    * rename artifact testing script to onnx testing script

---
+++ travis/run_onnx_model_artifact_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# https://stackoverflow.com/questions/42218009/how-to-tell-if-any-command-in-bash-script-failed-non-zero-exit-status/42219754#42219754
+# Set err to 1 if pytest failed.
+error=0
+trap 'error=1' ERR
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cd $GIT_ROOT
+
+# Install required packages for onnx model artifacts test
+pip install onnx onnxruntime skl2onnx
+pytest $GIT_ROOT/tests/artifact/test_onnx_model_artifact.py --cov=bentoml
+
+
+test $error = 0 # Return non-zero if pytest failed

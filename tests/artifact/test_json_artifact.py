5d23859bb34f7e1942db31435273f9255929efc0
adriaan <lemontheme@gmail.com>
2020-06-01 20:49:27 +0200

    Feature/670 add jsonartifact (#746)
    * Add JSON artifact class and tests. Resolves #670.
    
    * Add clarifying documentation.
    
    * Yield to linter. Reformat l13.
    
    * Fix test
    
    Was testing on types instead of expected values.

---
+++ tests/artifact/test_json_artifact.py
@@ -0,0 +1,23 @@
+import bentoml
+from bentoml.artifact import JSONArtifact
+
+
+@bentoml.artifacts([JSONArtifact("hparams")])
+class ExampleServiceWithJSONArtifact(bentoml.BentoService):
+    pass
+
+
+def test_json_artifact_simple_service_round_trip(tmp_path):
+    service = ExampleServiceWithJSONArtifact()
+    # 'hparams' = hyperparameters used at training time.
+    hparams = {
+        "lr": 1e-3,
+        "patience": 10,
+        "decay": 0.1,
+        "batch_size": 16
+    }
+    service.pack("hparams", hparams)
+    service.save_to_dir(str(tmp_path))
+    del service
+    new_service = bentoml.bundler.load(str(tmp_path))
+    assert new_service.artifacts.hparams == hparams

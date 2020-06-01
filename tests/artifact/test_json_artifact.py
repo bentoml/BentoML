39c5aad77919220dd7bd6cac308a9cf0d219a265
adriaan <lemontheme@gmail.com>
2020-06-01 11:45:44 +0200

    Yield to linter. Reformat l13.

--- tests/artifact/test_json_artifact.py
+++ tests/artifact/test_json_artifact.py
@@ -10,7 +10,12 @@ class ExampleServiceWithJSONArtifact(bentoml.BentoService):
 def test_json_artifact_simple_service_round_trip(tmp_path):
     service = ExampleServiceWithJSONArtifact()
     # 'hparams' = hyperparameters used at training time.
-    service.pack("hparams", {"lr": 1e-3, "patience": 10, "decay": 0.1, "batch_size": 16})
+    service.pack("hparams", {
+        "lr": 1e-3,
+        "patience": 10,
+        "decay": 0.1,
+        "batch_size": 16
+    })
     service.save_to_dir(str(tmp_path))
     del service
     new_service = bentoml.bundler.load(str(tmp_path))

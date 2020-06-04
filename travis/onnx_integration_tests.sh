8487feb5dc86a55b3ac46b7effdffccc0845ed6f
Chaoyu <paranoyang@gmail.com>
2020-06-04 11:25:05 -0700

    [CI] Move onnx test to integration_tests directory (#757)

--- travis/onnx_integration_tests.sh
+++ travis/onnx_integration_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # https://stackoverflow.com/questions/42218009/how-to-tell-if-any-command-in-bash-script-failed-non-zero-exit-status/42219754#42219754
 # Set err to 1 if pytest failed.
 error=0
@@ -8,6 +9,9 @@ trap 'error=1' ERR
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT
 
-pytest tests --cov=bentoml --ignore tests/artifact
+# Install required packages for onnx model artifacts test
+pip install onnx onnxruntime skl2onnx
+pytest $GIT_ROOT/tests/integration_tests/test_onnx_model_artifact.py --cov=bentoml
+
 
 test $error = 0 # Return non-zero if pytest failed

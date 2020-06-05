633be8e413c7c29e5464db74f5e511e91b609217
jjmachan <jamesjithin97@gmail.com>
2020-06-05 18:02:48 +0530

    unit testing for pytorch artifacts

---
+++ tests/bento_service_examples/pytorch_classifier.py
@@ -0,0 +1,17 @@
+import numpy
+import torch
+
+import bentoml
+from bentoml.artifact import PytorchModelArtifact
+from bentoml.handlers import DataframeHandler
+
+@bentoml.env(auto_pip_dependencies=True)
+@bentoml.artifacts([PytorchModelArtifact('model')])
+class PytorchClassifier(bentoml.BentoService):
+    @bentoml.api(DataframeHandler)
+    def predict(self, df):
+        input_data = df.to_numpy().astype(numpy.float32)
+        input_tensor = torch.from_numpy(input_data)
+        output = self.artifacts.model(input_tensor)
+
+        return output.unsqueeze(dim=0).item()

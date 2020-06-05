8c61f52e625469dfa8024e26ac3723f83a131ca8
Chaoyu <paranoyang@gmail.com>
2020-06-05 04:41:35 -0700

    update sagemaker docker image config

--- bentoml/deployment/sagemaker/__init__.py
+++ bentoml/deployment/sagemaker/__init__.py
@@ -57,29 +57,15 @@ FROM {docker_base_image}
 
 EXPOSE 8080
 
-RUN set -x \\
-     && apt-get update \\
-     && apt-get install --no-install-recommends --no-install-suggests -y libpq-dev build-essential\\
-     && apt-get install -y nginx \\
-     && rm -rf /var/lib/apt/lists/*
-
-# pre-install BentoML base dependencies
-RUN conda install pip numpy scipy \\
-      && pip install gunicorn gevent
+RUN apt-get update --fix-missing && \
+    apt-get install -y nginx && \
+    apt-get clean
 
 # copy over model files
 COPY . /opt/program
 WORKDIR /opt/program
 
-# update conda base env
-RUN conda env update -n base -f /opt/program/environment.yml
-RUN pip install -r /opt/program/requirements.txt
-
-# Install additional pip dependencies inside bundled_pip_dependencies dir
-RUN if [ -f /bento/bentoml_init.sh ]; then /bin/bash -c /bento/bentoml_init.sh; fi
-
-# run user defined setup script
-RUN if [ -f /opt/program/setup.sh ]; then /bin/bash -c /opt/program/setup.sh; fi
+RUN if [ -f /opt/program/bentoml_init.sh ]; then /bin/bash -c /opt/program/bentoml_init.sh; fi
 
 ENV PATH="/opt/program:$PATH"
 """  # noqa: E501

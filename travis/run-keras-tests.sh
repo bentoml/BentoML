bdae6076fcd7ad218e943d0c7ef940ce63de3fcf
jjmachan <jamesjithin97@gmail.com>
2020-06-05 18:33:53 +0530

    added travis script

---
+++ travis/run-keras-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/42218009/how-to-tell-if-any-command-in-bash-script-failed-non-zero-exit-status/42219754#42219754
+# Set err to 1 if pytest failed.
+error=0
+trap 'error=1' ERR
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cd $GIT_ROOT
+
+# Install tensorflow
+pip install tensorflow
+pytest $GIT_ROOT/tests/integration_tests/test_pytorch_model_artifact.py --cov=bentoml
+
+
+test $error = 0 # Return non-zero if pytest failed

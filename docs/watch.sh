d77cbd5a6e4f17a1a5b681dde587fa6353d3bf29
Aanand Kainth <aanand@akainth.me>
2020-06-10 19:10:50 +0200

    [DEV] Improve bash script robustness (#774)
    * Repository and Deployment refactor and cleanup (#771)
    
    * make sagemaker docker image have same file structure
    
    * use consistent file names
    
    * move operator code out of __init__ to avoid loading unused code in model server startup
    
    * refactor deployment validator
    
    * reorganize bento repository code
    
    * deployment valiator test&linting error fix
    
    * more repository code cleanup
    
    * renaming and adding inline comments
    
    * move out lambda operator code to separate file
    
    * Improve robustness of shell scripts
    
    * Fix header
    
    * Fix issues in generate.sh
    
    * Fix -type in call to find
    
    Co-authored-by: cory <cory.massaro@gmail.com>

--- docs/watch.sh
+++ docs/watch.sh
@@ -7,18 +7,18 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 trap 'kill $(jobs -p)' EXIT
 
 echo "Initial docs build..."
-cd $GIT_ROOT/docs && make html
+cd "$GIT_ROOT"/docs && make html
 
 echo "Starting local http server for preview..."
-python3 -m http.server --directory $GIT_ROOT/docs/build/html &
+python3 -m http.server --directory "$GIT_ROOT"/docs/build/html &
 
 echo "Open browser..."
 open -a "Google Chrome" http://0.0.0.0:8000/
 
-fswatch -o $GIT_ROOT/docs $GIT_ROOT/bentoml | while read; \
+fswatch -o "$GIT_ROOT"/docs "$GIT_ROOT"/bentoml | while read -r; \
   do \
     echo "Change detected, rebuilding docs..."
-    cd $GIT_ROOT/docs && make html
+    cd "$GIT_ROOT"/docs && make html
 
     # refresh page
     osascript -e '

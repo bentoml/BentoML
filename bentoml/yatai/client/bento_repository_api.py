cfbd294f126c63316e2a3bdac35b90fe67b5592a
Chaoyu <paranoyang@gmail.com>
2020-06-23 19:54:27 -0700

    linter fix

--- bentoml/yatai/client/bento_repository_api.py
+++ bentoml/yatai/client/bento_repository_api.py
@@ -34,7 +34,7 @@ from bentoml.yatai.proto.repository_pb2 import (
 )
 from bentoml.yatai.proto import status_pb2
 from bentoml.utils.tempdir import TempDirectory
-from bentoml.saved_bundle import save_to_dir, load_bento_service_metadata
+from bentoml.saved_bundle import save_to_dir
 from bentoml.yatai.status import Status
 
 

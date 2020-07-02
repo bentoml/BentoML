e3b07847e8dc7aede8f41dd0468a0c53f53a55cc
hrmthw <hrmthw.troy@gmail.com>
2020-07-02 18:08:59 +0800

    Stop test docker

--- travis/integration_tests.sh
+++ travis/integration_tests.sh
@@ -19,12 +19,13 @@ python "$GIT_ROOT"/tests/integration/api_server/example_service.py "$TMP_DIR"
 
 cd "$TMP_DIR"
 docker build . -t example_service
-docker run -itd -p 5000:5000 example_service:latest --workers 1 --enable-microbatch
-docker run -itd -p 5001:5000 example_service:latest --workers 1
+docker run -itd --name test_bento_server_mb -p 5000:5000 example_service:latest --workers 1 --enable-microbatch
+docker run -itd --name test_bento_server -p 5001:5000 example_service:latest --workers 1
 sleep 10
 
 # Run test
 python -m pytest --batch-request --host "localhost:5000" "$GIT_ROOT"/tests/integration/api_server
 python -m pytest --host "localhost:5001" "$GIT_ROOT"/tests/integration/api_server
 
+docker container stop test_bento_server_mb test_bento_server
 test $error = 0 # Return non-zero if pytest failed

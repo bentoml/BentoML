4bb1403f29e22dcce9a295effd17ae40c4dd2e73
Aanand Kainth <aanand@akainth.me>
2020-07-06 12:26:02 -0700

    Add a multipart file for use in tests

---
+++ dev/multipart-unit-test-generator.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+	if (req.method !== "POST") return;
+	console.debug(`Delicious! Eating up a request with content-type ${req.headers['content-type']}`);
+	const chunks = [];
+	req.on("data", chunk => {
+		chunks.push(chunk);
+	});
+	req.on("end", () => {
+		bytes = Buffer.concat(chunks);
+		fs.writeFileSync("../tests/multipart", bytes);
+		res.end("Thanks, that was delicious! Your multipart file is located at tests/multipart");
+	});
+});
+console.log("My mouth is open on port 8000, and you can feed me with multipart-unit-test.html");
+server.listen(8000);

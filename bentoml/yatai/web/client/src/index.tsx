72e627fa2cbc1ef443489bfa1056346a22eb55a4
yubozhao <yubz86@gmail.com>
2020-07-14 12:30:54 -0700

    Fix yatai web client import

--- bentoml/yatai/web/client/src/index.tsx
+++ bentoml/yatai/web/client/src/index.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 import { render } from "react-dom";
 import "./index.css";
 
-import { App } from "./App";
+import { App } from "./app";
 
 render(<App />, document.getElementById("root"));

65fd127511cd9385b6fbc2970ec1c99df87e304f
Guy Rapaport <grapaport@apple.com>
2020-07-29 16:04:10 +0300

    Don't install torch[vision] '+cpu' packages on osx

--- travis/coreml_integration_tests.sh
+++ travis/coreml_integration_tests.sh
@@ -14,7 +14,8 @@ export PATH="${HOME}/miniconda/bin:${PATH}"
 hash -r
 
 # Install PyTorch (for training a model) and coremltools (to convert trained PyTorch model to CoreML).
-pip install coremltools==4.0b2 torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+# On Linux we'd might install torch==1.5.0+cpu torchvision==0.6.0+cpu but this runs on Mac.
+pip install coremltools==4.0b2 torch==1.5.0 torchvision==0.6.0 -f https://download.pytorch.org/whl/torch_stable.html
 pytest "$GIT_ROOT"/tests/integration/test_coreml_model_artifact.py --cov=bentoml --cov-config=.coveragerc
 
 test $error = 0 # Return non-zero if pytest failed

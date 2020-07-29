fe7562ff37c4b0d0046225eec62750e0fd7261b5
Guy Rapaport <grapaport@apple.com>
2020-07-29 12:49:54 +0300

    Moved Python installation to before_install script
    https://travis-ci.org/github/bentoml/BentoML/jobs/712856491

--- travis/coreml_integration_tests.sh
+++ travis/coreml_integration_tests.sh
@@ -9,22 +9,6 @@ trap 'error=1' ERR
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd "$GIT_ROOT" || exit
 
-# Install miniconda - taken from https://stackoverflow.com/q/45257534/2064085
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-$MINICONDAVERSION-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r  # https://stackoverflow.com/q/45257534/2064085
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda config --add channels conda-forge
-conda install -y python=3.6  # change base env Python to conda
-# Useful for debugging any issues with conda
-conda info -a
-command -v python
-command -v pip
-python -V
-pip -V
-
 # Install PyTorch (for training a model) and coremltools (to convert trained PyTorch model to CoreML).
 pip install coremltools==4.0b2 torch==1.5.0+cpu torchvision==0.6.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 pytest "$GIT_ROOT"/tests/integration/test_coreml_model_artifact.py --cov=bentoml --cov-config=.coveragerc

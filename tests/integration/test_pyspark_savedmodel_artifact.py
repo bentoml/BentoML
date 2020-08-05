38d69c942d59c07d82993d1b33f0184bd00537c2
Joshua Newton <joshuacwnewton@gmail.com>
2020-08-04 20:31:41 -0700

    Refactor PySpark tests into fixtures

--- tests/integration/test_pyspark_savedmodel_artifact.py
+++ tests/integration/test_pyspark_savedmodel_artifact.py
@@ -1,3 +1,5 @@
+import pytest
+
 import pandas as pd
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.feature import VectorAssembler
@@ -10,7 +12,8 @@ train_pddf = pd.DataFrame([[0, -1.0], [1, 1.0]], columns=["label", "feature1"])
 test_pddf = pd.DataFrame([-5.0, 5.0, -0.5, 0.5], columns=["feature1"])
 
 
-def test_pyspark_model_pack(spark_session, tmpdir):
+@pytest.fixture()
+def pyspark_model(spark_session):
     # Put pandas training df into Spark df form with Vector features
     train_spdf = spark_session.createDataFrame(train_pddf)
     assembler = VectorAssembler(inputCols=['feature1'], outputCol='features')
@@ -20,19 +23,40 @@ def test_pyspark_model_pack(spark_session, tmpdir):
     lr = LogisticRegression()
     lr_model = lr.fit(train_spdf)
 
-    # Test service with packed PySpark model
+    return lr_model
+
+
+@pytest.fixture()
+def pyspark_svc(pyspark_model):
     svc = PysparkClassifier()
-    svc.pack('model', lr_model)
-    output_df = svc.predict(test_pddf)
+    svc.pack('model', pyspark_model)
+
+    return svc
+
+
+@pytest.fixture()
+def pyspark_svc_saved_dir(tmp_path_factory, pyspark_svc):
+    """Save a TensorFlow2 BentoService and return the saved directory."""
+    tmpdir = str(tmp_path_factory.mktemp("pyspark_svc"))
+    pyspark_svc.save_to_dir(tmpdir)
+
+    return tmpdir
+
+
+@pytest.fixture()
+def pyspark_svc_loaded(pyspark_svc_saved_dir):
+    """Return a TensorFlow2 BentoService that has been saved and loaded."""
+    return bentoml.load(pyspark_svc_saved_dir)
+
+
+def test_pyspark_artifact(pyspark_svc):
+    output_df = pyspark_svc.predict(test_pddf)
     assert list(output_df.prediction) == [0.0, 1.0, 0.0, 1.0]
 
-    # Test service that has been saved and loaded
-    saved_dir = svc.save(str(tmpdir))
-    loaded_svc = bentoml.load(saved_dir)
-    output_df = loaded_svc.predict(test_pddf)
-    assert list(output_df.prediction) == [0.0, 1.0, 0.0, 1.0]
 
-    # TODO: Refactor service making into fixtures (like TensorFlow 2.0 tests)
+def test_pyspark_artifact_loaded(pyspark_svc_loaded):
+    output_df = pyspark_svc_loaded.predict(test_pddf)
+    assert list(output_df.prediction) == [0.0, 1.0, 0.0, 1.0]
 
     # TODO: Test PySparkModelArtifact with Docker (like TensorFlow 2.0 tests)
 

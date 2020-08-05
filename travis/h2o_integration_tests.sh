c0d4dd22929183ac3b173a56770a718e6556e33e
yubozhao <yubz86@gmail.com>
2020-08-04 18:04:50 -0700

    remove

--- travis/h2o_integration_tests.sh
+++ travis/h2o_integration_tests.sh
@@ -11,15 +11,6 @@ cd "$GIT_ROOT" || exit
 
 
 # Install required packages for h2o model artifacts test
-#wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-#chmod +x miniconda.sh
-#./miniconda.sh -b
-#export PATH=/home/travis/miniconda/bin:$PATH
-#conda create --yes -n test python=$TRAVIS_PYTHON_VERSION
-#conda config --add channels h2oai
-#source activate test
-#conda install --yes openjdk
-
 pip install h2o
 
 pytest "$GIT_ROOT"/tests/integration/test_h2o_model_artifact.py --cov=bentoml --cov-config=.coveragerc

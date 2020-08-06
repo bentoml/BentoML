4ab5d7daf96eafd5d5b72b00430ee6f991ba1533
Chaoyu <paranoyang@gmail.com>
2020-08-05 19:57:31 -0700

    Update py_module_utils.py

--- bentoml/saved_bundle/py_module_utils.py
+++ bentoml/saved_bundle/py_module_utils.py
@@ -111,7 +111,7 @@ def copy_used_py_modules(target_module, destination):
         )
 
     # Find all modules must be imported for target module to run
-    finder = modulefinder.ModuleFinder()
+    finder = modulefinder.ModuleFinder(excludes=['bentoml'])
     # NOTE: This method could take a few seconds to run
     try:
         logger.debug(
@@ -178,9 +178,7 @@ def copy_used_py_modules(target_module, destination):
     # decorator when creating a new BentoService class
     user_packages_and_modules = {}
     for name, module in finder.modules.items():
-        if name == "bentoml" or name.startswith("bentoml."):
-            pass
-        elif hasattr(module, "__file__") and module.__file__ is not None:
+        if hasattr(module, "__file__") and module.__file__ is not None:
             module_src_file = _get_module_src_file(module)
 
             is_module_in_site_or_dist_package = False

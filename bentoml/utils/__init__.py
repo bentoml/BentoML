931009c2ed2d027b2b57d5f35da86356df49a02d
bojiang <hrmthw@outlook.com>
2020-08-28 08:40:13 +0800

    remove unused code

--- bentoml/utils/__init__.py
+++ bentoml/utils/__init__.py
@@ -14,7 +14,7 @@
 
 import socket
 from contextlib import contextmanager
-from functools import wraps, update_wrapper, singledispatch
+from functools import wraps
 from io import StringIO
 from urllib.parse import urlparse, uses_netloc, uses_params, uses_relative
 
@@ -93,42 +93,3 @@ class catch_exceptions(object):
                 return self.fallback
 
         return _
-
-
-# Descriptor version
-class singledispatchmethod:
-    """***
-    backport from https://github.com/python/cpython/blob/3.8/Lib/functools.py#L888
-    ***
-
-    Single-dispatch generic method descriptor.
-    Supports wrapping existing descriptors and handles non-descriptor
-    callables as instance methods.
-    """
-
-    def __init__(self, func):
-        if not callable(func) and not hasattr(func, "__get__"):
-            raise TypeError(f"{func!r} is not callable or a descriptor")
-
-        self.dispatcher = singledispatch(func)
-        self.func = func
-
-    def register(self, cls, method=None):
-        """generic_method.register(cls, func) -> func
-        Registers a new implementation for the given *cls* on a *generic_method*.
-        """
-        return self.dispatcher.register(cls, func=method)
-
-    def __get__(self, obj, cls=None):
-        def _method(*args, **kwargs):
-            method = self.dispatcher.dispatch(args[0].__class__)
-            return method.__get__(obj, cls)(*args, **kwargs)
-
-        _method.__isabstractmethod__ = self.__isabstractmethod__
-        _method.register = self.register
-        update_wrapper(_method, self.func)
-        return _method
-
-    @property
-    def __isabstractmethod__(self):
-        return getattr(self.func, '__isabstractmethod__', False)

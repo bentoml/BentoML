59b10bf21fb3d31ed90b44086eb63acadcae39c4
yubozhao <yubz86@gmail.com>
2020-08-28 15:57:59 -0700

    build model server for different python version

--- docker/model-server/release.sh
+++ docker/model-server/release.sh
@@ -11,27 +11,34 @@ fi
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd "$GIT_ROOT"/docker/model-server
 
-echo "Releasing debian based docker base image.."
-docker build --pull \
-    --build-arg BENTOML_VERSION="$BENTOML_VERSION" \
-    -t bentoml/model-server:"$BENTOML_VERSION" \
-    .
-docker push bentoml/model-server:"$BENTOML_VERSION"
-
 PYTHON_MAJOR_VERSIONS=(3.6 3.7 3.8)
+
+for version in "${PYTHON_MAJOR_VERSIONS[@]}"
+do
+    echo "Releasing debian based docker base image for Python $version .."
+    docker build --pull \
+        --build-arg BENTOML_VERSION="$BENTOML_VERSION" \
+        --build-arg PYTHON_VERSION="$version" \
+        -t bentoml/model-server:"$BENTOML_VERSION"-py"${version//.}" \
+        .
+
+    docker push bentoml/model-server:"$BENTOML_VERSION"-py"${version//.}"
+
+done
+
 echo "Building slim docker base images for ${PYTHON_MAJOR_VERSIONS[*]}"
 for version in "${PYTHON_MAJOR_VERSIONS[@]}"
 do
     echo "Releasing slim docker base image for Python $version.."
     docker build --pull \
-    --build-arg BENTOML_VERSION=$BENTOML_VERSION \
-    --build-arg PYTHON_VERSION=$version \
-    -t bentoml/model-server:$BENTOML_VERSION-slim-py${version//.} \
+    --build-arg BENTOML_VERSION="$BENTOML_VERSION" \
+    --build-arg PYTHON_VERSION="$version" \
+    -t bentoml/model-server:"$BENTOML_VERSION"-slim-py"${version//.}" \
     -f Dockerfile-slim \
     --network=host \
     .
 
-    docker push bentoml/model-server:$BENTOML_VERSION-slim-py${version//.}
+    docker push bentoml/model-server:"$BENTOML_VERSION"-slim-py"${version//.}"
 
 done
 echo "Done"

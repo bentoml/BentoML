60648760409054a45920ed097509f068ff8d6eb9
bojiang <5886138+bojiang@users.noreply.github.com>
2020-08-31 10:41:44 +0800

    Refactor TfTensorInput/Output (#1048)

--- bentoml/adapters/base_output.py
+++ bentoml/adapters/base_output.py
@@ -12,18 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Iterable, Tuple, Generic
+import itertools
+from typing import Iterable, Sequence, Tuple, Iterator
 
 from bentoml.types import (
     AwsLambdaEvent,
     HTTPResponse,
     ApiFuncReturnValue,
+    InferenceTask,
     InferenceResult,
-    InferenceContext,
+    Output,
 )
 
 
-class BaseOutputAdapter(Generic[ApiFuncReturnValue]):
+def regroup_return_value(
+    return_values: Iterable, tasks: Sequence[InferenceTask]
+) -> Iterator[Tuple[Output, InferenceTask]]:
+    iter_rv = iter(return_values)
+    for task in tasks:
+        if task.batch is None:
+            yield next(iter_rv), task
+        else:
+            yield tuple(itertools.islice(iter_rv, task.batch)), task
+
+
+class BaseOutputAdapter:
     """
     OutputAdapter is an layer between result of user defined API callback function
     and final output in a variety of different forms,
@@ -45,8 +58,8 @@ class BaseOutputAdapter(Generic[ApiFuncReturnValue]):
         return []
 
     def pack_user_func_return_value(
-        self, return_result: ApiFuncReturnValue, contexts: Tuple[InferenceContext],
-    ) -> Tuple[InferenceResult]:
+        self, return_result: ApiFuncReturnValue, tasks: Sequence[InferenceTask],
+    ) -> Sequence[InferenceResult]:
         """
         Pack the return value of user defined API function into InferenceResults
         """

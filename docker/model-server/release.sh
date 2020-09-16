748dcb43dd6ea2c65fb259636bcb3e2634d2abf9
Greg D <gdd314596@gmail.com>
2020-09-16 23:57:13 +0000

    release will now build conda versions for multiple python versions

--- docker/model-server/release.sh
+++ docker/model-server/release.sh
@@ -11,17 +11,34 @@ fi
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd "$GIT_ROOT"/docker/model-server
 
-echo "Releasing debian based docker base image.."
-docker build --pull \
-    --build-arg BENTOML_VERSION="$BENTOML_VERSION" \
-    -t bentoml/model-server:"$BENTOML_VERSION" \
-    -t bentoml/model-server:latest \
-    .
-docker push bentoml/model-server:"$BENTOML_VERSION"
+PYTHON_MAJOR_VERSIONS=(3.6 3.7 3.8)
+#which version to tag with latest tag
+PYTHON_LATEST_VERSION=3.7
+
+
+echo "Building debian based docker base images for ${PYTHON_MAJOR_VERSIONS[*]}"
+for version in "${PYTHON_MAJOR_VERSIONS[@]}"
+do
+    echo "Releasing debian based docker base image for Python $version .."
+    docker build --pull \
+        --build-arg BENTOML_VERSION="$BENTOML_VERSION" \
+        --build-arg PYTHON_VERSION="$version" \
+        -t bentoml/model-server:"$BENTOML_VERSION"-py"${version//.}" \
+        -t bentoml/model-server:latest-py"${version//.}" \
+        .
+
+    docker push bentoml/model-server:"$BENTOML_VERSION"-py"${version//.}"
+    docker push bentoml/model-server:latest-py"${version//.}"
+
+done
+
+# tag the default version as both latest and unspecified python version
+docker tag bentoml/model-server:latest-py${PYTHON_LATEST_VERSION//.} bentoml/model-server:latest
 docker push bentoml/model-server:latest
 
+docker tag bentoml/model-server:$BENTOML_VERSION-py${PYTHON_LATEST_VERSION//.} bentoml/model-server:$BENTOML_VERSION
+docker push bentoml/model-server:$BENTOML_VERSION
 
-PYTHON_MAJOR_VERSIONS=(3.6 3.7 3.8)
 echo "Building slim docker base images for ${PYTHON_MAJOR_VERSIONS[*]}"
 for version in "${PYTHON_MAJOR_VERSIONS[@]}"
 do

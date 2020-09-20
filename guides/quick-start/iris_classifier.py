9e205abcfee8131e9cb44c06496fb04730826e9f
Chaoyu <paranoyang@gmail.com>
2020-09-19 22:52:23 -0700

    Update iris_classifier.py

--- guides/quick-start/iris_classifier.py
+++ guides/quick-start/iris_classifier.py
@@ -1,14 +1,21 @@
 import pandas as pd
 
-from bentoml import BentoService, api, artifacts, env
+from bentoml import env, artifacts, api, BentoService
 from bentoml.adapters import DataframeInput
 from bentoml.frameworks.sklearn import SklearnModelArtifact
-
 
 @env(infer_pip_packages=True)
 @artifacts([SklearnModelArtifact('model')])
 class IrisClassifier(BentoService):
+    """
+    A minimum prediction service exposing a Scikit-learn model
+    """
+
     @api(input=DataframeInput(), batch=True)
     def predict(self, df: pd.DataFrame):
-        # Optional pre-processing, post-processing code goes here
+        """
+        An inference API named `predict` with Dataframe input adapter, which codifies
+        how HTTP requests or CSV files are converted to a pandas Dataframe object as the
+        inference API function input
+        """
         return self.artifacts.model.predict(df)

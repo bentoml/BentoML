cebf201505b0c8a231ce43d3f32a1d38161f7940
yubozhao <yubz86@gmail.com>
2020-09-21 20:17:47 -0700

    Update sample_bento_service.py

--- e2e_tests/sample_bento_service.py
+++ e2e_tests/sample_bento_service.py
@@ -7,14 +7,14 @@ logger = logging.getLogger('bentoml.test')
 
 
 class SampleBentoService(BentoService):
-    @api(input=JsonInput(), batch=True)
+    @api(input=JsonInput(), batch=False)
     def predict(self, data):
         logger.info(f"SampleBentoService predict API received data {data}")
-        return ['cat']
+        return 'cat'
 
 
 class UpdatedSampleBentoService(BentoService):
-    @api(input=JsonInput(), batch=True)
+    @api(input=JsonInput(), batch=False)
     def predict(self, data):
         logger.info(f"UpdatedSampleBentoService predict API received data {data}")
-        return ['dog']
+        return 'dog'

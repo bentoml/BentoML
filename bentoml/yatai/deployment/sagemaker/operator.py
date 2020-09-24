c43769bb97ce865c7bdb0278755a85bc4d475a4b
Bozhao <yubz86@gmail.com>
2020-09-24 16:36:47 -0700

    update sagemaker dependency version (#1131)

--- bentoml/yatai/deployment/sagemaker/operator.py
+++ bentoml/yatai/deployment/sagemaker/operator.py
@@ -49,7 +49,7 @@ RUN apt-get update --fix-missing && \
     apt-get clean
 
 # gevent required by AWS Sagemaker
-RUN pip install gevent==1.4
+RUN pip install gevent>=20.9.0
 
 # copy over model files
 COPY . /bento

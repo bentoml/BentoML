bb78079665161c78275cec96667f8493277f4053
bojiang <5886138+bojiang@users.noreply.github.com>
2020-09-25 01:17:00 +0800

    [DOC] refine docstrings of DataframeInput and JsonInput (#1129)
    * [DOC] refine DataframeInput docs
    
    * fix style
    
    * fix title
    
    * fix: break line
    
    * fix: longline
    
    * [DOC] refine JsonInput docstring
    
    * fix format
    
    * fix 2
    
    * make pylint happy

--- bentoml/adapters/dataframe_input.py
+++ bentoml/adapters/dataframe_input.py
@@ -83,8 +83,60 @@ class DataframeInput(StringInput):
 
     Raises
     -------
-        ValueError: Incoming data is missing required columns in dtype
-        ValueError: Incoming data format can not be handled. Only json and csv
+    ValueError: Incoming data is missing required columns in dtype
+
+    ValueError: Incoming data format can not be handled. Only json and csv
+
+    Examples
+    -------
+    Example Service:
+
+    .. code-block:: python
+
+        from bentoml import env, artifacts, api, BentoService
+        from bentoml.adapters import DataframeInput
+        from bentoml.frameworks.sklearn import SklearnModelArtifact
+
+        @env(infer_pip_packages=True)
+        @artifacts([SklearnModelArtifact('model')])
+        class IrisClassifier(BentoService):
+
+            @api(
+                input=DataframeInput(
+                    orient="records",
+                    columns=["sw", "sl", "pw", "pl"],
+                    dtype={"sw": "float", "sl": "float", "pw": "float", "pl": "float"},
+                ),
+                batch=True,
+            )
+            def predict(self, df):
+                # Optional pre-processing, post-processing code goes here
+                return self.artifacts.model.predict(df)
+
+    Query with HTTP request::
+
+        curl -i \\
+          --header "Content-Type: application/json" \\
+          --request POST \\
+          --data '[{"sw": 1, "sl": 2, "pw": 1, "pl": 2}]' \\
+          localhost:5000/predict
+
+    OR::
+
+        curl -i \\
+          --header "Content-Type: text/csv" \\
+          --request POST \\
+          --data @file.csv \\
+          localhost:5000/predict
+
+    Query with CLI command::
+
+        bentoml run IrisClassifier:latest predict --input \\
+          '[{"sw": 1, "sl": 2, "pw": 1, "pl": 2}]'
+
+    OR::
+
+        bentoml run IrisClassifier:latest predict --format csv --input-file test.csv
 
     """
 

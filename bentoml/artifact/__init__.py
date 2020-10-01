6cdf0133c22d6c13659de5799bf51a84b407f277
yubozhao <yubz86@gmail.com>
2020-10-01 15:04:02 -0700

    formatting

--- bentoml/artifact/__init__.py
+++ bentoml/artifact/__init__.py
@@ -20,9 +20,9 @@ from bentoml.service.artifacts.common import TextFileArtifact
 from bentoml.service.artifacts.common import JSONArtifact
 from bentoml.service.artifacts.common import PickleArtifact
 
-from bentoml.frameworks.coreml import CoreMLModelArtifact
-from bentoml.frameworks.fastai import Fastai1ModelArtifact as FastaiModelArtifact
 from bentoml.frameworks.fastai import FastaiModelArtifact as Fastai2ModelArtifact
+from bentoml.frameworks.fastai import Fastai1ModelArtifact as FastaiModelArtifact
+from bentoml.frameworks.coreml import CoreMLModelArtifact
 from bentoml.frameworks.fasttext import FasttextModelArtifact
 from bentoml.frameworks.h2o import H2oModelArtifact
 from bentoml.frameworks.keras import KerasModelArtifact

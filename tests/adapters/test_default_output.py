555f414def712788874236f83f045176e010aaf7
yubozhao <yubz86@gmail.com>
2020-10-05 14:48:27 -0700

    linting

--- tests/adapters/test_default_output.py
+++ tests/adapters/test_default_output.py
@@ -12,12 +12,12 @@ except ImportError:
     from mock import MagicMock
 
 
-@pytest.fixture()
-def api_returns_inference_result(make_api):
+@pytest.fixture(name='inference_result_api')
+def inference_result_api(make_api):
     def test_func_inference_result(df):
         return [
             InferenceResult(
-                data=json.dumps({'name': 'john'}),
+                data=json.dumps({'name': df['name'][0]}),
                 http_status=200,
                 http_headers={
                     'Content-Type': 'application/json',
@@ -30,37 +30,37 @@ def api_returns_inference_result(make_api):
     return api
 
 
-def test_default_output_http_requests(api_returns_inference_result):
+def test_default_output_http_requests(inference_result_api):
     csv_data = b'name,game,city\njohn,mario,sf'
     request = MagicMock(spec=flask.Request)
     request.headers = {'Content-Type': 'text/csv'}
     request.get_data.return_value = csv_data
 
-    result = api_returns_inference_result.handle_request(request)
+    result = inference_result_api.handle_request(request)
     assert result.get_data().decode('utf-8') == '{"name": "john"}'
     assert result.headers['Content-Type'] == 'application/json'
     assert result.headers['Access-Control-Allow-Origin'] == '*'
 
 
-def test_default_output_cli(capsys, api_returns_inference_result, tmpdir):
+def test_default_output_cli(capsys, inference_result_api, tmpdir):
     json_file = tmpdir.join("test.csv")
     with open(str(json_file), "w") as f:
         f.write('name,game,city\njohn,mario,sf')
 
     test_args = ["--input-file", str(json_file), "--format", "csv"]
-    api_returns_inference_result.handle_cli(test_args)
+    inference_result_api.handle_cli(test_args)
     out, _ = capsys.readouterr()
     assert "john" in out
 
 
-def test_default_output_aws_lambda(api_returns_inference_result):
+def test_default_output_aws_lambda(inference_result_api):
     test_content = '[{"name": "john","game": "mario","city": "sf"}]'
 
     event_without_content_type_header = {
         "headers": {},
         "body": test_content,
     }
-    response = api_returns_inference_result.handle_aws_lambda_event(
+    response = inference_result_api.handle_aws_lambda_event(
         event_without_content_type_header
     )
     assert response["statusCode"] == 200

8f68ac7e0a081955187dd2eccc4f0928d5e919ee
yubozhao <yubz86@gmail.com>
2020-10-21 17:17:55 -0700

    update

--- bentoml/cli/bento_service.py
+++ bentoml/cli/bento_service.py
@@ -76,9 +76,8 @@ def create_bento_service_cli(pip_installed_bundle_path=None):
     @click.argument('run_args', nargs=-1, type=click.UNPROCESSED)
     def run(api_name, run_args, bento=None):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--yatai-uri', type=str, default=None)
+        parser.add_argument('--yatai-url', type=str, default=None)
         parsed_args, _ = parser.parse_known_args(run_args)
-
         yatai_url = parsed_args.yatai_url
         saved_bundle_path = resolve_bundle_path(
             bento, pip_installed_bundle_path, yatai_url

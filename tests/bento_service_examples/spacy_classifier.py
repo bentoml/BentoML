2ae04b344fb3e62361586a97d1a48666c2592755
Vignesh S <45061877+telescopic@users.noreply.github.com>
2020-11-12 01:29:26 +0530

    Add integration test for spaCy (#1236)
    * Add SpacyModelService class
    
    * Add integration test for spaCy model artifact
    
    * Add .sh file for spaCy integration test

---
+++ tests/bento_service_examples/spacy_classifier.py
@@ -0,0 +1,12 @@
+import bentoml
+from bentoml.adapters import JsonInput
+from bentoml.frameworks.spacy import SpacyModelArtifact
+
+
+@bentoml.env(infer_pip_packages=True)
+@bentoml.artifacts([SpacyModelArtifact('nlp')])
+class SpacyModelService(bentoml.BentoService):
+    @bentoml.api(input=JsonInput(), batch=False)
+    def predict(self, parsed_json):
+        output = self.artifacts.nlp(parsed_json['text'])
+        return output

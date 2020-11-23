4bc2efe6e9fc6c9ce006aa86197d87f9e3981a22
yubozhao <yubz86@gmail.com>
2020-11-23 14:31:30 -0800

    update postgres test

--- tests/integration/yatai_server/test_postgres_local_fs.py
+++ tests/integration/yatai_server/test_postgres_local_fs.py
@@ -3,15 +3,18 @@ import logging
 from bentoml.yatai.client import get_yatai_client
 from bentoml.yatai.proto.repository_pb2 import BentoUri
 from tests.bento_service_examples.example_bento_service import ExampleBentoService
-from tests.yatai.local_yatai_service import local_yatai_service_container
+from tests.yatai.local_yatai_service import local_yatai_service_from_cli
 
 logger = logging.getLogger('bentoml.test')
 
 
 def test_yatai_server_with_postgres_and_local_storage():
-    postgres_db_url = 'postgresql://postgres:postgres@localhost:5432'
+    postgres_db_url = 'postgresql://postgres:postgres@localhost/bentoml:5432'
+    from sqlalchemy_utils import create_database
 
-    with local_yatai_service_container(db_url=postgres_db_url) as yatai_server_url:
+    create_database(postgres_db_url)
+
+    with local_yatai_service_from_cli(db_url=postgres_db_url) as yatai_server_url:
         logger.info('Saving bento service')
         logger.info(f'yatai url is {yatai_server_url}')
         yc = get_yatai_client(yatai_server_url)

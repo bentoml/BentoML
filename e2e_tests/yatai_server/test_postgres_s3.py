5963c6b8c605e9d5579885d5e7c846121ec2cfda
yubozhao <yubz86@gmail.com>
2020-11-22 17:01:44 -0800

    update e2e yatai tests

--- e2e_tests/yatai_server/test_postgres_s3.py
+++ e2e_tests/yatai_server/test_postgres_s3.py
@@ -29,10 +29,10 @@ def test_yatai_server_with_postgres_and_s3(postgres_db_container_url):
         logger.info('BentoService saved')
 
         logger.info("Display bentoservice info")
-        get_svc_result = get_bento_service_info(svc.name, svc.version)
-        logger.info(get_svc_result)
+        bento = get_bento_service_info(svc.name, svc.version)
+        logger.info(bento)
         assert (
-            get_svc_result.bento.uri.type == BentoUri.S3
+            bento.uri.type == BentoUri.S3
         ), 'BentoService storage type mismatched, expect S3'
 
         logger.info('Validate BentoService prediction result')

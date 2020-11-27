a9751c535699c45194a6f8ce571374ef3f8959be
bojiang <hrmthw@outlook.com>
2020-11-27 16:12:58 +0800

    [CI]add integration test for tf1

--- tests/bento_service_examples/tensorflow_classifier.py
+++ tests/bento_service_examples/tensorflow_classifier.py
@@ -1,6 +1,13 @@
+import sys
+
 import bentoml
 from bentoml.adapters import TfTensorInput
 from bentoml.frameworks.tensorflow import TensorflowSavedModelArtifact
+
+if "tensorflow" not in sys.modules:
+    import tensorflow
+
+    tensorflow.enable_eager_execution()
 
 
 @bentoml.env(infer_pip_packages=True)
@@ -9,3 +16,16 @@ class Tensorflow2Classifier(bentoml.BentoService):
     @bentoml.api(input=TfTensorInput(), batch=True)
     def predict(self, tensor):
         return self.artifacts.model(tensor)
+
+
+@bentoml.env(infer_pip_packages=True)
+@bentoml.artifacts([TensorflowSavedModelArtifact('model')])
+class Tensorflow1Classifier(bentoml.BentoService):
+    @bentoml.api(input=TfTensorInput(), batch=True)
+    def predict(self, tensor):
+        import tensorflow as tf
+
+        tf.enable_eager_execution()
+
+        pred_func = self.artifacts.model.signatures['serving_default']
+        return pred_func(tensor)['prediction']

cb4249f7f4ccac90e794c1b78fd2c324985900de
yubozhao <yubz86@gmail.com>
2020-12-03 16:39:38 -0800

    Update test_local_fs.py

--- tests/integration/yatai_server/test_local_fs.py
+++ tests/integration/yatai_server/test_local_fs.py
@@ -12,32 +12,13 @@ from tests.yatai.local_yatai_service import (
 logger = logging.getLogger('bentoml.test')
 
 
-def test_sqlite_and_local_fs():
-    with local_yatai_service_container() as yatai_server_url:
-        yc = get_yatai_client(yatai_server_url)
-        svc = ExampleBentoService()
-        svc.pack('model', [1, 2, 3])
-        bento_tag = f'{svc.name}:{svc.version}'
-        logger.info(f'Saving BentoML saved bundle {bento_tag}')
-        svc.save(yatai_url=yatai_server_url)
-
-        bento_pb = yc.repository.get(bento_tag)
-        assert (
-            bento_pb.uri.type == BentoUri.LOCAL
-        ), 'BentoService storage type mismatched, expect LOCAL'
-
-        logger.info(f'Deleting saved bundle {bento_tag}')
-        delete_svc_result = yc.repository.delete(bento_tag)
-        assert delete_svc_result is None
-
-
 def test_yatai_server_with_postgres_and_local_storage():
     postgres_db_url = 'postgresql://postgres:postgres@localhost/bentoml:5432'
 
     from sqlalchemy_utils import create_database
 
     create_database(postgres_db_url)
-    time.sleep(120)
+    time.sleep(90)
 
     with local_yatai_service_from_cli(db_url=postgres_db_url) as yatai_server_url:
         logger.info('Saving bento service')
@@ -53,3 +34,22 @@ def test_yatai_server_with_postgres_and_local_storage():
         assert (
             bento_pb.uri.type == BentoUri.LOCAL
         ), 'BentoService storage type mismatched, expect LOCAL'
+
+
+def test_sqlite_and_local_fs():
+    with local_yatai_service_container() as yatai_server_url:
+        yc = get_yatai_client(yatai_server_url)
+        svc = ExampleBentoService()
+        svc.pack('model', [1, 2, 3])
+        bento_tag = f'{svc.name}:{svc.version}'
+        logger.info(f'Saving BentoML saved bundle {bento_tag}')
+        svc.save(yatai_url=yatai_server_url)
+
+        bento_pb = yc.repository.get(bento_tag)
+        assert (
+                bento_pb.uri.type == BentoUri.LOCAL
+        ), 'BentoService storage type mismatched, expect LOCAL'
+
+        logger.info(f'Deleting saved bundle {bento_tag}')
+        delete_svc_result = yc.repository.delete(bento_tag)
+        assert delete_svc_result is None

5b11f25efaba080f7c2fc2909ca49ef600a3655f
bojiang <hrmthw@outlook.com>
2020-12-05 14:55:34 +0800

    [CI] loose benchmark test for microbatch dispatcher

--- tests/integration/api_server/test_microbatch.py
+++ tests/integration/api_server/test_microbatch.py
@@ -1,9 +1,9 @@
 # pylint: disable=redefined-outer-name
 
-import time
-import pytest
 import asyncio
+import time
 
+import pytest
 
 DEFAULT_MAX_LATENCY = 10 * 1000
 
@@ -52,11 +52,11 @@ async def test_fast_server(host):
     if not pytest.enable_microbatch:
         pytest.skip()
 
-    A, B = 0.002, 0.01
+    A, B = 0.0002, 0.01
     data = '{"a": %s, "b": %s}' % (A, B)
 
     time_start = time.time()
-    req_count = 100
+    req_count = 500
     tasks = tuple(
         pytest.assert_request(
             "POST",
@@ -70,4 +70,4 @@ async def test_fast_server(host):
         for i in range(req_count)
     )
     await asyncio.gather(*tasks)
-    assert time.time() - time_start < 1
+    assert time.time() - time_start < 5

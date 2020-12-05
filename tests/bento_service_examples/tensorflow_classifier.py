da57ca447ae9398c163ca7b4696d6b9d8a7c683f
bojiang <hrmthw@outlook.com>
2020-12-05 11:40:59 +0800

    [CI] add integration tests for ragged_tensor

--- tests/bento_service_examples/tensorflow_classifier.py
+++ tests/bento_service_examples/tensorflow_classifier.py
@@ -1,5 +1,5 @@
 import bentoml
-from bentoml.adapters import TfTensorInput
+from bentoml.adapters import JsonInput, TfTensorInput
 from bentoml.frameworks.tensorflow import TensorflowSavedModelArtifact
 
 
@@ -7,5 +7,16 @@ from bentoml.frameworks.tensorflow import TensorflowSavedModelArtifact
 @bentoml.artifacts([TensorflowSavedModelArtifact('model')])
 class Tensorflow2Classifier(bentoml.BentoService):
     @bentoml.api(input=TfTensorInput(), batch=True)
-    def predict(self, tensor):
-        return self.artifacts.model(tensor)
+    def predict1(self, tensor):
+        return self.artifacts.model1(tensor)
+
+    @bentoml.api(input=TfTensorInput(), batch=True)
+    def predict2(self, tensor):
+        return self.artifacts.model2(tensor)
+
+    @bentoml.api(input=JsonInput(), batch=True)
+    def predict3(self, jsons):
+        import tensorflow as tf
+
+        tensor = tf.ragged.constant(jsons, dtype=tf.float64)
+        return self.artifacts.model3(tensor)

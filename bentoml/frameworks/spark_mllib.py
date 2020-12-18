455f1b73392dfb5e8dd40d30ce34f8e2ff2b995d
Aanand Kainth <aanand@akainth.me>
2020-12-18 03:08:06 -0800

    More thoroughly tested edition

--- bentoml/frameworks/spark_mllib.py
+++ bentoml/frameworks/spark_mllib.py
@@ -8,7 +8,7 @@ from typing import Optional
 from bentoml.exceptions import MissingDependencyException, BentoMLException
 from bentoml.service import BentoServiceArtifact
 
-if typing.TYPE_CHECKING:
+if typing.TYPE_CHECKING:  # note: requires Python 3.7
     import pyspark
     from pyspark.ml.base import Model
 
@@ -47,24 +47,23 @@ class PySparkModelArtifact(BentoServiceArtifact):
         from pyspark.sql import SparkSession
         spark = SparkSession.builder.appName('BentoService').getOrCreate()
 
-        model_data_path = os.path.join(path, "pyspark_model_data")
-        metadata_path = os.path.join(path, "metadata.json")
+        model_data_path = os.path.join(path, self.name)
+        metadata_path = os.path.join(path, self.name, "metadata/part-00000")
 
         with open(metadata_path, "r") as metadata:
             metadata = json.load(metadata)
-        if "model_class" not in metadata:
+        if "class" not in metadata:
             raise BentoMLException("Malformed metadata file.")
-        model_class = metadata["model_class"]
+        model_class = metadata["class"]
 
         logger = logging.getLogger(self.name)
         logger.info("Loading %s model from %s" % (model_class, model_data_path))
-        splits = model_class.split(".")
-        module = ".".join(splits[:-1])
+        splits = model_class.split(".")[2:]  # skip the org.apache
+        module = "py" + ".".join(splits[:-1])
         class_name = splits[-1]
+        # noinspection PyPep8Naming
         ModelClass = getattr(importlib.import_module(module), class_name)
-        if issubclass(ModelClass,
-                      pyspark.ml.pipeline.PipelineModel) or issubclass(
-            ModelClass, pyspark.ml.base.Model):
+        if issubclass(ModelClass, pyspark.ml.base.Model):
             model = ModelClass.load(model_data_path)
         else:
             model = ModelClass.load(spark.sparkContext, model_data_path)

d6f4a249b87ae85badd396ddc36e5df8696b6a3a
bojiang <hrmthw@outlook.com>
2021-02-06 17:05:11 +0800

    [CI] clean old tests

--- tests/integration/api_server/test_meta.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=redefined-outer-name
-import pytest
-
-
-@pytest.mark.asyncio
-async def test_api_server_meta(host):
-    await pytest.assert_request("GET", f"http://{host}/")
-    await pytest.assert_request("GET", f"http://{host}/healthz")
-    await pytest.assert_request("GET", f"http://{host}/docs.json")

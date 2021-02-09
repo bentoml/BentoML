ded763ee7fad8340ab487790c3d381aeda233499
Sean Sheng <ssheng@linkedin.com>
2021-02-09 12:16:19 -0800

    [Enhancement] Introduce Dependency Injection and Configuration Revamp (#1374) (#1422)
    * [Feature] BentoML Configuration Revamp
      - Introduced dependency injection
      - Introduced new configuration schema and validation
      - Standardized configuration override
      - Migraded `bentoml serve` and `bentoml serve-gunicorn` as proof of concept
    
    * Add debug print statement for troubleshoot
    
    * Fix a bug caught in the integration test.
    
    * Track error message in click utils.
    
    * Add stacktrace to click utils tracking.
    
    * Change wiring to be based on modules instead of packages.
    
    * Fix linter issues.
    
    * Disable test_track_cli_with_keyboard_interrupt test on Windows.
    
    * Fix test_track_cli_with_keyboard_interrupt test.

---
+++ bentoml/configuration/containers.py
@@ -0,0 +1,172 @@
+# Copyright 2020 Atalaya Tech, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import multiprocessing
+import os
+
+from deepmerge import always_merger
+from dependency_injector import containers, providers
+from schema import Schema, SchemaError, And, Or
+
+from bentoml.configuration import config
+from bentoml.exceptions import BentoMLConfigException
+from bentoml.utils.ruamel_yaml import YAML
+
+LOGGER = logging.getLogger(__name__)
+
+SCHEMA = Schema(
+    {
+        "api_server": {
+            "port": And(int, lambda port: port > 0),
+            "enable_microbatch": bool,
+            "run_with_ngrok": bool,
+            "enable_swagger": bool,
+            "enable_metrics": bool,
+            "enable_feedback": bool,
+            "max_request_size": And(int, lambda size: size > 0),
+            "workers": Or(And(int, lambda workers: workers > 0), None),
+            "timeout": And(int, lambda timeout: timeout > 0),
+        },
+        "marshal_server": {
+            "max_batch_size": And(int, lambda size: size > 0),
+            "max_latency": And(int, lambda latency: latency > 0),
+            "workers": Or(And(int, lambda workers: workers > 0), None),
+            "request_header_flag": str,
+        },
+        "yatai": {"url": Or(str, None)},
+        "tracing": {"zipkin_api_url": Or(str, None)},
+        "instrument": {"namespace": str},
+    }
+)
+
+
+class BentoMLConfiguration:
+    def __init__(
+        self,
+        default_config_file: str = None,
+        override_config_file: str = None,
+        validate_schema: bool = True,
+        legacy_compatibility: bool = True,
+    ):
+        # Default configuraiton
+        if default_config_file is None:
+            default_config_file = os.path.join(
+                os.path.dirname(__file__), "default_bentoml.yml"
+            )
+
+        with open(default_config_file, "rb") as f:
+            self.config = YAML().load(f.read())
+
+        if validate_schema:
+            try:
+                SCHEMA.validate(self.config)
+            except SchemaError as e:
+                raise BentoMLConfigException(
+                    "Default configuration 'default_bentoml.yml' does not"
+                    " conform to the required schema."
+                ) from e
+
+        # Legacy configuration compatibility
+        if legacy_compatibility:
+            try:
+                self.config["api_server"]["port"] = config("apiserver").getint(
+                    "default_port"
+                )
+                self.config["api_server"]["max_request_size"] = config(
+                    "apiserver"
+                ).getint("default_max_request_size")
+                self.config["marshal_server"]["max_batch_size"] = config(
+                    "marshal_server"
+                ).getint("default_max_batch_size")
+                self.config["marshal_server"]["max_latency"] = config(
+                    "marshal_server"
+                ).getint("default_max_latency")
+                self.config["marshal_server"]["request_header_flag"] = config(
+                    "marshal_server"
+                ).get("marshal_request_header_flag")
+                self.config["yatai"]["url"] = config("yatai_service").get("url")
+                self.config["tracing"]["zipkin_api_url"] = config("tracing").get(
+                    "zipkin_api_url"
+                )
+                self.config["instrument"]["namespace"] = config("instrument").get(
+                    "default_namespace"
+                )
+            except KeyError as e:
+                raise BentoMLConfigException(
+                    "Overriding a non-existent configuration key in compatibility mode."
+                ) from e
+
+            if validate_schema:
+                try:
+                    SCHEMA.validate(self.config)
+                except SchemaError as e:
+                    raise BentoMLConfigException(
+                        "Configuration after applying legacy compatibility"
+                        " does not conform to the required schema."
+                    ) from e
+
+        # User override configuration
+        if override_config_file is not None and os.path.exists(override_config_file):
+            LOGGER.info("Applying user config override from %s" % override_config_file)
+            with open(override_config_file, "rb") as f:
+                override_config = YAML().load(f.read())
+            always_merger.merge(self.config, override_config)
+
+            if validate_schema:
+                try:
+                    SCHEMA.validate(self.config)
+                except SchemaError as e:
+                    raise BentoMLConfigException(
+                        "Configuration after user override does not conform to"
+                        " the required schema."
+                    ) from e
+
+    def override(self, keys: list, value):
+        if keys is None:
+            raise BentoMLConfigException("Configuration override key is None.")
+        if len(keys) == 0:
+            raise BentoMLConfigException("Configuration override key is empty.")
+        if value is None:
+            return
+
+        c = self.config
+        for key in keys[:-1]:
+            if key not in c:
+                raise BentoMLConfigException(
+                    "Configuration override key is invalid, %s" % keys
+                )
+            c = c[key]
+        c[keys[-1]] = value
+
+        try:
+            SCHEMA.validate(self.config)
+        except SchemaError as e:
+            raise BentoMLConfigException(
+                "Configuration after applying override does not conform"
+                " to the required schema, key=%s, value=%s." % (keys, value)
+            ) from e
+
+    def as_dict(self) -> dict:
+        return self.config
+
+
+class BentoMLContainer(containers.DeclarativeContainer):
+
+    config = providers.Configuration(strict=True)
+
+    api_server_workers = providers.Callable(
+        lambda workers: workers if workers else (multiprocessing.cpu_count() // 2) + 1,
+        config.api_server.workers,
+    )
---
+++ tests/configuration/test_containers.py
@@ -0,0 +1,200 @@
+import pytest
+import tempfile
+import os
+
+from bentoml.configuration.containers import BentoMLConfiguration, BentoMLContainer
+from bentoml.exceptions import BentoMLConfigException
+
+
+def test_override():
+    config = BentoMLConfiguration(legacy_compatibility=False)
+    config.override(["api_server", "port"], 6000)
+    config_dict = config.as_dict()
+    assert config_dict is not None
+    assert config_dict["api_server"]["port"] == 6000
+
+
+def test_override_schema_violation():
+    config = BentoMLConfiguration()
+    with pytest.raises(BentoMLConfigException) as e:
+        config.override(["api_server", "port"], "non-integer")
+    assert e is not None
+
+
+def test_override_nonexist_key():
+    config = BentoMLConfiguration()
+    with pytest.raises(BentoMLConfigException) as e:
+        config.override(["non-existent", "non-existent"], 6000)
+    assert e is not None
+
+
+def test_override_none_value():
+    config = BentoMLConfiguration(legacy_compatibility=False)
+    config.override(["api_server", "port"], None)
+    config_dict = config.as_dict()
+    assert config_dict is not None
+    assert config_dict["api_server"]["port"] == 5000
+
+
+def test_override_none_key():
+    config = BentoMLConfiguration(legacy_compatibility=False)
+    with pytest.raises(BentoMLConfigException) as e:
+        config.override(None, 6000)
+    assert e is not None
+
+
+def test_override_empty_key():
+    config = BentoMLConfiguration(legacy_compatibility=False)
+    with pytest.raises(BentoMLConfigException) as e:
+        config.override([], 6000)
+    assert e is not None
+
+
+def test_legacy_compatibiltiy():
+    config = tempfile.NamedTemporaryFile(delete=False)
+    config.write(
+        b"""
+api_server:
+  port: 0
+  max_request_size: 0
+marshal_server:
+  max_batch_size: 0
+  max_latency: 0
+  request_header_flag: Null
+yatai:
+  url: Null
+tracing:
+  zipkin_api_url: Null
+instrument:
+  namespace: Null
+"""
+    )
+    config.close()
+
+    config_dict = BentoMLConfiguration(
+        default_config_file=config.name,
+        legacy_compatibility=True,
+        validate_schema=False,
+    ).as_dict()
+    os.remove(config.name)
+    assert config_dict is not None
+    assert config_dict["api_server"]["port"] == 5000
+    assert config_dict["api_server"]["max_request_size"] == 20971520
+    assert config_dict["marshal_server"]["max_batch_size"] == 2000
+    assert config_dict["marshal_server"]["max_latency"] == 10000
+    assert config_dict["marshal_server"]["request_header_flag"] == (
+        "BentoML-Is-Merged-Request"
+    )
+    assert config_dict["yatai"]["url"] == ""
+    assert config_dict["tracing"]["zipkin_api_url"] == ""
+    assert config_dict["instrument"]["namespace"] == "BENTOML"
+
+
+def test_validate_schema():
+    config = tempfile.NamedTemporaryFile(delete=False)
+    config.write(
+        b"""
+invalid_key1:
+  invalid_key2: Null
+"""
+    )
+    config.close()
+
+    with pytest.raises(BentoMLConfigException) as e:
+        BentoMLConfiguration(
+            default_config_file=config.name,
+            validate_schema=True,
+            legacy_compatibility=False,
+        )
+
+    assert e is not None
+    os.remove(config.name)
+
+
+def test_api_server_workers():
+    container = BentoMLContainer()
+
+    config_auto_workers = tempfile.NamedTemporaryFile(delete=False)
+    config_auto_workers.write(
+        b"""
+api_server:
+  workers: Null
+"""
+    )
+    config_auto_workers.close()
+
+    container.config.from_dict(
+        BentoMLConfiguration(
+            default_config_file=config_auto_workers.name,
+            validate_schema=False,
+            legacy_compatibility=False,
+        ).as_dict(),
+    )
+    os.remove(config_auto_workers.name)
+    workers = container.api_server_workers()
+    assert workers is not None
+    assert workers > 0
+
+    config_manual_workers = tempfile.NamedTemporaryFile(delete=False)
+    config_manual_workers.write(
+        b"""
+api_server:
+  workers: 42
+"""
+    )
+    config_manual_workers.close()
+
+    container.config.from_dict(
+        BentoMLConfiguration(
+            default_config_file=config_manual_workers.name,
+            validate_schema=False,
+            legacy_compatibility=False,
+        ).as_dict(),
+    )
+    os.remove(config_manual_workers.name)
+    workers = container.api_server_workers()
+    assert workers is not None
+    assert workers == 42
+
+
+def test_config_file_override():
+    default_config_file = tempfile.NamedTemporaryFile(delete=False)
+    default_config_file.write(
+        b"""
+key1:
+  key2:
+    key3: value3
+    key4: value4
+    key5: value5
+"""
+    )
+    default_config_file.close()
+
+    override_config_file = tempfile.NamedTemporaryFile(delete=False)
+    override_config_file.write(
+        b"""
+key1:
+  key2:
+    key3: override3
+    key5: override5
+"""
+    )
+    override_config_file.close()
+
+    config = BentoMLConfiguration(
+        default_config_file=default_config_file.name,
+        override_config_file=override_config_file.name,
+        validate_schema=False,
+        legacy_compatibility=False,
+    ).as_dict()
+
+    os.remove(default_config_file.name)
+    os.remove(override_config_file.name)
+    print(default_config_file.name)
+
+    assert config is not None
+    assert config["key1"] is not None
+    assert config["key1"]["key2"] is not None
+    assert config["key1"]["key2"]["key3"] == "override3"
+    assert config["key1"]["key2"]["key4"] == "value4"
+    assert config["key1"]["key2"]["key5"] == "override5"

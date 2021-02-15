3aea81b0f1374d1fe39c9037e17a36c69b7f2a4f
Vadim Geshiktor <vadim.geshiktor@beyondminds.ai>
2021-02-15 12:34:12 +0200

    fix opentracing import

--- tests/server/test_tracing_api.py
+++ tests/server/test_tracing_api.py
@@ -6,11 +6,12 @@ from bentoml import config
 @pytest.mark.skipif(not config("tracing").get("opentracing_server_address"),
                     reason="Opentracing is not configured")
 def test_initialize_tracer():
+    import opentracing
     from bentoml.tracing.opentrace import initialize_tracer
 
     service_name = 'test service name'
 
-    tracer = initialize_tracer(service_name=service_name) or opentracing.global_tracer()  # noqa: F821,E501
+    tracer = initialize_tracer(service_name=service_name) or opentracing.global_tracer()  # noqa: F821, E501
 
     assert tracer is not None
 

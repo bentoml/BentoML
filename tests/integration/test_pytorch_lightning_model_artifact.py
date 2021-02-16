957fc39822a378af3088a45dc68d60bb0ad5419d
yubozhao <yubz86@gmail.com>
2021-02-16 14:40:40 -0800

    Update test_pytorch_lightning_model_artifact.py

--- tests/integration/test_pytorch_lightning_model_artifact.py
+++ tests/integration/test_pytorch_lightning_model_artifact.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 import pytest
 
 import torch
@@ -12,7 +13,6 @@ from tests.bento_service_examples.pytorch_lightning_classifier import (
 )
 from pytorch_lightning.core.lightning import LightningModule
 from tests.integration.utils import (
-    export_service_bundle,
     run_api_server_docker_container,
     build_api_server_docker_image,
 )
@@ -21,6 +21,11 @@ from tests.integration.utils import (
 class TorchLightningModel(LightningModule):
     def forward(self, x):
         return x.add(1)
+
+
+class TorchLightningModelTwo(LightningModule):
+    def forward(self, x):
+        return x.add(2)
 
 
 def test_pytorch_lightning_model_artifact_with_saved_lightning_model():
@@ -32,10 +37,10 @@ def test_pytorch_lightning_model_artifact_with_saved_lightning_model():
         torch.jit.save(script, script_path)
         svc.pack('model', script_path)
 
-        with export_service_bundle(svc) as saved_path:
-            svc = bentoml.load(saved_path)
-            result = svc.predict(pd.DataFrame([[5, 4, 3, 2]]))
-            assert result.tolist() == [[6, 5, 4, 3]]
+        saved_path = svc.save(version=uuid.uuid4().hex[0:8])
+        svc = bentoml.load(saved_path)
+        result = svc.predict(pd.DataFrame([[5, 4, 3, 2]]))
+        assert result.tolist() == [[6, 5, 4, 3]]
 
 
 def test_pytorch_lightning_model_artifact():
@@ -43,20 +48,20 @@ def test_pytorch_lightning_model_artifact():
     model = TorchLightningModel()
     svc.pack('model', model)
 
-    with export_service_bundle(svc) as saved_path:
-        svc = bentoml.load(saved_path)
-        result = svc.predict(pd.DataFrame([[5, 4, 3, 2]]))
-        assert result.tolist() == [[6, 5, 4, 3]]
+    saved_path = svc.save(version=uuid.uuid4().hex[0:8])
+    svc = bentoml.load(saved_path)
+    result = svc.predict(pd.DataFrame([[5, 4, 3, 2]]))
+    assert result.tolist() == [[6, 5, 4, 3]]
 
 
 @pytest.fixture(scope='module')
 def image(clean_context):
     svc = PytorchLightningService()
-    model = TorchLightningModel()
+    model = TorchLightningModelTwo()
     svc.pack('model', model)
+    saved_path = svc.save(version=uuid.uuid4().hex[0:8])
 
-    with export_service_bundle(svc) as saved_path:
-        yield clean_context.enter_context(build_api_server_docker_image(saved_path))
+    yield clean_context.enter_context(build_api_server_docker_image(saved_path))
 
 
 @pytest.fixture(scope='module')
@@ -75,5 +80,5 @@ async def test_pytorch_lightning_with_docker(host):
         headers=(("Content-Type", "application/json"),),
         data=json.dumps([[5, 4, 3, 2]]),
         assert_status=200,
-        assert_data=b'[[6, 5, 4, 3]]',
+        assert_data=b'[[7, 6, 5, 4]]',
     )

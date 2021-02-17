d4a626439e9a066a9a357a08633971e3c6c09e1a
jackyzha0 <j.zhao2k19@gmail.com>
2021-02-16 17:56:37 -0800

    write out integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,6 +3,8 @@ import contextlib
 
 import pytest
 
+from tests.integration.utils import run_api_server_docker_container
+
 
 @pytest.fixture(scope="session")
 def clean_context():
@@ -14,3 +16,66 @@ def clean_context():
 def enable_microbatch(request):
     pytest.enable_microbatch = request.param
     return request.param
+
+
+@pytest.fixture(scope="module")
+def host(image, enable_microbatch):
+    with run_api_server_docker_container(
+        image, enable_microbatch=enable_microbatch, timeout=500
+    ) as host:
+        yield host
+
+
+@pytest.fixture()
+def trained_pytorch_classifier():
+    import torch
+    import torch.nn as nn
+    import torch.nn.functional as F
+    import torchvision
+    import torchvision.transforms as transforms
+    import torch.optim as optim
+
+    # reproducability
+    torch.manual_seed(0)
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    trainset = torchvision.datasets.CIFAR10(root='./tests/integration/data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = x.view(-1, 16 * 5 * 5)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+    net = Net()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    # train
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+    return net
+

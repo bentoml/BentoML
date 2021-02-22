a025481c2426d732245cbad78fa3ef386d06b469
bojiang <hrmthw@outlook.com>
2021-02-22 12:18:24 +0800

    fix endpoint script

--- bentoml/saved_bundle/docker-entrypoint.sh
+++ bentoml/saved_bundle/docker-entrypoint.sh
@@ -12,7 +12,7 @@ _is_sourced() {
 _main() {
   # if first arg looks like a flag, assume we want to start bentoml YataiService
   if [ "${1:0:1}" = '-' ]; then
-    set -- bentoml serve-gunicorn "$@" /bento
+    set -- bentoml serve-gunicorn "$@" /home/bentoml
   fi
 
   # Set BentoML API server port via env var

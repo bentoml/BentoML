f059bc2ab06cad0c2d5c1e47ec17fdfe107a7075
Chaoyu <paranoyang@gmail.com>
2021-03-15 17:56:59 -0700

    Update __init__.py

--- bentoml/tracing/__init__.py
+++ bentoml/tracing/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
 from contextlib import contextmanager
 
 from bentoml import config

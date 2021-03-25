e5cb68396d08120bf35935c116cd2dab6d9c4c4c
yubozhao <yubz86@gmail.com>
2021-03-24 23:50:59 -0700

    Update conftest.py

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -4,10 +4,10 @@ import pytest
 
 @pytest.fixture()
 def postgres_db_url():
-    postgres_db_url = 'postgresql://postgres:postgres@localhost/bentoml:5432'
+    db_url = 'postgresql://postgres:postgres@localhost/bentoml:5432'
     from sqlalchemy_utils import create_database
 
-    create_database(postgres_db_url)
+    create_database(db_url)
     time.sleep(60)
 
-    yield postgres_db_url
+    yield db_url

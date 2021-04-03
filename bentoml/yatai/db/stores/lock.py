3e492e93068917eef9cb957d7179be5d32c2d38d
jackyzha0 <j.zhao2k19@gmail.com>
2021-04-02 20:06:06 -0700

    add locking module impl with retry

--- bentoml/yatai/db/stores/lock.py
+++ bentoml/yatai/db/stores/lock.py
@@ -8,10 +8,6 @@ from bentoml.yatai.db import Base
 from bentoml.yatai.proto.label_selectors_pb2 import LabelSelectors
 
 
-class RESOURCE_TYPE(enum.Enum):
-    deployment = 1
-    bento = 2
-
 class LOCK_STATUS(enum.Enum):
     read_lock = 1
     write_lock = 2
@@ -21,43 +17,39 @@ class Lock(Base):
     __tablename__ = 'locks'
     __table_args__ = tuple(
         UniqueConstraint(
-            'resource_type',
             'resource_id',
-            name='_resource_type_resource_id_uc',
+            name='_resource_id_uc',
         )
     )
     id = Column(Integer, primary_key=True)
-    resource_type = Column(Enum(RESOURCE_TYPE))
     resource_id = Column(Integer, nullable=False)
     lock_status = Column(Enum(LOCK_STATUS))
 
 
 class LockStore(object):
     @staticmethod
-    def _find_lock(sess, resource_type, resource_id):
+    def _find_lock(sess, resource_id):
         lock_obj = (
             sess.query(Lock)
-            .filter_by(
-                resource_id=resource_id,
-                resource_type=resource_type,
-            )
+            .filter_by(resource_id=resource_id)
             .one()
         )
         return lock_obj
 
     @staticmethod
-    def acquire(sess, lock, type, resource_type, resource_id):
+    def acquire(sess, lock_type, resource_id):
+        lock = LockStore._find_lock(sess, resource_id)
+
         # no lock found; free to acquire
         if not lock:
             lock = Lock()
-            lock.lock_status = type
+            lock.lock_status = lock_type
             lock.resource_id = resource_id
-            lock.resource_type = resource_type
             sess.add(lock)
             return True
 
         # acquire read lock
-        if type == LOCK_STATUS.read_lock:
+        if lock_type == LOCK_STATUS.read_lock:
             if lock.lock_status == LOCK_STATUS.write_lock:
                 raise LockUnavailable("Failed to acquire read lock, write lock held")
 
@@ -68,18 +60,7 @@ class LockStore(object):
             # can't acquire read/write lock when write lock is already held
             raise LockUnavailable(f"Failed to acquire write lock, lock held")
 
-    # thin wrapper around acquire
     @staticmethod
-    def acquire_read(sess, resource_type, resource_id):
-        lock = LockStore._find_lock(sess, resource_type, resource_id)
-        LockStore.acquire(sess, lock, LOCK_STATUS.read_lock)
-
-    @staticmethod
-    def acquire_write(sess, resource_type, resource_id):
-        lock = LockStore._find_lock(sess, resource_type, resource_id)
-        LockStore.acquire(sess, lock, LOCK_STATUS.write_lock)
-
-    @staticmethod
-    def release(sess, resource_type, resource_id):
-        lock = LockStore._find_lock(sess, resource_type, resource_id)
+    def release(sess, resource_id):
+        lock = LockStore._find_lock(sess, resource_id)
         sess.delete(lock)
---
+++ bentoml/yatai/locking/lock.py
@@ -0,0 +1,34 @@
+from bentoml.exceptions import LockUnavailable
+import random
+import time
+
+from bentoml.yatai.db.stores.lock import LockStore, LOCK_STATUS
+
+
+class LockType:
+    READ = LOCK_STATUS.read_lock
+    WRITE = LOCK_STATUS.write_lock
+
+class Lock():
+    def __init__(self, db, lock_identifier, type=LockType.READ, timeout_seconds=None, timeout_jitter_seconds=1, max_retry_count=5):
+        self.db = db
+        self.id = lock_identifier
+        self.type = type
+        self.timeout = timeout_seconds
+        self.jitter = timeout_jitter_seconds
+        self.tries = max_retry_count
+
+    def __enter__(self):
+        for i in range(self.tries):
+            try:
+                with self.db.create_session() as sess:
+                    ok = LockStore.acquire(sess, self.type, self.id)
+                    if ok:
+                        return sess
+            except LockUnavailable as e:
+                print(f"Failed to acquire lock: {e}. Retrying {self.tries - i} more times...")
+                sleep_seconds = self.timeout + random.random() * self.jitter
+                time.sleep(sleep_seconds)
+
+    def __exit__(self, type, value, traceback):
+        LockStore.release(self.id)

dac127bd913afbd512e46a00951d8cacea20cc31
yubozhao <yubz86@gmail.com>
2021-04-08 15:30:08 -0700

    Update install_e2e_test_deps.sh

--- ci/install_e2e_test_deps.sh
+++ ci/install_e2e_test_deps.sh
@@ -1,7 +1,7 @@
- #!/usr/bin/env bash
- set -x
+#!/usr/bin/env bash
+set -x
 
- python -m pip install --upgrade pip
- pip install urllib3>=1.25.10 six>=1.15
- pip install .
- pip install --editable .[test]
+python -m pip install --upgrade pip
+pip install urllib3>=1.25.10 six>=1.15
+pip install .
+sudo pip install --upgrade --editable .[test]

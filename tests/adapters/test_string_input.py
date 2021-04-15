c3fcdb7312a1ed0c9087fe2f06c8eea5c1ba9ac1
bojiang <hrmthw@outlook.com>
2021-04-15 08:54:18 +0800

    [CI] add unittest for StringInput

---
+++ tests/adapters/test_string_input.py
@@ -0,0 +1,22 @@
+# pylint: disable=redefined-outer-name
+
+from bentoml.types import HTTPRequest
+
+
+def test_string_input(make_api):
+    from bentoml.adapters import StringInput, JsonOutput
+
+    api = make_api(
+        input_adapter=StringInput(), output_adapter=JsonOutput(), user_func=lambda i: i,
+    )
+
+    body = b'{"a": 1}'
+
+    request = HTTPRequest(body=body)
+    response = api.handle_request(request)
+
+    assert response.body == body.decode()
+
+    responses = api.handle_batch_request([request] * 3)
+    for response in responses:
+        assert response.body == body.decode()

d2da8e50895e8b4134e4d66e6c98bbfc08d47bf2
Andrew M. Sica <andrewsi@us.ibm.com>
2021-04-22 11:24:57 -0400

    fiixing CI test

--- tests/integration/test_onnxmlir_model_artifact.py
+++ tests/integration/test_onnxmlir_model_artifact.py
@@ -1,12 +1,14 @@
 import pytest
 import numpy as np
+import pandas 
 import tensorflow as tf
 import subprocess
 from tests.bento_service_examples.onnxmlir_classifier import OnnxMlirClassifier
 from bentoml.yatai.client import YataiClient
 
 test_data = [[1, 2, 3, 4, 5]]
-test_tensor = tf.constant(np.asfarray(test_data))
+test_df = pandas.DataFrame([[1, 2, 3, 4, 5]])
+test_tensor = np.asfarray(test_data)
 
 class TfNativeModel(tf.Module):
     def __init__(self):
@@ -20,66 +22,77 @@ class TfNativeModel(tf.Module):
     def __call__(self, inputs):
         return self.dense(inputs)
 
+@pytest.fixture()
+def onnxmlir_classifier_class():
+    # When the ExampleBentoService got saved and loaded again in the test, the two class
+    # attribute below got set to the loaded BentoService class. Resetting it here so it
+    # does not effect other tests
+    OnnxMlirClassifier._bento_service_bundle_path = None
+    OnnxMlirClassifier._bento_service_bundle_version = None
+    return OnnxMlirClassifier()
 
 @pytest.fixture()
 def tensorflow_model(tmp_path_factory):
     model1 = TfNativeModel()
     tmpdir = str(tmp_path_factory.mktemp("tf2_model"))
-    print(tmpdir)
     tf.saved_model.save(model1, tmpdir)
     return tmpdir
 
 
 @pytest.fixture()
 def convert_to_onnx(tensorflow_model, tmp_path_factory):
-    tf_model = tensorflow_model()
-    tmpdir = tmp_path_factory.mktemp("onnx_model")
+    tf_model = tensorflow_model
+    tmpdir = str(tmp_path_factory.mktemp("onnx_model"))
     modelpath = tmpdir + '/model.onnx'
     command = [
-        'python -m tf2onnx.convert',
+        'python',
+        '-m',
+        'tf2onnx.convert',
         '--saved-model',
-        tf_model,
+        '.',
         '--output',
-        modelpath,
+        modelpath
     ]
     docker_proc = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,cwd=tf_model,
+        text=True
     )
-    stdout = docker_proc.stdout.read().decode('utf-8')
-    assert 'model.onnx' in stdout, 'Failed to convert TF model'
-    return modelpath
+    stdout, stderr = docker_proc.communicate()
+    assert 'ONNX model is saved' in stderr, 'Failed to convert TF model'
+    return tmpdir
 
 
 @pytest.fixture()
 def compile_model(convert_to_onnx, tmp_path_factory):
+    onnxmodelloc = convert_to_onnx+'/model.onnx'
     command = [
-        './onnx-mlir',
+        'onnx-mlir',
         '--EmitLib',
-        convert_to_onnx,
+        onnxmodelloc
     ]
     docker_proc = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        text=True
     )
+    stdout, stderr = docker_proc.communicate()
     # should return something like: 'Shared library model.so has been compiled.'
-    stdout = docker_proc.stdout.read().decode('utf-8')
     assert 'has been compiled' in stdout, 'Failed to compile model'
-    modelname = 'model.so'
-    return modelname
-
+    #modelname = 'model.so'
+    return convert_to_onnx
 
 @pytest.fixture()
-def get_onnx_mlir_svc(compile_model):
-    svc = OnnxMlirClassifier()
+def get_onnx_mlir_svc(compile_model, onnxmlir_classifier_class):
+    svc = onnxmlir_classifier_class
     # need to check compile output location from compile_model
-    model = compile_model()
+    model = compile_model + '/model.so'
     svc.pack('model', model)
     return svc
 
 
 def test_onnxmlir_artifact(get_onnx_mlir_svc):
-    svc = get_onnx_mlir_svc()
+    svc = get_onnx_mlir_svc
     assert (
-        get_onnx_mlir_svc.predict(test_tensor) == 15.0
+        svc.predict(test_df) == 15.0
     ), 'Inference on onnx-mlir artifact does not match expected'
 
     # clean up saved bundle

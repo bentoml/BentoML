ee1c3c6faae4b6548d096f3c09256d8d39530843
Sean Sheng <s3sheng@gmail.com>
2021-05-06 02:25:26 -0700

    Fix tests.

--- tests/cli/test_yatai_service.py
+++ tests/cli/test_yatai_service.py
@@ -7,6 +7,12 @@ from bentoml.cli.yatai_service import add_yatai_service_sub_command
 from bentoml.configuration import expand_env_var
 
 
+FILE_SYSTEM_REPOSITORY = expand_env_var(os.path.join("~", "bentoml", "repository"))
+SQLITE_DATABASE_URL = "sqlite:///" + expand_env_var(
+    os.path.join("~", "bentoml", "storage.db")
+)
+
+
 def test_yatai_service_start():
     runner = CliRunner()
 
@@ -21,15 +27,13 @@ def test_yatai_service_start():
         runner.invoke(yatai_service_start_cmd)
         mocked_start_yatai_service_grpc_server.assert_called()
         mocked_start_yatai_service_grpc_server.assert_called_with(
-            db_url="sqlite:////Users/ssheng/bentoml/storage.db",
+            db_url=SQLITE_DATABASE_URL,
             grpc_port=50051,
             ui_port=3000,
             with_ui=True,
             base_url=".",
             repository_type="file_system",
-            file_system_directory=expand_env_var(
-                os.path.join("~", "bentoml", "repository")
-            ),
+            file_system_directory=FILE_SYSTEM_REPOSITORY,
             s3_url=None,
             s3_endpoint_url=None,
             gcs_url=None,
@@ -38,15 +42,13 @@ def test_yatai_service_start():
         runner.invoke(yatai_service_start_cmd, ["--repo-base-url=s3://url_address"])
         mocked_start_yatai_service_grpc_server.assert_called()
         mocked_start_yatai_service_grpc_server.assert_called_with(
-            db_url="sqlite:////Users/ssheng/bentoml/storage.db",
+            db_url=SQLITE_DATABASE_URL,
             grpc_port=50051,
             ui_port=3000,
             with_ui=True,
             base_url=".",
             repository_type="s3",
-            file_system_directory=expand_env_var(
-                os.path.join("~", "bentoml", "repository")
-            ),
+            file_system_directory=FILE_SYSTEM_REPOSITORY,
             s3_url="s3://url_address",
             s3_endpoint_url=None,
             gcs_url=None,
@@ -55,15 +57,13 @@ def test_yatai_service_start():
         runner.invoke(yatai_service_start_cmd, ["--repo-base-url=gs://url_address"])
         mocked_start_yatai_service_grpc_server.assert_called()
         mocked_start_yatai_service_grpc_server.assert_called_with(
-            db_url="sqlite:////Users/ssheng/bentoml/storage.db",
+            db_url=SQLITE_DATABASE_URL,
             grpc_port=50051,
             ui_port=3000,
             with_ui=True,
             base_url=".",
             repository_type="gcs",
-            file_system_directory=expand_env_var(
-                os.path.join("~", "bentoml", "repository")
-            ),
+            file_system_directory=FILE_SYSTEM_REPOSITORY,
             s3_url=None,
             s3_endpoint_url=None,
             gcs_url="gs://url_address",
@@ -93,15 +93,13 @@ def test_yatai_service_start_repository_types():
         )
         mocked_start_yatai_service_grpc_server.assert_called()
         mocked_start_yatai_service_grpc_server.assert_called_with(
-            db_url="sqlite:////Users/ssheng/bentoml/storage.db",
+            db_url=SQLITE_DATABASE_URL,
             grpc_port=50051,
             ui_port=3000,
             with_ui=True,
             base_url=".",
             repository_type="s3",
-            file_system_directory=expand_env_var(
-                os.path.join("~", "bentoml", "repository")
-            ),
+            file_system_directory=FILE_SYSTEM_REPOSITORY,
             s3_url="s3://url_address",
             s3_endpoint_url=None,
             gcs_url=None,
@@ -117,15 +115,13 @@ def test_yatai_service_start_repository_types():
         )
         mocked_start_yatai_service_grpc_server.assert_called()
         mocked_start_yatai_service_grpc_server.assert_called_with(
-            db_url="sqlite:////Users/ssheng/bentoml/storage.db",
+            db_url=SQLITE_DATABASE_URL,
             grpc_port=50051,
             ui_port=3000,
             with_ui=True,
             base_url=".",
             repository_type="s3",
-            file_system_directory=expand_env_var(
-                os.path.join("~", "bentoml", "repository")
-            ),
+            file_system_directory=FILE_SYSTEM_REPOSITORY,
             s3_url="s3://url_address",
             s3_endpoint_url="s3://endpoint_url_address",
             gcs_url=None,
@@ -137,15 +133,13 @@ def test_yatai_service_start_repository_types():
         )
         mocked_start_yatai_service_grpc_server.assert_called()
         mocked_start_yatai_service_grpc_server.assert_called_with(
-            db_url="sqlite:////Users/ssheng/bentoml/storage.db",
+            db_url=SQLITE_DATABASE_URL,
             grpc_port=50051,
             ui_port=3000,
             with_ui=True,
             base_url=".",
             repository_type="gcs",
-            file_system_directory=expand_env_var(
-                os.path.join("~", "bentoml", "repository")
-            ),
+            file_system_directory=FILE_SYSTEM_REPOSITORY,
             s3_url=None,
             s3_endpoint_url=None,
             gcs_url="gs://url_address",

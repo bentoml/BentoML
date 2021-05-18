a49ccc2ff5ecb8d3eca6885944437ffd53bbaa82
yubozhao <yubz86@gmail.com>
2021-05-18 15:36:51 -0700

    Update install_e2e_test_deps.sh

--- ci/install_e2e_test_deps.sh
+++ ci/install_e2e_test_deps.sh
@@ -2,7 +2,5 @@
 set -x
 
 python -m pip install --upgrade pip
-pip install urllib3>=1.25.10 six>=1.15 psycopg2
-pip install .
-python -m pip install --upgrade --editable ".[test]"
+sudo python -m pip install --upgrade --editable ".[test]"
 exit

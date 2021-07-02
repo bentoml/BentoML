ab5711794d6508180e5d3ac47448685084c06465
Zhao Shenyang <dev@zsy.im>
2021-07-02 16:58:18 +0800

    fix: minor fix of git hooks installation script

--- dev/install_git_hooks.sh
+++ dev/install_git_hooks.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
 cd "$GIT_ROOT" || exit
 
 HOOKS_PATH="$GIT_ROOT/.git/hooks"
-cd $HOOKS_PATH && ln -s ../../dev/githooks/* .
+cd "$HOOKS_PATH" && ln -s "$GIT_ROOT"/dev/githooks/* .
 

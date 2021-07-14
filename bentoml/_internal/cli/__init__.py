6fbedb4b4f7a51569a526c2609d98c4463df7b1f
yubozhao <yubz86@gmail.com>
2021-07-13 18:10:57 -0700

    Remove deployment clis

--- bentoml/_internal/cli/__init__.py
+++ bentoml/_internal/cli/__init__.py
@@ -1,7 +1,3 @@
-from bentoml.cli.aws_ec2 import get_aws_ec2_sub_command
-from bentoml.cli.aws_lambda import get_aws_lambda_sub_command
-from bentoml.cli.aws_sagemaker import get_aws_sagemaker_sub_command
-from bentoml.cli.azure_functions import get_azure_functions_sub_command
 from bentoml.cli.bento_management import add_bento_sub_command
 from bentoml.cli.bento_service import create_bento_service_cli
 from bentoml.cli.deployment import get_deployment_sub_command
@@ -16,17 +12,9 @@ def create_bentoml_cli():
     # Commands created here aren't mean to be used from generated BentoService CLI when
     # installed as PyPI package. The are only used as part of BentoML cli command.
 
-    aws_sagemaker_sub_command = get_aws_sagemaker_sub_command()
-    aws_lambda_sub_command = get_aws_lambda_sub_command()
     deployment_sub_command = get_deployment_sub_command()
-    azure_function_sub_command = get_azure_functions_sub_command()
-    aws_ec2_sub_command = get_aws_ec2_sub_command()
     add_bento_sub_command(_cli)
     add_yatai_service_sub_command(_cli)
-    _cli.add_command(aws_sagemaker_sub_command)
-    _cli.add_command(aws_lambda_sub_command)
-    _cli.add_command(aws_ec2_sub_command)
-    _cli.add_command(azure_function_sub_command)
     _cli.add_command(deployment_sub_command)
 
     return _cli

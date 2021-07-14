56f39911c738d2fbd986183594eecccaa9775068
Joe Conwell <jmc529@vt.edu>
2021-07-14 16:21:01 -0400

    Resolved Aaron's concerns

--- bentoml/yatai/web/src/index.ts
+++ bentoml/yatai/web/src/index.ts
@@ -8,6 +8,11 @@ const port = args[1];
 const base_log_path = args[2];
 const base_url = args[3];
 const prometheus_address = args[4];
+if (!grpcServerAddress || !prometheus_address || !port || !base_log_path) {
+  throw Error(
+    "Required field grpc server address, port, base log path or prometheus address is missing"
+  );
+}
 
 createLogger(base_log_path);
 const app = getExpressApp(grpcServerAddress, base_url, prometheus_address);

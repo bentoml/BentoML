8e982eb620e5ab081c9a4473c6a035ca4adf2326
yubozhao <yubz86@gmail.com>
2021-07-15 13:57:15 -0700

    update

---
+++ yatai/yatai/cli/bento_management.py
@@ -0,0 +1,13 @@
+def add_bento_management_sub_commands(cli):
+
+    @cli.command()
+    def list():
+      pass
+
+    @cli.command()
+    def get():
+      pass
+
+    @cli.command()
+    def delete():
+      pass
---
+++ yatai/yatai/cli/label.py
@@ -0,0 +1,13 @@
+def add_label_sub_commands(cli):
+
+    @cli.command()
+    def add(self):
+        pass
+
+    @cli.command()
+    def update(self):
+        pass
+
+    @cli.command()
+    def remove(self):
+        pass

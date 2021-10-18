98c16c0c1241f18659a00ada420aad938b894753
Damir Temir <dtemi2@uis.edu>
2021-10-18 16:31:08 -0500

    tests: add pycaret integration tests

--- tests/integration/frameworks/test_pycaret_impl.py
+++ tests/integration/frameworks/test_pycaret_impl.py
@@ -1,41 +1,98 @@
-from pycaret.classification import create_model, predict_model
+import os
+import typing as t
+
+from pycaret.classification import create_model, predict_model, finalize_model, save_model
 from pycaret.classification import setup as pycaret_setup
 from pycaret.classification import tune_model
 from pycaret.datasets import get_data
+import sklearn
 
-from bentoml.pycaret import PycaretModel
+import pytest
+
+import bentoml.pycaret
+from bentoml.exceptions import BentoMLException
+from tests.utils.frameworks.pycaret_utils import test_df
 from tests.utils.helpers import assert_have_file_extension
 
+if t.TYPE_CHECKING:
+    from bentoml._internal.models.store import ModelInfo, ModelStore
 
-def set_dataset():
-    dataset = get_data("credit")
-    train_data = dataset.sample(frac=0.95, random_state=786)
-    pred_data = dataset.drop(train_data.index)
-    train_data.reset_index(inplace=True, drop=True)
-    pred_data.reset_index(inplace=True, drop=True)
-    return train_data, pred_data
+TEST_MODEL_NAME = __name__.split(".")[-1]
 
+def pycaret_model() -> t.Any:
+    dataset = get_data('credit')
+    data = dataset.sample(frac=0.95, random_state=786)
+    data_unseen = dataset.drop(data.index)
+    data.reset_index(inplace=True, drop=True)
+    data_unseen.reset_index(inplace=True, drop=True)
 
-def predict_data(model, data):
-    pred = predict_model(model, data=data)
-    return pred.iloc[0]["Score"]
+    # note: silent must be set to True to avoid the confirmation input of data types
+    env = pycaret_setup(data=data, target='default', session_id=123, silent=True)
+    dt = create_model('dt')
+    tuned_dt = tune_model(dt)
+    final_dt = finalize_model(tuned_dt)
 
+    return final_dt
 
-def test_pycaret_save_load(tmpdir):
-    data, expected = set_dataset()
-    _ = pycaret_setup(
-        data=data,
-        target="default",
-        silent=True,
-        html=False,
-        session_id=420,
-        n_jobs=1,
+@pytest.fixture(scope="module")
+def save_proc(
+    modelstore: "ModelStore"
+) -> t.Callable[[t.Dict[str, t.Any], t.Dict[str, t.Any]], "ModelInfo"]:
+    def _(metadata) -> "ModelInfo":
+        model = pycaret_model()
+        tag = bentoml.pycaret.save(
+            TEST_MODEL_NAME,
+            model,
+            metadata=metadata,
+            model_store=modelstore
+        )
+        info = modelstore.get(tag)
+        return info
+
+    return _
+
+def wrong_module(modelstore: "ModelStore"):
+    model = pycaret_model()
+    with modelstore.register(
+        "wrong_mmodule",
+        module=__name__,
+        options=None,
+        framework_context=None,
+        metadata=None,
+    ) as ctx:
+        save_model(model, os.path.join(ctx.path, "saved_model.pkl"))
+        return str(ctx.path)
+
+@pytest.mark.parametrize(
+    "metadata",
+    [
+        ({"acc": 0.876}),
+    ],
+)
+def test_pycaret_save_load(
+    metadata, modelstore, save_proc
+):  # noqa # pylint: disable
+    info = save_proc(metadata)
+    assert info.metadata is not None
+    assert_have_file_extension(info.path, ".pkl")
+
+    pycaret_loaded = bentoml.pycaret.load(
+        info.tag,
+        model_store=modelstore,
     )
-    dt = create_model("dt")  # create a decision tree classifier
-    tdt = tune_model(dt)
+    assert isinstance(pycaret_loaded, sklearn.pipeline.Pipeline)
+    assert predict_model(pycaret_loaded, data=test_df)['Score'][0] == 0.7609
 
-    PycaretModel(tdt).save(tmpdir)
-    assert_have_file_extension(tmpdir, ".pkl")
+@pytest.mark.parametrize("exc", [BentoMLException])
+def test_pycaret_load_exc(exc, modelstore):
+    tag = wrong_module(modelstore)
+    with pytest.raises(exc):
+        bentoml.pycaret.load(tag, model_store=modelstore)
 
-    pycaret_loaded = PycaretModel.load(tmpdir)
-    assert predict_data(pycaret_loaded, expected) == predict_data(tdt, expected)
+def test_pycaret_runner_setup_run_batch(modelstore, save_proc):
+    info = save_proc(None)
+
+    params = {"ml_usecase": "classification", "available_plots": {},
+              "data": data, "target": "default", "session_id": 123}
+
+    runner = bentoml

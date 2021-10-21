534e26a53a13ae934ecf8e3ac871da9945c5b507
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2021-10-21 19:23:58 -0400

    chore: lint and format
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- dev/format.sh
+++ dev/format.sh
@@ -3,5 +3,15 @@
 GIT_ROOT="$(git rev-parse --show-toplevel)"
 
 cd "$GIT_ROOT" || exit 1
-black .
-isort .
+
+log(){
+	echo -e "\033[2mINFO::\033[0m \e[1;32m$*\e[m" 1>&2
+}
+
+log "Running black and isort on bentoml directory..."
+black --config "$GIT_ROOT"/pyproject.toml bentoml/
+isort bentoml
+
+log "Running black and isort on tests and docker directory..."
+black --config "$GIT_ROOT"/pyproject.toml tests/ docker/
+isort tests/ docker/

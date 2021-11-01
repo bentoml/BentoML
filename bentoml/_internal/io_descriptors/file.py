8534700ca43b59a660442d49e7066ccb48f128ed
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2021-11-01 17:44:38 -0400

    fix: file bytes stream
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- bentoml/_internal/io_descriptors/file.py
+++ bentoml/_internal/io_descriptors/file.py
@@ -1,20 +1,15 @@
-import inspect
+import io
 import logging
-import shutil
 import typing as t
-from pathlib import Path
-from urllib.parse import urlparse
 
 from starlette.requests import Request
-from starlette.responses import FileResponse
+from starlette.responses import StreamingResponse
 
 from ...exceptions import BentoMLException, InvalidArgument
-from ..types import FileLike, PathType
+from ..types import FileLike
 from .base import IODescriptor
 
 logger = logging.getLogger(__name__)
-
-FNAME = "bentoml_output_file"
 
 
 class File(IODescriptor):
@@ -44,38 +39,16 @@ class File(IODescriptor):
     Users can then send a cURL requests like shown in different terminal session with an input PDF files::
         % curl -H "Content-Type: multipart/form-data" -F 'fileobj=@test.pdf;type=application/pdf' http://0.0.0.0:5000/predict
 
+        %PDF-1.7
+                  zed 1/L 1959874/O 282/E 157204/N 12/T 1959312/H [ 500 221]>>
+        <</DecodeParms<</Columns 5/Predictor 12>>/Filter/FlateDecode/ID[<5901CB390F31F2B4497ED82C610CB725><7DF08A87AED4DE458FEBB8B3350A46FF>]/Index[280 35]/Info 279 0 R/Length 88/Prev 1959313/Root 281 0 R/Size 315/Type/XRef/W[1 3 1]>>stream
         Warning: Binary output can mess up your terminal. Use "--output -" to tell
         Warning: curl to output it to your terminal anyway, or consider "--output
-        Warning: <FILE>" to save to a file.%
-
-    We will then save the given file to your current directory of `vit_svc.py`.
-
-    Args:
-        fname (`str`, `optional`, default to `bentoml_output_file`):
-            Given filename for Response output. If not specified the the output file will have the same extensions
-             with the inputs file.
-        output_dir (`PathType`, `optional`):
-            Output path to save output files. If not specified, BentoML will use current directory containing your
-             service definition. For example, if path to `vit_svc.py` is `/home/users/services/vit_svc.py` then the output
-             file will be saved under `/home/users/services`
+        Warning: <FILE>" to save to a file.
 
     Returns:
         IO Descriptor that represents file-like objects.
     """
-
-    def __init__(
-        self, fname: t.Optional[str] = None, *, output_dir: t.Optional[PathType] = None
-    ):
-        if not output_dir:
-            output_dir = (
-                Path(inspect.getframeinfo(inspect.stack()[1][0]).filename)
-                .resolve()
-                .parent
-            )
-        if not fname:
-            fname = FNAME
-        self._output_dir = output_dir
-        self._output_file = Path(output_dir, fname)
 
     def openapi_request_schema(self) -> t.Dict[str, t.Any]:
         """Returns OpenAPI schema for incoming requests"""
@@ -90,12 +63,12 @@ class File(IODescriptor):
                 f"{self.__class__.__name__} should have `Content-Type: multipart/form-data`, got {content_type} instead"
             )
         form = await request.form()
-        file = form[list(form.keys()).pop()]
-        _output_file = self._output_file.with_suffix(Path(file.filename).suffix)
-        contents = await file.read()
-        return FileLike(bytes_=contents, uri=_output_file.as_uri())
+        contents = await form[list(form.keys()).pop()].read()
+        return FileLike(bytes_=contents)
 
-    async def to_http_response(self, obj: t.Union[FileLike, bytes]) -> FileResponse:
+    async def to_http_response(
+        self, obj: t.Union[FileLike, bytes]
+    ) -> StreamingResponse:
         if not any(isinstance(obj, i) for i in [FileLike, bytes]):
             raise InvalidArgument(
                 f"Unsupported Image type received: {type(obj)},"
@@ -103,18 +76,6 @@ class File(IODescriptor):
             )
         if isinstance(obj, FileLike):
             resp = obj.bytes_
-            _output_file = (
-                self._output_file if not obj.uri else Path(urlparse(obj.uri).path)
-            )
         else:
             resp = obj
-            _output_file = self._output_file
-
-        try:
-            with _output_file.open("w+b") as of:
-                of.write(resp)
-            return FileResponse(path=str(_output_file))
-        except Exception as e:  # noqa # pylint: disable=broad-except
-            logger.warning(f"Failed to write {str(self._output_file)}, deleting...")
-            shutil.rmtree(self._output_file)
-            raise e
+        return StreamingResponse(io.BytesIO(resp), media_type="multipart/form-data")

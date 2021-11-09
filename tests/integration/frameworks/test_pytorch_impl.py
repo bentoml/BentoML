03605b52e824b754d47c2b5ce899ca74455e7f22
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2021-11-08 21:52:22 -0500

    fix: torch.set_num_threads()
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- tests/integration/frameworks/test_pytorch_impl.py
+++ tests/integration/frameworks/test_pytorch_impl.py
@@ -1,3 +1,4 @@
+import psutil
 import numpy as np
 import pandas as pd
 import pytest
@@ -55,7 +56,7 @@ def test_pytorch_runner_setup_run_batch(modelstore, input_data):
 
     assert tag in runner.required_models
     assert runner.num_replica == 1
-    assert torch.get_num_threads() == runner.num_concurrency_per_replica
+    assert runner.num_concurrency_per_replica == psutil.cpu_count()
 
     res = runner.run_batch(input_data)
     assert res.unsqueeze(dim=0).item() == 5.0

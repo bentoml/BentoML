b52e79c876b7c58a3dbd4fd3beba9fcc55f2d310
Sauyon Lee <git@sjle.co>
2021-11-10 13:59:26 -0800

    test(internal): add .bentomlignore to test

--- tests/unit/_internal/bento/test_bentos.py
+++ tests/unit/_internal/bento/test_bentos.py
@@ -15,11 +15,11 @@ def test_create_simplebento(tmpdir):
         version="1.0",
         build_ctx="./simplebento",
         additional_models=[],
-        include=["*.py", "config.json", "somefile", "*dir*"],
+        include=["*.py", "config.json", "somefile", "*dir*", ".bentomlignore"],
         exclude=[
             "*.storage",
             "/somefile",
-        ],  # + anything specified in .bentoml_ignore file
+        ],  # + anything specified in .bentomlignore file
         env=dict(
             # pip_install=bentoml.utils.find_required_pypi_packages(svc),
             conda="./environment.yaml",
@@ -37,19 +37,17 @@ def test_create_simplebento(tmpdir):
     )
 
     test_path = os.path.join(tmpdir)
-    assert set(os.listdir(test_path)) == set(["test.simplebento"])
+    assert set(os.listdir(test_path)) == {"test.simplebento"}
     test_path = os.path.join(test_path, "test.simplebento")
-    assert set(os.listdir(test_path)) == set(["latest", "1.0"])
+    assert set(os.listdir(test_path)) == {"latest", "1.0"}
     test_path = os.path.join(test_path, "1.0")
-    assert set(os.listdir(test_path)) == set(
-        [
-            "bento.yaml",
-            "apis",
-            "README.md",
-            "test.simplebento",
-            "env",
-        ]
-    )
+    assert set(os.listdir(test_path)) == {
+        "bento.yaml",
+        "apis",
+        "README.md",
+        "test.simplebento",
+        "env",
+    }
     test_path = os.path.join(test_path, "test.simplebento")
-    assert set(os.listdir(test_path)) == set(["simplebento.py", "subdir"])
-    assert set(os.listdir(os.path.join(test_path, "subdir"))) == set(["somefile"])
+    assert set(os.listdir(test_path)) == {"simplebento.py", "subdir", ".bentomlignore"}
+    assert set(os.listdir(os.path.join(test_path, "subdir"))) == {"somefile"}

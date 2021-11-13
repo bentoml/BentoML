641c422242dc3822e394c72058c13a7d1c01118c
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2021-11-13 04:18:07 -0500

    fix: pyright
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- scripts/ci/style/pyright_check.sh
+++ scripts/ci/style/pyright_check.sh
@@ -6,19 +6,10 @@ source ./scripts/ci/helpers.sh
 
 set_on_failed_callback "FAIL pyright errors"
 
-if [[ -n $GITHUB_BASE_REF ]]; then
-  echo "Running pyright on changed files..."
-  git fetch origin "$GITHUB_BASE_REF"
-  if ! (git diff --name-only --diff-filter=d "origin/$GITHUB_BASE_REF" -z -- '*.py' | xargs -0 --no-run-if-empty pyright); then
-    FAIL "pyright failed on changed files."
-    exit 1
-  fi
-else
-  echo "Running pyright..."
-  if ! (pyright .); then
-    FAIL "pyright failed."
-    exit 1
-  fi
+echo "Running pyright..."
+if ! (pyright .); then
+  FAIL "pyright failed."
+  exit 1
 fi
 
 PASS "pyright passed!"

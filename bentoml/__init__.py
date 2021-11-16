81aa1b07531ffe8de86757f3f78317e017041fa7
Sauyon Lee <2347889+sauyon@users.noreply.github.com>
2021-11-16 21:16:42 +0000

    Bento object fixes (#2015)
    * test(internal): add .bentomlignore to test
    
    * refactor(internal): rename bento metadata to bento info
    
    * fix(internal): make StoreItem.creation_time a property
    
    * fix(internal): import datetime.datetime
    
    * fix(internal): go back to using attrs
    
    * feat(internal): add OSBento for bentos on OSFS
    
    * fix(internal): reuse bento object when saving
    
    * fix(internal): BentoInfo: explicitly use UTC for creation time
    
    * fix(internal): fix errors in poetry dependencies
    
    * fix(internal): BentoInfo: use a class instead of a dict

--- bentoml/__init__.py
+++ bentoml/__init__.py
@@ -1,6 +1,7 @@
 from ._version import get_versions
 
-__version__ = get_versions()["version"]
+__version__: str = get_versions()["version"]  # type: ignore
+
 del get_versions
 
 from ._internal.configuration import load_global_config  # noqa: E402

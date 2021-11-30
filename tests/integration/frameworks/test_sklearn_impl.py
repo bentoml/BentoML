92d7fd5e6e3d5389e9299200f49e97c9ba3c2a2c
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2021-11-30 02:33:45 -0500

    chore: fix sklearn
    chore: lint and format
    
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- tests/integration/frameworks/test_sklearn_impl.py
+++ tests/integration/frameworks/test_sklearn_impl.py
@@ -12,7 +12,6 @@ import bentoml.sklearn
 from bentoml.exceptions import BentoMLException
 from tests.utils.frameworks.sklearn_utils import sklearn_model_data
 from tests.utils.helpers import assert_have_file_extension
-from tests.utils.types import InvalidModule, Pipeline
 
 # fmt: off
 res_arr = np.array(
@@ -28,11 +27,33 @@ res_arr = np.array(
 # fmt: on
 if TYPE_CHECKING:
     from bentoml._internal.models import Model, ModelStore
+    from bentoml._internal.types import Tag
 
 
-def save_sklearn_model(models: "Model") -> None:
+def save_procedure(metadata: t.Dict[str, t.Any], _modelstore: "ModelStore") -> "Tag":
     model, _ = sklearn_model_data(clf=RandomForestClassifier)
-    joblib.dump(model, models.path_of("saved_model.pkl"))
+    tag_info = bentoml.sklearn.save(
+        "test_sklearn_model",
+        model,
+        metadata=metadata,
+        model_store=_modelstore,
+    )
+    return tag_info
+
+
+def forbidden_procedure(_modelstore: "ModelStore") -> "Tag":
+    model, _ = sklearn_model_data(clf=RandomForestClassifier)
+    with bentoml.models.create(
+        "invalid_module",
+        module=__name__,
+        labels=None,
+        options=None,
+        framework_context=None,
+        metadata=None,
+        _model_store=_modelstore,
+    ) as ctx:
+        joblib.dump(model, ctx.path_of("saved_model.pkl"))
+        return ctx.tag
 
 
 @pytest.mark.parametrize(
@@ -43,10 +64,11 @@ def save_sklearn_model(models: "Model") -> None:
     ],
 )
 def test_sklearn_save_load(
-    metadata: t.Dict[str, t.Any], modelstore: "ModelStore", pipeline: Pipeline
+    metadata: t.Dict[str, t.Any], modelstore: "ModelStore"
 ) -> None:
-    model, data = sklearn_model_data(clf=RandomForestClassifier)
-    _model = pipeline(model, bentoml.sklearn, metadata=metadata)
+    _, data = sklearn_model_data(clf=RandomForestClassifier)
+    tag = save_procedure(metadata, _modelstore=modelstore)
+    _model = bentoml.models.get(tag, _model_store=modelstore)
     assert _model.info.metadata is not None
     assert_have_file_extension(_model.path, ".pkl")
 
@@ -57,25 +79,20 @@ def test_sklearn_save_load(
     np.testing.assert_array_equal(loaded.predict(data), res_arr)
 
 
-def test_get_model_info_exc(
-    modelstore: "ModelStore", invalid_module: InvalidModule
-) -> None:
-    tag = invalid_module(save_sklearn_model)
+def test_get_model_info_exc(modelstore: "ModelStore") -> None:
+    tag = forbidden_procedure(_modelstore=modelstore)
     with pytest.raises(BentoMLException):
-        bentoml.sklearn._get_model_info(tag, model_store=modelstore)
+        _ = bentoml.sklearn.load(tag, model_store=modelstore)
 
 
-def test_sklearn_runner_setup_run_batch(
-    modelstore: "ModelStore", pipeline: Pipeline
-) -> None:
-    model, data = sklearn_model_data(clf=RandomForestClassifier)
-    _model = pipeline(model, bentoml.sklearn)
-    runner = bentoml.sklearn.load_runner(_model.tag, model_store=modelstore)
+def test_sklearn_runner_setup_run_batch(modelstore: "ModelStore") -> None:
+    _, data = sklearn_model_data(clf=RandomForestClassifier)
+    tag = save_procedure({}, _modelstore=modelstore)
+    runner = bentoml.sklearn.load_runner(tag, model_store=modelstore)
 
-    assert _model.tag in runner.required_models
+    assert tag in runner.required_models
     assert runner.num_concurrency_per_replica == 1
     assert runner.num_replica == psutil.cpu_count()
 
     res = runner.run_batch(data)
-    print(res)
     assert (res == res_arr).all()

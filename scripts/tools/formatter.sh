d432ea64bb27b543fb7716cf9f6d7fa853e4442d
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2021-12-01 23:11:34 -0500

    feat: add some pre-commit hooks (#2078)

--- scripts/tools/formatter.sh
+++ scripts/tools/formatter.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-black --config ./pyproject.toml bentoml/ tests/ docker/ typings/
+GIT_ROOT=$(git rev-parse --show-toplevel)
 
-isort bentoml/ tests/ docker/ typings/
+cd "$GIT_ROOT" || exit 1
+
+source ./scripts/ci/helpers.sh
+
+INFO "(black) Formatting codebase..."
+
+black --config ./pyproject.toml bentoml/ tests/ docker/
+
+INFO "(black) Formatting stubs..."
+
+black --pyi typings/**/*.pyi
+
+INFO "(isort) Reordering imports..."
+
+isort .
--- scripts/tools/linter.sh
+++ scripts/tools/linter.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+cd "$GIT_ROOT" || exit 1
+
+source ./scripts/ci/helpers.sh
+
+INFO "(flake8) Linting codebase..."
+
 flake8 --config=setup.cfg bentoml tests docker
 
+INFO "(pylint) Linting bentoml..."
+
 pylint --rcfile="./pyproject.toml" bentoml
+
+INFO "(pylint) Linting tests and docker..."
 
 pylint --rcfile="./pyproject.toml" --disable=E0401,F0010 tests docker

21a5e37324c2e0dfc528221393ecc67dffcbb3bd
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2021-12-10 09:27:42 -0500

    fix: sed difference between MacOS and Linux
    fix: setup.cfg
    
    chore: fix docstring error
    
    revert: scripts cleanup
    
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- scripts/tools/stubs_cleanup.sh
+++ scripts/tools/stubs_cleanup.sh
@@ -17,6 +17,12 @@ fi
 
 need_cmd black || (echo "make sure to run \`make install-dev-deps\`"; exit 1);
 
+if [[ $(uname) == "Darwin" ]]; then
+  SED_OPTS=( -E -i '' )
+else
+  SED_OPTS=( -E -i )
+fi
+
 INFO "(pyminify) reducing stubs size..."
 
 for file in $(git ls-files | grep -e "**.pyi$"); do
@@ -24,23 +30,16 @@ for file in $(git ls-files | grep -e "**.pyi$"); do
     PASS "$file already minified, skipping..."
     continue
   else
-    INFO "Processing $file..."
+    INFO "Processing $file ..."
     INFO "Removing pyright bugs..."
-    sed -i "s/],:/]/g; s/,,/,/g; s/]\n    .../]: .../g" "$file"
+    sed "${SED_OPTS[@]}" "s/],:/]/g; s/,,/,/g; s/]\\\n    .../]: .../g" "$file"
     # sed -i "s/]$/]:/g" "$file"
     cp "$file" "$file".bak && rm "$file"
-    if ! pyminify "${MINIFY_OPTS[@]}" "$file".bak &> "$file"; then
-      FAIL "unable to processed $file, reverting to previous state, opening editor to fix..."
+    if ! pyminify "${MINIFY_OPTS[@]}" "$file".bak > "$file"; then
+      FAIL "Unable to processed $file, reverting to previous state. One can also use https://python-minifier.com/ to test where the problem may be. Make sure to match ${MINIFY_OPTS[@]}\nExitting now..."
       rm "$file"
       mv "$file".bak "$file"
-      "$EDITOR" "$file" || exit
-      cp "$file" "$file".bak
-      if ! pyminify "${MINIFY_OPTS[@]}" "$file".bak &> "$file"; then
-        FAIL "Failed to fix $files. One can also use https://python-minifier.com/ to test where the problem may be. Make sure to match ${MINIFY_OPTS[@]}\nExitting now..."
-        rm "$file"
-        mv "$file".bak "$file"
-        exit 1
-      fi
+      exit 1
     fi
     black --fast --config "$GIT_ROOT"/pyproject.toml --pyi "$file"
     printf "%s\n" "$file" >> "$PROCESSED_TXT"

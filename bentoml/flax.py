d81830e5a28a97f1f15d67e3a479a3298d19143c
Chaoyu <paranoyang@gmail.com>
2021-12-12 22:58:23 -0800

    remove unimplemented placeholder

--- bentoml/flax.py
+++ bentoml/flax.py
@@ -1,5 +1,1 @@
-from ._internal.frameworks.flax import load
-from ._internal.frameworks.flax import save
-from ._internal.frameworks.flax import load_runner
-
-__all__ = ["load", "load_runner", "save"]
+# TODO: add flax support

c094664aca9e1fc8b05cbe5a691b846d2eba221e
bojiang <bojiang_@outlook.com>
2022-02-23 11:45:11 +0800

    fix picklable_model tests

--- tests/integration/frameworks/test_picklable_model_impl.py
+++ tests/integration/frameworks/test_picklable_model_impl.py
@@ -29,27 +29,12 @@ def save_procedure(
     metadata: t.Dict[str, t.Any],
 ) -> "Tag":
     model_to_save = MyCoolModel()
-    tag_info = bentoml.picklable_model.save(
+    tag = bentoml.picklable_model.save(
         "test_picklable_model",
         model_to_save,
         metadata=metadata,
-        method="predict",
     )
-    return tag_info
-
-
-def save_batch_procedure(
-    metadata: t.Dict[str, t.Any],
-) -> "Tag":
-    model_to_save = MyCoolBatchModel()
-    tag_info = bentoml.picklable_model.save(
-        "test_picklable_model",
-        model_to_save,
-        batch=True,
-        metadata=metadata,
-        method="predict",
-    )
-    return tag_info
+    return tag
 
 
 @pytest.mark.parametrize(
@@ -71,7 +56,7 @@ def test_picklable_model_save_load(
 def test_picklable_model_runner_setup_run() -> None:
 
     tag = save_procedure({})
-    runner = bentoml.picklable_model.load_runner(tag)
+    runner = bentoml.picklable_model.load_runner(tag, method_name="predict")
 
     assert tag in runner.required_models
     assert runner.run(3) == np.array([9])
@@ -82,16 +67,15 @@ def test_pickle_runner_setup_run_method() -> None:
     tag = bentoml.picklable_model.save(
         "test_pickle_model", lambda x: x ** 2, metadata={}
     )
-    runner = bentoml.picklable_model.load_runner(tag)
+    runner = bentoml.picklable_model.load_runner(tag, method_name="predict")
 
     assert tag in runner.required_models
     assert runner.run(3) == np.array([9])
 
 
 def test_pickle_runner_setup_run_batch() -> None:
-
-    tag = save_batch_procedure({})
-    runner = bentoml.picklable_model.load_runner(tag)
+    tag = save_procedure({})
+    runner = bentoml.picklable_model.load_runner(tag, method_name="predict", batch=True)
 
     assert tag in runner.required_models
     assert runner.run_batch([3, 9]) == [9, 81]

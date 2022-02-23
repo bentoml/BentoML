f17aea836aa5af2ccfedfb66c9ed405873da06bd
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-02-23 04:27:05 -0500

    chore(typings): add deepmerge stubs
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

---
+++ typings/deepmerge/merger.pyi
@@ -0,0 +1,23 @@
+from typing import Any, Dict, List, Tuple, Union
+from .strategy.core import StrategyList
+from .strategy.dict import DictStrategies
+from .strategy.list import ListStrategies
+from .strategy.set import SetStrategies
+
+ConfigDictType = Dict[str, Any]
+
+class Merger:
+    PROVIDED_TYPE_STRATEGIES: Dict[
+        type, Union[ListStrategies, DictStrategies, SetStrategies]
+    ] = ...
+    def __init__(
+        self,
+        type_strategies: List[Tuple[type, str]],
+        fallback_strategies: List[str],
+        type_conflict_strategies: List[str],
+    ) -> None: ...
+    def merge(self, base: ConfigDictType, nxt: ConfigDictType) -> None: ...
+    def type_conflict_strategy(self, *args: Any) -> Any: ...
+    def value_strategy(
+        self, path: str, base: StrategyList, nxt: StrategyList
+    ) -> None: ...
---
+++ typings/deepmerge/strategy/dict.pyi
@@ -0,0 +1,12 @@
+from ..merger import Merger
+from .core import StrategyList
+
+class DictStrategies(StrategyList):
+    @staticmethod
+    def strategy_merge(
+        config: Merger, path: str, base: StrategyList, nxt: StrategyList
+    ) -> StrategyList: ...
+    @staticmethod
+    def strategy_override(
+        config: Merger, path: str, base: StrategyList, nxt: StrategyList
+    ) -> StrategyList: ...

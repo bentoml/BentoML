ed30ac46a8ea7dd88fa1c17e65346a7197e4d39c
Zhao Shenyang <dev@zsy.im>
2022-02-24 10:24:34 +0800

    fix(framework): keras

--- bentoml/_internal/frameworks/keras.py
+++ bentoml/_internal/frameworks/keras.py
@@ -345,6 +345,7 @@ def save(
 
 class _KerasRunner(_TensorflowRunner):
     def _setup(self) -> None:
+        self._configure(self._device_id)
         self._session = get_session()
         self._session.config = self._config_proto
         self._model = load(self._tag)

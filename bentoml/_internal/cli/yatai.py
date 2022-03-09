97f75c1270a4e70c084d6821e8e4629945454013
yetone <yetoneful@gmail.com>
2022-03-09 20:58:48 +0800

    fix: YataiClientContext initialization missing email argument

--- bentoml/_internal/cli/yatai.py
+++ bentoml/_internal/cli/yatai.py
@@ -4,8 +4,8 @@ import click
 
 from bentoml.exceptions import CLIException
 
+from ..yatai_rest_api_client.yatai import YataiRESTApiClient
 from ..yatai_rest_api_client.config import add_context
-from ..yatai_rest_api_client.config import update_context
 from ..yatai_rest_api_client.config import YataiClientContext
 from ..yatai_rest_api_client.config import default_context_name
 
@@ -29,27 +29,25 @@ def add_login_command(cli):
         if not api_token:
             raise CLIException("need --api-token")
 
-        ctx = YataiClientContext(
-            name=default_context_name,
-            endpoint=endpoint,
-            api_token=api_token,
-        )
-
-        add_context(ctx)
-
-        yatai_rest_client = ctx.get_yatai_rest_api_client()
+        yatai_rest_client = YataiRESTApiClient(endpoint, api_token)
         user = yatai_rest_client.get_current_user()
 
         if user is None:
             raise CLIException("current user is not found")
-        ctx.email = user.email
 
         org = yatai_rest_client.get_current_organization()
 
         if org is None:
             raise CLIException("current organization is not found")
 
-        update_context(ctx.name, ctx)
+        ctx = YataiClientContext(
+            name=default_context_name,
+            endpoint=endpoint,
+            api_token=api_token,
+            email=user.email,
+        )
+
+        add_context(ctx)
 
         logger.info(
             f"login successfully! user: {user.name}, organization: {org.name}",

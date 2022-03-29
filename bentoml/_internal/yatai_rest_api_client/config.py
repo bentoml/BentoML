bf8cde5623c8c0f65fefdfcc6e9ef0e75cd6323e
Chaoyu <paranoyang@gmail.com>
2022-03-28 17:41:48 -0700

    revert back yatai client changes

--- bentoml/_internal/yatai_rest_api_client/config.py
+++ bentoml/_internal/yatai_rest_api_client/config.py
@@ -27,9 +27,7 @@ class YataiClientContext:
     name: str
     endpoint: str
     api_token: str
-    email: Optional[str] = attr.field(
-        converter=attr.converters.default_if_none(""), default=None
-    )
+    email: Optional[str] = attr.field(default=None)
 
     def get_yatai_rest_api_client(self) -> YataiRESTApiClient:
         return YataiRESTApiClient(self.endpoint, self.api_token)

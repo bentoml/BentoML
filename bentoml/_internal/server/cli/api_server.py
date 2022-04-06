6420f1b2e2a7d08011ab4aff4f12b29e246a94e8
bojiang <bojiang_@outlook.com>
2022-04-06 23:43:39 +0800

    fix(server): init prometheus dir for yatai

--- bentoml/_internal/server/cli/api_server.py
+++ bentoml/_internal/server/cli/api_server.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 from urllib.parse import urlparse
 
 from bentoml import load
+from bentoml._internal.server import ensure_prometheus_dir
 from bentoml._internal.utils.uri import uri_to_path
 
 from ...log import LOGGING_CONFIG
@@ -23,13 +24,44 @@ import click
 @click.option("--runner-map", type=click.STRING, envvar="BENTOML_RUNNER_MAP")
 @click.option("--backlog", type=click.INT)
 @click.option("--working-dir", type=click.Path(exists=True))
+@click.option(
+    "--as-worker",
+    required=False,
+    type=click.BOOL,
+    is_flag=True,
+    default=False,
+)
 def main(
     bento_identifier: str = "",
     bind: str = "",
     runner_map: t.Optional[str] = None,
     backlog: int = 2048,
     working_dir: t.Optional[str] = None,
+    as_worker: bool = False,
 ):
+    """
+    Start BentoML API server.
+
+    Args
+    ----
+    bento_identifier: str
+        BentoML identifier.
+    bind: str
+        Bind address.
+    runner_map: str
+        Path to runner map file.
+    backlog: int
+        Backlog size.
+    working_dir: str
+        Working directory.
+    as_worker: bool (default: False)
+        If True, start the server as a bare worker. Else, start a standalone server
+        with a supervisor process.
+    """
+    if not as_worker:
+        # if not as_worker, ensure_prometheus_dir may cause a race.
+        ensure_prometheus_dir()
+
     import uvicorn  # type: ignore
 
     ServiceContext.component_name_var.set("api_server")

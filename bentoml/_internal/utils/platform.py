ea160247e70782a3f432266519f92167c1bbee8d
bojiang <bojiang_@outlook.com>
2022-04-25 17:03:12 +0800

    fix runtime typing

--- bentoml/_internal/utils/platform.py
+++ bentoml/_internal/utils/platform.py
@@ -3,7 +3,7 @@ import subprocess
 import psutil
 
 
-def kill_subprocess_tree(p: subprocess.Popen[bytes]) -> None:
+def kill_subprocess_tree(p: "subprocess.Popen[bytes]") -> None:
     """
     Tell the process to terminate and kill all of its children. Availabe both on Windows and Linux.
     Note: It will return immediately rather than wait for the process to terminate.

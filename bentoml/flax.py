058bb3f07a09ec4b3acb3540d945cc2156d84200
Chaoyu <paranoyang@gmail.com>
2022-05-27 16:03:10 -0700

    reformat

--- bentoml/flax.py
+++ bentoml/flax.py
@@ -1,5 +1,7 @@
-raise NotImplementedError(f"""\
+raise NotImplementedError(
+    f"""\
 Support for "{__name__}" is temporarily unavailable as BentoML transition to the new \
 design in version 1.0.0 release. Before this module is officially implemented in \
 BentoML, users may use Custom Runner as a workaround. Learn more at http://docs.bentoml.org
-""")
+"""
+)

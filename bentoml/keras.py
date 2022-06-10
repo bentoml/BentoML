c7eea8fc97ae92c3d32d5f15b3b7b92f8c52b983
Sean Sheng <s3sheng@gmail.com>
2022-06-09 22:58:37 -0700

    Use alias instead of module variable

--- bentoml/keras.py
+++ bentoml/keras.py
@@ -4,9 +4,7 @@ from ._internal.frameworks.keras import get
 from ._internal.frameworks.keras import load_model
 from ._internal.frameworks.keras import save_model
 from ._internal.frameworks.keras import get_runnable
-from ._internal.frameworks.keras import KerasOptions
-
-ModelOptions = KerasOptions
+from ._internal.frameworks.keras import KerasOptions as ModelOptions  # type: ignore
 
 
 logger = logging.getLogger(__name__)
--- bentoml/tensorflow.py
+++ bentoml/tensorflow.py
@@ -4,9 +4,7 @@ from ._internal.frameworks.tensorflow_v2 import get
 from ._internal.frameworks.tensorflow_v2 import load_model
 from ._internal.frameworks.tensorflow_v2 import save_model
 from ._internal.frameworks.tensorflow_v2 import get_runnable
-from ._internal.frameworks.tensorflow_v2 import TensorflowOptions
-
-ModelOptions = TensorflowOptions
+from ._internal.frameworks.tensorflow_v2 import TensorflowOptions as ModelOptions  # type: ignore
 
 
 logger = logging.getLogger(__name__)

10eb0a6ee7030e5e511deb7e7b6fce295ee748ae
Michael Kuhlen <mike@missionlane.com>
2022-06-13 17:41:53 -0700

    LightGBM framework implementation, plus integration test.

--- bentoml/_internal/frameworks/lightgbm.py
+++ bentoml/_internal/frameworks/lightgbm.py
@@ -1,22 +1,25 @@
-import os
+from __future__ import annotations
+
 import typing as t
+import logging
 from typing import TYPE_CHECKING
 
-import joblib  # type: ignore[reportMissingTypeStubs]
-from simple_di import inject
-from simple_di import Provide
+import numpy as np
 
 import bentoml
 from bentoml import Tag
-from bentoml.exceptions import BentoMLException
+from bentoml.exceptions import NotFound
+from bentoml.exceptions import InvalidArgument
 from bentoml.exceptions import MissingDependencyException
+from bentoml._internal.models.model import ModelContext
 
-from ..models import PKL_EXT
-from ..models import TXT_EXT
-from ..models import SAVE_NAMESPACE
 from ..utils.pkg import get_pkg_version
-from .common.model_runner import BaseModelRunner
-from ..configuration.containers import BentoMLContainer
+
+if TYPE_CHECKING:
+    from bentoml.types import ModelSignature
+    from bentoml.types import ModelSignatureDict
+
+    from .. import external_typing as ext
 
 try:
     import lightgbm as lgb  # type: ignore[reportMissingTypeStubs]
@@ -28,119 +31,106 @@ except ImportError:  # pragma: no cover
         """
     )
 
-
-if TYPE_CHECKING:
-    from .. import external_typing as ext
-    from ..models import ModelStore
-
-    _LightGBMModelType = t.Union[
-        "lgb.LGBMModel",
-        "lgb.LGBMClassifier",
-        "lgb.LGBMRegressor",
-        "lgb.LGBMRanker",
-    ]
-
 MODULE_NAME = "bentoml.lightgbm"
+MODEL_FILENAME = "saved_model.ubj"
+API_VERSION = "v1"
+
+logger = logging.getLogger(__name__)
 
 
-def _get_model_info(
-    tag: t.Union[str, Tag],
-    booster_params: t.Optional[t.Dict[str, t.Union[str, int]]],
-    model_store: "ModelStore",
-) -> t.Tuple["Model", str, t.Dict[str, t.Any]]:
-    model = model_store.get(tag)
-    if model.info.module not in (MODULE_NAME, __name__):
-        raise BentoMLException(
-            f"Model {tag} was saved with module {model.info.module}, failed loading with {MODULE_NAME}."
-        )
-    _fname = (
-        f"{SAVE_NAMESPACE}{TXT_EXT}"
-        if not model.info.options["sklearn_api"]
-        else f"{SAVE_NAMESPACE}{PKL_EXT}"
-    )
-    model_file = model.path_of(_fname)
-    _booster_params: t.Dict[str, t.Union[str, int]] = (
-        dict() if not booster_params else booster_params
-    )
-    for key, value in model.info.options.items():
-        if key not in _booster_params:
-            _booster_params[key] = value
-
-    return model, model_file, _booster_params
-
-
-@inject
-def load(
-    tag: t.Union[str, Tag],
-    booster_params: t.Optional[t.Dict[str, t.Union[str, int]]] = None,
-    model_store: "ModelStore" = Provide[BentoMLContainer.model_store],
-) -> t.Union["lgb.basic.Booster", "_LightGBMModelType"]:
+def get(tag_like: str | Tag) -> bentoml.Model:
     """
-    Load a model from BentoML local modelstore with given name.
+    Get the BentoML model with the given tag.
 
     Args:
-        tag (:code:`Union[str, Tag]`):
-            Tag of a saved model in BentoML local modelstore.
-        booster_params (:code:`Dict[str, Union[str, int]]`, `optional`, defaults to `None`):
-            Parameters for boosters. Refers to `Parameters Docs <https://lightgbm.readthedocs.io/en/latest/Parameters.html>`_
-            for more information.
-        model_store (:mod:`~bentoml._internal.models.store.ModelStore`, default to :mod:`BentoMLContainer.model_store`):
-            BentoML modelstore, provided by DI Container.
-
+        tag_like (``str`` ``|`` :obj:`~bentoml.Tag`):
+            The tag of the model to retrieve from the model store.
     Returns:
-        :obj::code:`Union[lightgbm.LGBMModel, lightgbm.LGBMClassifier, lightgbm.LGBMRegressor, lightgbm.LGBMRanker, lightgbm.basic.Booster]`: An instance of either
-        :obj::code:`Union[lightgbm.LGBMModel, lightgbm.LGBMClassifier, lightgbm.LGBMRegressor, lightgbm.LGBMRanker]` or :obj:`lightgbm.basic.Booster`
-        from BentoML modelstore.
+        :obj:`~bentoml.Model`: A BentoML :obj:`~bentoml.Model` with the matching tag.
+    Example:
 
-    Examples:
+    .. code-block:: python
+
+        import bentoml
+        # target model must be from the BentoML model store
+        model = bentoml.lightgbm.get("my_lightgbm_model")
+    """
+    model = bentoml.models.get(tag_like)
+    if model.info.module not in (MODULE_NAME, __name__):
+        raise NotFound(
+            f"Model {model.tag} was saved with module {model.info.module}, failed loading with {MODULE_NAME}."
+        )
+    return model
+
+
+def load_model(bento_model: str | Tag | bentoml.Model) -> lgb.basic.Booster:
+    """
+    Load the LightGNM model with the given tag from the local BentoML model store.
+
+    Args:
+        bento_model (``str`` ``|`` :obj:`~bentoml.Tag` ``|`` :obj:`~bentoml.Model`):
+            Either the tag of the model to get from the store, or a BentoML `~bentoml.Model`
+            instance to load the model from.
+    Returns:
+        :obj:`~lightgbm.basic.Booster`: The LightGBM model loaded from the model store or BentoML :obj:`~bentoml.Model`.
+
+    Example:
 
     .. code-block:: python
 
         import bentoml
         gbm = bentoml.lightgbm.load("my_lightgbm_model:latest")
     """  # noqa
-    _, _model_file, _booster_params = _get_model_info(tag, booster_params, model_store)
+    if not isinstance(bento_model, bentoml.Model):
+        bento_model = get(bento_model)
+        assert isinstance(bento_model, bentoml.Model)
 
-    if os.path.splitext(_model_file)[1] == PKL_EXT:
-        return joblib.load(_model_file)
-    else:
-        return lgb.Booster(params=_booster_params, model_file=_model_file)
+    model_file = bento_model.path_of(MODEL_FILENAME)
+    booster = lgb.basic.Booster(model_file=model_file)
+    return booster
 
 
-def save(
+def save_model(
     name: str,
-    model: t.Union["lgb.basic.Booster", "_LightGBMModelType"],
+    model: lgb.basic.Booster,
     *,
-    booster_params: t.Optional[t.Dict[str, t.Union[str, int]]] = None,
-    labels: t.Optional[t.Dict[str, str]] = None,
-    custom_objects: t.Optional[t.Dict[str, t.Any]] = None,
-    metadata: t.Optional[t.Dict[str, t.Any]] = None,
+    signatures: dict[str, ModelSignatureDict] | None = None,
+    labels: dict[str, str] | None = None,
+    custom_objects: dict[str, t.Any] | None = None,
+    metadata: dict[str, t.Any] | None = None,
 ) -> Tag:
     """
-    Save a model instance to BentoML modelstore.
+    Save a LightGBM model instance to the BentoML model store.
 
     Args:
-        name (:code:`str`):
-            Name for given model instance. This should pass Python identifier check.
-        model (:code:`Union[lightgbm.basic.Booster, lightgbm.LGBMModel, lightgbm.LGBMClassifier, lightgbm.LGBMRegressor, lightgbm.LGBMRanker]`):
-            Instance of model to be saved.
-        booster_params (:code:`Dict[str, Union[str, int]]`, `optional`, defaults to `None`):
-            Parameters for boosters. Refers to `Parameters Doc <https://lightgbm.readthedocs.io/en/latest/Parameters.html>`_
-            for more information.
-        labels (:code:`Dict[str, str]`, `optional`, default to :code:`None`):
-            user-defined labels for managing models, e.g. team=nlp, stage=dev
-        custom_objects (:code:`Dict[str, Any]]`, `optional`, default to :code:`None`):
-            user-defined additional python objects to be saved alongside the model,
-            e.g. a tokenizer instance, preprocessor function, model configuration json
-        metadata (:code:`Dict[str, Any]`, `optional`, default to :code:`None`):
-            Custom metadata for given model.
-        model_store (:mod:`~bentoml._internal.models.store.ModelStore`, default to :mod:`BentoMLContainer.model_store`):
-            BentoML modelstore, provided by DI Container.
+        name (``str``):
+            The name to give to the model in the BentoML store. This must be a valid
+            :obj:`~bentoml.Tag` name.
+        model (:obj:`~lgb.basic.Booster`):
+            The LightGBM model (booster) to be saved.
+        signatures (``dict[str, ModelSignatureDict]``, optional):
+            Signatures of predict methods to be used. If not provided, the signatures default to
+            ``{"predict": {"batchable": False}}``. See :obj:`~bentoml.types.ModelSignature` for more
+            details.
+        labels (``dict[str, str]``, optional):
+            A default set of management labels to be associated with the model. An example is
+            ``{"training-set": "data-1"}``.
+        custom_objects (``dict[str, Any]``, optional):
+            Custom objects to be saved with the model. An example is
+            ``{"my-normalizer": normalizer}``.
 
+            Custom objects are currently serialized with cloudpickle, but this implementation is
+            subject to change.
+        metadata (``dict[str, Any]``, optional):
+            Metadata to be associated with the model. An example is ``{"max_depth": 2}``.
+
+            Metadata is intended for display in model management UI and therefore must be a default
+            Python type, such as ``str`` or ``int``.
     Returns:
-        :obj:`~bentoml.Tag`: A :obj:`tag` with a format `name:version` where `name` is the user-defined model's name, and a generated `version` by BentoML.
+        :obj:`~bentoml.Tag`: A :obj:`tag` with a format `name:version` where `name` is the
+        user-defined model's name, and a generated `version` by BentoML.
 
-    Examples:
+    Example:
 
     .. code-block:: python
 
@@ -176,121 +166,92 @@ def save(
             params, lgb_train, num_boost_round=20, valid_sets=lgb_eval
         )
 
+        # `save` the booster to BentoML modelstore:
         tag = bentoml.lightgbm.save("my_lightgbm_model", gbm, booster_params=params)
-    """  # noqa
-    context: t.Dict[str, t.Any] = {
-        "framework_name": "lightgbm",
-        "pip_dependencies": [f"lightgbm=={get_pkg_version('lightgbm')}"],
-    }
+    """  # noqa: LN001
+
+    # Ensure that `model` is actually the Booster object, and not for example one of the scikit-learn wrapper objects.
+    if not isinstance(model, lgb.basic.Booster):
+        # Deal with a LightGBM issue (https://github.com/microsoft/LightGBM/issues/3014)
+        if not hasattr(model, "fitted_"):
+            model.fitted_ = True
+
+        try:
+            model = model.booster_
+        except AttributeError as e:
+            logger.error("Unable to obtain a lightgbm.basic.Booster from the specified model.")
+            raise e
+
+    assert isinstance(model, lgb.basic.Booster), "Unable to obtain a lightgbm.basic.Booster from the specified model."
+
+    context: ModelContext = ModelContext(
+        framework_name="lightgbm",
+        framework_versions={"lightgbm": get_pkg_version("lightgbm")},
+    )
+
+    if signatures is None:
+        logger.info(
+            'Using default model signature `{"predict": {"batchable": False}}` for LightGBM model'
+        )
+        signatures = {
+            "predict": {"batchable": False},
+        }
 
     with bentoml.models.create(
         name,
         module=MODULE_NAME,
+        api_version=API_VERSION,
+        signatures=signatures,
         labels=labels,
         custom_objects=custom_objects,
-        options=booster_params,
-        context=context,
         metadata=metadata,
-    ) as _model:
+        context=context,
+    ) as bento_model:
+        model.save_model(bento_model.path_of(MODEL_FILENAME))
 
-        _model.info.options["sklearn_api"] = False
-        if any(
-            isinstance(model, _)
-            for _ in [
-                lgb.LGBMModel,
-                lgb.LGBMClassifier,
-                lgb.LGBMRegressor,
-                lgb.LGBMRanker,
-            ]
-        ):
-            joblib.dump(model, _model.path_of(f"{SAVE_NAMESPACE}{PKL_EXT}"))
-            _model.info.options["sklearn_api"] = True
-        else:
-            model.save_model(_model.path_of(f"{SAVE_NAMESPACE}{TXT_EXT}"))  # type: ignore
-
-        return _model.tag
+        return bento_model.tag
 
 
-class _LightGBMRunner(BaseModelRunner):
-    @inject
-    def __init__(
-        self,
-        tag: t.Union[Tag, str],
-        infer_api_callback: str,
-        booster_params: t.Optional[t.Dict[str, t.Union[str, int]]],
-        name: t.Optional[str] = None,
-    ):
-        super().__init__(tag, name=name)
-        self._booster_params = booster_params if booster_params is not None else {}
-        self._infer_api_callback = infer_api_callback
+def get_runnable(bento_model: bentoml.Model) -> t.Type[bentoml.Runnable]:
+    """
+    Private API: use :obj:`~bentoml.Model.to_runnable` instead.
+    """
 
-        self._predict_fn: t.Any = None
-        self._model: t.Any = None
+    class LightGBMRunnable(bentoml.Runnable):
+        SUPPORT_NVIDIA_GPU = False  # afaik LightGBM only supports GPU during training, not for inference.
+        SUPPORT_CPU_MULTI_THREADING = True
 
-    def _is_gpu(self):
-        try:
-            if "device" in self._booster_params:
-                assert isinstance(self._booster_params["device"], str)
-                return "gpu" in self._booster_params["device"]
-        except KeyError:
-            return False
+        def __init__(self):
+            super().__init__()
+            self.model = load_model(bento_model)
 
-    @property
-    def num_replica(self) -> int:
-        if self._is_gpu() and self.resource_quota.nvidia_gpu:
-            return self.resource_quota.nvidia_gpu
-        return 1
+            self.predict_fns: dict[str, t.Callable[..., t.Any]] = {}
+            for method_name in bento_model.info.signatures:
+                try:
+                    self.predict_fns[method_name] = getattr(self.model, method_name)
+                except AttributeError:
+                    raise InvalidArgument(
+                        f"No method with name {method_name} found for LightGBM model of type {self.model.__class__}"
+                    )
 
-    def _setup(self) -> None:
-        self._model = load(
-            tag=self._tag,
-            booster_params=self._booster_params,
-            model_store=self.model_store,
+    def add_runnable_method(method_name: str, options: ModelSignature):
+        def _run(
+            self: LightGBMRunnable,
+            input_data: ext.NpNDArray | ext.PdDataFrame,
+        ) -> ext.NpNDArray:
+            res = self.predict_fns[method_name](input_data)
+            return np.asarray(res)
+
+        LightGBMRunnable.add_method(
+            _run,
+            name=method_name,
+            batchable=options.batchable,
+            batch_dim=options.batch_dim,
+            input_spec=options.input_spec,
+            output_spec=options.output_spec,
         )
-        self._predict_fn = getattr(self._model, self._infer_api_callback)
 
-    def _run_batch(self, input_data: "ext.NpNDArray") -> "ext.NpNDArray":  # type: ignore[reportIncompatibleMethodOverride]
-        return self._predict_fn(input_data)
+    for method_name, options in bento_model.info.signatures.items():
+        add_runnable_method(method_name, options)
 
-
-@inject
-def load_runner(
-    tag: t.Union[str, Tag],
-    infer_api_callback: str = "predict",
-    *,
-    booster_params: t.Optional[t.Dict[str, t.Union[str, int]]] = None,
-    name: t.Optional[str] = None,
-) -> "_LightGBMRunner":
-    """
-    Runner represents a unit of serving logic that can be scaled horizontally to
-    maximize throughput. :func:`bentoml.lightgbm.load_runner` implements a Runner class that
-    wrap around a LightGBM model, which optimize it for the BentoML runtime.
-
-    Args:
-        tag (:code:`Union[str, Tag]`):
-            Tag of a saved model in BentoML modelstore.
-        infer_api_callback (:code:`str`, `optional`, default to :code:`predict`):
-            Inference API callback from given model. If not specified, BentoML will use default :code:`predict`.
-            Users can also choose to use :code:`predict_proba` for supported model.
-        booster_params (:code:`Dict[str, Union[str, int]]`, `optional`, defaults to `None`):
-            Parameters for boosters. Refers to `Parameters Doc <https://lightgbm.readthedocs.io/en/latest/Parameters.html>`_
-            for more information.
-
-    Returns:
-        :obj:`~bentoml._internal.runner.Runner`: Runner instances for :mod:`bentoml.lightgbm` model
-
-    Examples:
-
-    .. code-block:: python
-
-        import bentoml
-
-        runner = bentoml.lightgbm.load_runner("my_lightgbm_model:latest")
-        runner.run_batch(X_test, num_iteration=gbm.best_iteration)
-    """
-    return _LightGBMRunner(
-        tag=tag,
-        infer_api_callback=infer_api_callback,
-        name=name,
-        booster_params=booster_params,
-    )
+    return LightGBMRunnable
---
+++ tests/integration/frameworks/models/lightgbm.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import numpy as np
+import pandas as pd
+import lightgbm as lgb
+from sklearn.datasets import load_breast_cancer
+
+import bentoml
+
+from . import FrameworkTestModel
+from . import FrameworkTestModelInput as Input
+from . import FrameworkTestModelConfiguration as Config
+
+framework = bentoml.lightgbm
+
+# read in data
+cancer = load_breast_cancer()  # type: ignore (incomplete sklearn types)
+dt = lgb.basic.Dataset(cancer.data, label=cancer.target)  # type: ignore
+
+# specify parameters via map
+param = {"num_leaves": 31, "eta": 0.1, "num_iterations": 20, "objective": "softmax", "num_class": 2}
+
+
+cancer_model = FrameworkTestModel(
+    name="cancer",
+    model=lgb.train(param, dt),
+    configurations=[
+        Config(
+            test_inputs={
+                "predict": [
+                    Input(
+                        input_args=[np.array([cancer.data[0]])],
+                        expected=lambda out: np.isclose(
+                            out, [[0.8152496, 0.1847504]]
+                        ).all(),
+                    ),
+                    Input(
+                        input_args=[np.array([cancer.data[1]])],
+                        expected=lambda out: np.isclose(
+                            out, [[0.92220132, 0.07779868]]
+                        ).all(),
+                    ),
+                ],
+            },
+        ),
+        Config(
+            test_inputs={
+                "predict": [
+                    Input(
+                        input_args=[pd.DataFrame([cancer.data[0]])],
+                        expected=lambda out: np.isclose(
+                            out, [[0.8152496, 0.1847504]]
+                        ).all(),
+                    ),
+                    Input(
+                        input_args=[pd.DataFrame([cancer.data[1]])],
+                        expected=lambda out: np.isclose(
+                            out, [[0.92220132, 0.07779868]]
+                        ).all(),
+                    ),
+                ],
+            },
+        ),
+    ],
+)
+models: list[FrameworkTestModel] = [cancer_model]

e16d049dedb454065ff858089c254aa10ed9306c
Sean Sheng <s3sheng@gmail.com>
2022-06-16 12:03:23 -0700

    Update log.py

--- bentoml/_internal/log.py
+++ bentoml/_internal/log.py
@@ -43,7 +43,9 @@ CLI_LOGGING_CONFIG: dict[str, t.Any] = {
     "root": {"level": logging.WARNING},
 }
 
-TRACED_LOG_FORMAT = "%(asctime)s %(levelname_bracketed)s %(component)s %(message)s%(trace_msg)s"
+TRACED_LOG_FORMAT = (
+    "%(asctime)s %(levelname_bracketed)s %(component)s %(message)s%(trace_msg)s"
+)
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 
 SERVER_LOGGING_CONFIG: dict[str, t.Any] = {

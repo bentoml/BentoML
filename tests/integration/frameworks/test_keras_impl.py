8893026988e44ca966205d2dacbe530a489212c9
bojiang <bojiang_@outlook.com>
2022-06-21 20:47:05 +0800

    fix for ci

--- tests/integration/frameworks/test_keras_impl.py
+++ tests/integration/frameworks/test_keras_impl.py
@@ -5,18 +5,51 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 import pytest
+import tensorflow as tf
+import tensorflow.keras as keras
 
 import bentoml
 from tests.utils.helpers import assert_have_file_extension
-from tests.utils.frameworks.tensorflow_utils import CustomLayer
-from tests.utils.frameworks.tensorflow_utils import custom_activation
-from tests.utils.frameworks.tensorflow_utils import KerasSequentialModel
-
-if TYPE_CHECKING:
-    import tensorflow.keras as keras
-
 
 MODEL_NAME = __name__.split(".")[-1]
+
+
+def custom_activation(x):
+    return tf.nn.tanh(x) ** 2
+
+
+class CustomLayer(keras.layers.Layer):
+    def __init__(self, units=32, **kwargs):
+        super(CustomLayer, self).__init__(**kwargs)
+        self.units = tf.Variable(units, name="units")
+
+    def call(self, inputs, training=False):
+        if training:
+            return inputs * self.units
+        else:
+            return inputs
+
+    def get_config(self):
+        config = super(CustomLayer, self).get_config()
+        config.update({"units": self.units.numpy()})
+        return config
+
+
+def KerasSequentialModel() -> keras.models.Model:
+    net = keras.models.Sequential(
+        (
+            keras.layers.Dense(
+                units=1,
+                input_shape=(5,),
+                use_bias=False,
+                kernel_initializer=keras.initializers.Ones(),
+            ),
+        )
+    )
+    opt = keras.optimizers.Adam(0.002, 0.5)
+    net.compile(optimizer=opt, loss="binary_crossentropy", metrics=["accuracy"])
+    return net
+
 
 test_data = [1, 2, 3, 4, 5]
 res = KerasSequentialModel().predict(np.array([test_data]))

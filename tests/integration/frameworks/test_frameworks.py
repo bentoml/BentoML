d868501b14a555347fbc90cb90c140ea9fb4fc25
bojiang <bojiang_@outlook.com>
2022-06-21 20:48:08 +0800

    clean commented codes

--- tests/integration/frameworks/test_frameworks.py
+++ tests/integration/frameworks/test_frameworks.py
@@ -152,25 +152,7 @@ def test_load(
 ):
     for configuration in test_model.configurations:
         model = framework.load_model(saved_model)
-
         configuration.check_model(model, Resource())
-
-        """
-        for method, inps in configuration.test_inputs.items():
-            for inp in inps:
-                args = [inp.preprocess(arg) for arg in inp.input_args]
-                kwargs = {
-                    key: inp.preprocess(kwarg)
-                    for key, kwarg in inp.input_kwargs.items()
-                }
-                if test_model.model_method_caller:
-                    out = test_model.model_method_caller(
-                        test_model, method, args, kwargs
-                    )
-                else:
-                    out = getattr(model, method)(*args, **kwargs)
-                inp.check_output(out)
-                """
 
 
 def test_runner_batching(

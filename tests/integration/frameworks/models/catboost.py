0e3151a78a77238c6f94c0296c440d1ebb2ac2ba
bojiang <bojiang_@outlook.com>
2022-06-30 13:26:01 +0800

    fix: typo

--- tests/integration/frameworks/models/catboost.py
+++ tests/integration/frameworks/models/catboost.py
@@ -50,7 +50,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 classification_model = FrameworkTestModel(
     name="classification",
     save_kwargs={
-        "signature": {
+        "signatures": {
             "predict": {"batchable": False},
             "staged_predict": {"batchable": False},
         }

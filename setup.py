e87f42754c199acb16bd9c8037b5884413ab5a3e
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-07-16 16:58:53 -0700

    fix: setup.py generation
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,69 +1,46 @@
 from __future__ import annotations
 
 import os
-import sys
-import typing as t
-import logging
-import traceback
-import subprocess
 from pathlib import Path
 
-import setuptools
+import pkg_resources
+from setuptools import setup
+from grpc_tools.protoc import main as run_main  # type: ignore (unfinished type definition)
 
 GIT_ROOT = Path(os.path.abspath(__file__)).parent
 
+proto_path = GIT_ROOT.joinpath("protos")
+
 gen_stub_path: str = os.path.join(GIT_ROOT, "bentoml", "protos")
 
-logger = logging.getLogger(__name__)
+proto_include = pkg_resources.resource_filename("grpc_tools", "_proto")
 
 
-def gen_args(
-    proto_path: str | Path,
-    protofile: str,
-    *,
-    grpc_out: bool = False,
-) -> list[str]:
-    args: list[str] = [f"-I{str(proto_path)}", f"--python_out={gen_stub_path}"]
+def gen_args(file: str, *, grpc_out: bool = False) -> list[str]:
+    args = [
+        f"-I{proto_include}",
+        f"-I{str(proto_path)}",
+        f"--python_out={gen_stub_path}",
+    ]
     if grpc_out:
         args.append(f"--grpc_python_out={gen_stub_path}")
-    args.append(os.path.join(proto_path, protofile))
+
+    if not file.endswith(".proto"):
+        file += ".proto"
+    file_path = proto_path.joinpath(file)
+    if not file_path.exists():
+        raise FileNotFoundError(f"{str(file_path)} not found")
+
+    args.append(file_path.__fspath__())
+
     return args
 
 
-def run_protoc(filename: str, **kwargs: t.Any) -> None:
-    # Run protoc for the given filename.
-    logger.info("Generating protobuf stub for %s", filename)
-    cmd = [sys.executable, "-m", "grpc_tools.protoc"]
-
-    proto_path = GIT_ROOT.joinpath("protos")
-    if not filename.endswith(".proto"):
-        filename = f"{filename}.proto"
-    file_path = proto_path.joinpath(filename)
-
-    if not file_path.exists():
-        raise FileNotFoundError(f"Path {file_path.__fspath__()} does not exist.")
-
-    cmd.extend(gen_args(proto_path, filename, **kwargs))
-    logger.debug("Running: %s", cmd)
-
-    try:
-        subprocess.run(cmd, capture_output=True, check=True)
-    except subprocess.CalledProcessError:
-        traceback.print_exc()
-        raise
-
-
 if __name__ == "__main__":
-    try:
-        # generate io_descriptors_pb2.py
-        run_protoc("payload")
 
-        # generate service_pb2.py and service_grpc_pb2.py
-        run_protoc("service", grpc_out=True)
-        logger.info(f"Successfully generated gRPC stubs to {gen_stub_path}.")
+    # Run before setup
+    run_main(gen_args("payload"))
+    run_main(gen_args("service", grpc_out=True))
 
-        # run setuptools.build_meta
-        sys.exit(setuptools.setup())
-    except subprocess.CalledProcessError as e:
-        logger.error(e)
-        sys.exit(1)
+    # run setuptools.setup()
+    setup()

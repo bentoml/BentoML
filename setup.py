1609f11f9686e4a85b71cbe7dd5eaa28037e176b
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-07-26 12:56:52 -0700

    revert: remove errors and status code from message
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@ from __future__ import annotations
 
 import os
 import sys
-import typing as t
 import subprocess
-import importlib.util
 from pathlib import Path
 
 from setuptools import setup
@@ -19,20 +17,9 @@ _VERSION_MAP = {
 }
 
 
-def source_locations(pkg: str) -> str:
-    spec = importlib.util.find_spec(pkg)
-    if not spec:
-        raise RuntimeError(
-            f"{pkg} not found. Make sure to add to both pyproject.toml and setup.cfg."
-        )
-    (location,) = spec.submodule_search_locations  # type: ignore (unfinished type)
-    return t.cast(str, location)
-
-
 def get_args(parent_path: str, *paths: str, grpc_out: bool = False) -> list[str]:
     args = [
         "-I.",
-        f"-I{os.path.dirname(source_locations('google'))}",  # include common googleapis stubs.
         "--python_out=.",
         "--mypy_out=.",
     ]

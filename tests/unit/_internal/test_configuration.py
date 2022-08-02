b8fafdac3db8c939dca9ea04caebc36a7f937fd4
jjmachan <jamesjithin97@gmail.com>
2022-08-02 16:38:45 +0530

    more asserts

--- tests/unit/_internal/test_configuration.py
+++ tests/unit/_internal/test_configuration.py
@@ -39,4 +39,28 @@ def test_bentoml_configuration_runner_override():
         # test_runner_1
         test_runner_1 = runner_cfg["test_runner_1"]
         assert test_runner_1["batching"]["enabled"] is False
+        assert test_runner_1["batching"]["max_batch_size"] == 10
         assert test_runner_1["logging"]["access"]["enabled"] is False
+        # assert test_runner_1["resources"]["cpu"] == 4
+
+        # test_runner_2
+        test_runner_2 = runner_cfg["test_runner_2"]
+        assert test_runner_2["batching"]["enabled"] is False
+        assert test_runner_2["batching"]["max_batch_size"] == 10
+        assert test_runner_2["logging"]["access"]["enabled"] is False
+        assert test_runner_2["resources"]["cpu"] == 2
+
+        # test_runner_gpu
+        test_runner_gpu = runner_cfg["test_runner_gpu"]
+        assert test_runner_gpu["batching"]["enabled"] is False
+        assert test_runner_gpu["batching"]["max_batch_size"] == 10
+        assert test_runner_gpu["logging"]["access"]["enabled"] is False
+        assert test_runner_gpu["resources"]["cpu"] == 4  # should use global
+        assert test_runner_gpu["resources"]["nvidia.com/gpu"] == 1
+
+        # test_runner_batching
+        test_runner_batching = runner_cfg["test_runner_batching"]
+        assert test_runner_batching["batching"]["enabled"] is True
+        assert test_runner_batching["batching"]["max_batch_size"] == 10
+        assert test_runner_batching["logging"]["access"]["enabled"] is True
+        assert test_runner_batching["resources"]["cpu"] == 4  # should use global

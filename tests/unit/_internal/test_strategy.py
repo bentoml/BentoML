3123afa8099fdc9cd519a88d94fff06e9058edf9
jjmachan <jamesjithin97@gmail.com>
2022-08-05 00:28:32 +0530

    patched tests

--- tests/unit/_internal/test_strategy.py
+++ tests/unit/_internal/test_strategy.py
@@ -1,6 +1,9 @@
 import os
+import typing as t
 
 import bentoml
+from bentoml._internal.runner import strategy
+from bentoml._internal.resource import get_resource as resource_get_resource
 from bentoml._internal.runner.strategy import DefaultStrategy
 
 
@@ -8,7 +11,12 @@ class GPURunnable(bentoml.Runnable):
     SUPPORTED_RESOURCES = ("nvidia.com/gpu",)
 
 
-def test_default_gpu_strategy():
+def unvalidated_get_resource(x: t.Dict[str, t.Any], y: str):
+    return resource_get_resource(x, y, validate=False)
+
+
+def test_default_gpu_strategy(monkeypatch):
+    monkeypatch.setattr(strategy, "get_resource", unvalidated_get_resource)
     DefaultStrategy.get_worker_count(GPURunnable, {"nvidia.com/gpu": 2}) == 2
     DefaultStrategy.get_worker_count(GPURunnable, {"nvidia.com/gpu": 0}) == 0
     DefaultStrategy.get_worker_count(GPURunnable, {"nvidia.com/gpu": [2, 7]}) == 2
@@ -17,5 +25,5 @@ def test_default_gpu_strategy():
     assert os.environ.get("CUDA_VISIBLE_DEVICES") == "0"
     DefaultStrategy.setup_worker(GPURunnable, {"nvidia.com/gpu": 2}, 2)
     assert os.environ.get("CUDA_VISIBLE_DEVICES") == "1"
-    DefaultStrategy.setup_worker(GPURunnable, {"nvidia.com/gpu": [2, 7]}, 2) == 7
+    DefaultStrategy.setup_worker(GPURunnable, {"nvidia.com/gpu": [2, 7]}, 2)
     assert os.environ.get("CUDA_VISIBLE_DEVICES") == "7"

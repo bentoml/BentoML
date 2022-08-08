7d2a6e92528910caf768addb56cf9a329f1d56bf
Sean Sheng <s3sheng@gmail.com>
2022-08-08 00:02:19 -0700

    Update json.py

--- bentoml/_internal/io_descriptors/json.py
+++ bentoml/_internal/io_descriptors/json.py
@@ -38,7 +38,7 @@ else:
     )
 
 
-JSONType = t.Optional[str, t.Dict[str, t.Any], "pydantic.BaseModel"]
+JSONType = t.Union[str, t.Dict[str, t.Any], "pydantic.BaseModel", None]
 
 MIME_TYPE_JSON = "application/json"
 

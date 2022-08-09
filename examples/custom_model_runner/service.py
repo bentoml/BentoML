4acddb5489b9c6064a903ce2cd62dd27ea6f4ac7
Chaoyu <paranoyang@gmail.com>
2022-08-09 05:08:12 -0700

    auto format

--- examples/custom_model_runner/service.py
+++ examples/custom_model_runner/service.py
@@ -1,13 +1,16 @@
 import numpy as np
+
 import bentoml
 
 mnist_model = bentoml.pytorch.get("mnist_cnn:latest")
 mnist_runnable = mnist_model.to_runnable()
 
+
 class CustomMnistRunnable(mnist_runnable):
     def __init__(self):
         super().__init__()
         import torch
+
         print("Running on device:", self.device_id)
         print("Running on torch version:", torch.__version__)
 
@@ -16,13 +19,16 @@ class CustomMnistRunnable(mnist_runnable):
         output = super().__call__(input_array)
         return output.argmax(dim=1)
 
-mnist_runner = bentoml.Runner(CustomMnistRunnable, method_configs={
-    '__call__': {
-        'max_batch_size': 50, 'max_latency_ms': 600
-    }
-})
 
-svc = bentoml.Service("pytorch_mnist_demo", runners=[mnist_runner], models=[mnist_model])
+mnist_runner = bentoml.Runner(
+    CustomMnistRunnable,
+    method_configs={"__call__": {"max_batch_size": 50, "max_latency_ms": 600}},
+)
+
+svc = bentoml.Service(
+    "pytorch_mnist_demo", runners=[mnist_runner], models=[mnist_model]
+)
+
 
 @svc.api(input=bentoml.io.Image(), output=bentoml.io.NumpyNdarray())
 def predict(input_img):

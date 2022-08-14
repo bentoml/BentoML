41ce0ef7f35dd40e560a0cbf0cbbf73e3f11c00a
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-08-14 08:28:07 -0700

    refactor(config): bentoml.protos to bentoml.grpc
    add dynamic stubs generation
    
    update buf configuration to ignore standard names
    
    include generated stubs inside distribution
    
    readthedocs build with gRPC dependencies
    
    move coverage config to pyproject.toml
    
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,43 @@
-import setuptools
+from __future__ import annotations
+
+import os
+import sys
+import subprocess
+from pathlib import Path
+
+from setuptools import setup
+
+GIT_ROOT = Path(os.path.abspath(__file__)).parent
+
+_VERSION_MAP = {
+    "v1": {
+        ("service.proto", "service_test.proto"): {"grpc_out": True},
+    },
+}
+
+
+def get_args(parent_path: str, *paths: str, grpc_out: bool = False) -> list[str]:
+    args = [
+        "-I.",
+        "--python_out=.",
+        "--mypy_out=.",
+    ]
+    if grpc_out:
+        args.extend(["--grpc_python_out=.", "--mypy_grpc_out=."])
+    args.extend([os.path.join(parent_path, path) for path in paths])
+    return args
+
 
 if __name__ == "__main__":
-    setuptools.setup()
+
+    binary = [sys.executable, "-m", "grpc_tools.protoc"]
+
+    for version, file_map in _VERSION_MAP.items():
+        version_path = os.path.join("bentoml", "grpc", version)
+
+        # Generate bentoml stubs
+        for paths, options in file_map.items():
+            subprocess.check_call([*binary, *get_args(version_path, *paths, **options)])
+
+    # run setuptools.setup()
+    setup()

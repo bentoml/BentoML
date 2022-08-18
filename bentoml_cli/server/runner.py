4878d6a9e945e787d32421c47a5e661790f1c034
bojiang <bojiang_@outlook.com>
2022-08-18 19:15:47 +0800

    try to fix the env on action

--- bentoml_cli/server/runner.py
+++ bentoml_cli/server/runner.py
@@ -28,9 +28,8 @@ import click
 )
 @click.option(
     "--worker-id",
-    required=False,
+    required=True,
     type=click.INT,
-    default=None,
     help="If set, start the server as a bare worker with the given worker ID. Otherwise start a standalone server with a supervisor process.",
 )
 @click.option(
@@ -48,7 +47,7 @@ def main(
     bind: str,
     working_dir: t.Optional[str],
     no_access_log: bool,
-    worker_id: int | None,
+    worker_id: int,
     worker_env_map: str | None,
 ) -> None:
     """
@@ -66,34 +65,6 @@ def main(
         worker_id: (Optional) if set, the runner will be started as a worker with the given ID. Important: begin from 1.
         worker_env_map: (Optional) the environment variables to pass to the worker process. The format is a JSON string, e.g. '{0: {\"CUDA_VISIBLE_DEVICES\": 0}}'.
     """
-    if worker_id is None:
-
-        # Start a standalone server with a supervisor process
-        from circus.watcher import Watcher
-
-        from bentoml_cli.utils import unparse_click_params
-        from bentoml._internal.utils.circus import create_standalone_arbiter
-        from bentoml._internal.utils.circus import create_circus_socket_from_uri
-
-        circus_socket = create_circus_socket_from_uri(bind, name=runner_name)
-        params = ctx.params
-        params["bind"] = f"fd://$(circus.sockets.{runner_name})"
-        params["worker_id"] = "$(circus.wid)"
-        params["no_access_log"] = no_access_log
-        watcher = Watcher(
-            name=f"runner_{runner_name}",
-            cmd=sys.executable,
-            args=["-m", "bentoml_cli.server.runner"]
-            + unparse_click_params(params, ctx.command.params, factory=str),
-            copy_env=True,
-            numprocesses=1,
-            stop_children=True,
-            use_sockets=True,
-            working_dir=working_dir,
-        )
-        arbiter = create_standalone_arbiter(watchers=[watcher], sockets=[circus_socket])
-        arbiter.start()
-        return
 
     # setting up the environment for the worker process
     assert (

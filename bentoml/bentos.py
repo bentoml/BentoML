f0e0bf0a426e2edae6c536160513a993c7cf5438
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-08-31 15:37:37 -0700

    docs: fix bentoml containerize command help message (#2957)
    Co-authored-by: Sauyon Lee <2347889+sauyon@users.noreply.github.com>

--- bentoml/bentos.py
+++ bentoml/bentos.py
@@ -380,7 +380,7 @@ def build_bentofile(
 @inject
 def containerize(
     tag: Tag | str,
-    docker_image_tag: str | t.List[str] | None = None,
+    docker_image_tag: t.List[str] | None = None,
     *,
     add_host: dict[str, str] | None = None,
     allow: t.List[str] | None = None,
@@ -418,7 +418,7 @@ def containerize(
 
     bento = _bento_store.get(tag)
     if not docker_image_tag:
-        docker_image_tag = str(bento.tag)
+        docker_image_tag = [str(bento.tag)]
 
     dockerfile_path = os.path.join("env", "docker", "Dockerfile")
 
@@ -465,9 +465,14 @@ def containerize(
             )
         return False
     else:
-        logger.info(f'Successfully built docker image "{docker_image_tag}"')
         logger.info(
-            f'To run your newly built Bento container, use: "docker run -it --rm -p 3000:3000 {docker_image_tag}"'
+            'Successfully built docker image for "%s" with tags "%s"',
+            str(bento.tag),
+            ",".join(docker_image_tag),
+        )
+        logger.info(
+            'To run your newly built Bento container, use one of the above tags, and pass it to "docker run". i.e: "docker run -it --rm -p 3000:3000 %s"',
+            docker_image_tag[0],
         )
         return True
 

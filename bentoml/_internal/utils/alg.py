80e382db0b32a3b9e6e17394ae9bcfb608a38dc1
bojiang <bojiang_@outlook.com>
2022-09-08 11:41:12 +0800

    add config; fix typing

--- bentoml/_internal/utils/alg.py
+++ bentoml/_internal/utils/alg.py
@@ -1,18 +1,23 @@
+from __future__ import annotations
+
 import time
+import typing as t
+
+T = t.TypeVar("T")
 
 
-class FixedBucket:
+class FixedBucket(t.Generic[T]):
     """
     Fixed size FIFO container.
     """
 
-    def __init__(self, size):
-        self._data = [None] * size
+    def __init__(self, size: int):
+        self._data: list[T | None] = [None] * size
         self._cur = 0
         self._size = size
         self._flag_full = False
 
-    def put(self, v):
+    def put(self, v: T):
         self._data[self._cur] = v
         self._cur += 1
         if self._cur == self._size:
@@ -30,7 +35,7 @@ class FixedBucket:
             return self._cur
         return self._size
 
-    def __getitem__(self, sl):
+    def __getitem__(self, sl: slice):
         if not self._flag_full:
             return self._data[: self._cur][sl]
         return (self._data[self._cur :] + self._data[: self._cur])[sl]
@@ -41,11 +46,11 @@ class TokenBucket:
     Dynamic token bucket
     """
 
-    def __init__(self, init_amount=0):
+    def __init__(self, init_amount: int = 0):
         self._amount = init_amount
         self._last_consume_time = time.time()
 
-    def consume(self, take_amount, avg_rate, burst_size):
+    def consume(self, take_amount: int, avg_rate: float, burst_size: int):
         now = time.time()
         inc = (now - self._last_consume_time) * avg_rate
         current_amount = min(inc + self._amount, burst_size)

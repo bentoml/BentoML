b6addfa1daf483d557376af6d5262c8587fa09c2
asaf <asaf@DSG-AsafAlina.lan>
2022-09-12 23:53:57 +0300

    add test CLI command description

--- bentoml_cli/test.py
+++ bentoml_cli/test.py
@@ -14,4 +14,14 @@ def add_test_command(cli: click.Group) -> None:
     def test(
             bento_tag: str,
     ) -> None:
+        """Test given Bento by providing the bento_tag.
+
+        \b
+        `bentoml test <bento>` command will run the tests spegstcified in the `bentofile.yaml` file against the given Bento.
+        For example: `bentoml test iris_classifier:v1.2.0`
+        In the background, `bentoml test` command will create a docker image of the given bento,
+        run a docker container from the docker image, and run the tests against the container.
+        For every test case, the test result will be printed to the console.
+        After the tests are finished, the docker image and the docker container will be removed.
+        """
         test_bento_bundle(bento_tag)

d5ec1ccaa7e7fbb07464e7195cd8c28388d0089c
asaf <asaf@DSG-AsafAlina.lan>
2022-09-14 11:41:40 +0300

    add __all__

--- bentoml/client/__init__.py
+++ bentoml/client/__init__.py
@@ -1,2 +1,4 @@
 from bentoml.client.test_client import Endpoint
 from bentoml.client.test_client import TestClient
+
+__all__ = ["Endpoint", "TestClient"]
--- bentoml/client/test_client/__init__.py
+++ bentoml/client/test_client/__init__.py
@@ -2,3 +2,5 @@ from bentoml.client.test_client.utils import is_equal
 from bentoml.client.test_client.utils import get_test_data
 from bentoml.client.test_client.client import Endpoint
 from bentoml.client.test_client.client import TestClient
+
+__all__ = ["is_equal", "get_test_data", "Endpoint", "TestClient"]

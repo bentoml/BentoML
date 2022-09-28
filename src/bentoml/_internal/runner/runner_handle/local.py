5b0ed05284cd04ef4119f7d8e5f50fa6979df45c
Quan Nguyen <86090707+quandollar@users.noreply.github.com>
2022-09-28 13:31:31 -0400

    implement is_ready in the local runner

--- src/bentoml/_internal/runner/runner_handle/local.py
+++ src/bentoml/_internal/runner/runner_handle/local.py
@@ -25,6 +25,9 @@ class LocalRunnerRef(RunnerHandle):
         self._runnable = runner.runnable_class(**runner.runnable_init_params)  # type: ignore
         self._limiter = None
 
+    def is_ready(self) -> bool:
+        return True
+
     def run_method(
         self,
         __bentoml_method: RunnerMethod[t.Any, P, R],

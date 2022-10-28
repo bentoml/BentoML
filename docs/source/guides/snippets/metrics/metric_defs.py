db550fa99915c989f301feb07faced50e149adaa
Sean Sheng <s3sheng@gmail.com>
2022-10-27 23:27:04 -0700

    Update docs/source/guides/snippets/metrics/metric_defs.py

--- docs/source/guides/snippets/metrics/metric_defs.py
+++ docs/source/guides/snippets/metrics/metric_defs.py
@@ -6,7 +6,7 @@ inference_duration = bentoml.metrics.Histogram(
     name="inference_duration",
     documentation="Duration of inference",
     labelnames=["nltk_version", "sentiment_cls"],
-    buckets=exponential_buckets(0.001, 1.5, 10.0),
+    buckets=(0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, float("inf")),
 )
 
 num_invocation = bentoml.metrics.Counter(

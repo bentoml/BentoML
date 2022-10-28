b5bf81d3149926cf17ea2087b31341a53231e4fb
Sean Sheng <s3sheng@gmail.com>
2022-10-27 23:27:25 -0700

    Update docs/source/guides/snippets/metrics/metric_defs.py

--- docs/source/guides/snippets/metrics/metric_defs.py
+++ docs/source/guides/snippets/metrics/metric_defs.py
@@ -9,8 +9,8 @@ inference_duration = bentoml.metrics.Histogram(
     buckets=(0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, float("inf")),
 )
 
-num_invocation = bentoml.metrics.Counter(
-    name="num_invocation",
-    documentation="Count total number of invocation for a given endpoint",
-    labelnames=["endpoint"],
+polarity_counter = bentoml.metrics.Counter(
+    name="polarity_total",
+    documentation="Count total number of analysis by polarity scores",
+    labelnames=["polarity"],
 )

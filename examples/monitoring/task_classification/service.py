dfd6f18302507496ef70d2c6f7475574cda5c56f
bojiang <bojiang_@outlook.com>
2022-11-02 15:26:29 +0800

    doc(example): monitoring example for classification tasks

---
+++ examples/monitoring/task_classification/service.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+import bentoml
+from bentoml.io import NumpyNdarray
+
+iris_clf_runner = bentoml.sklearn.get("iris_clf:latest").to_runner()
+
+svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
+
+
+@svc.api(
+    input=NumpyNdarray.from_sample(np.array([[4.9, 3.0, 1.4, 0.2]], dtype=np.double)),
+    output=NumpyNdarray(),
+)
+async def classify(input_series: np.ndarray) -> np.ndarray:
+    with bentoml.monitor("iris_classifier_prediction") as mon:
+        mon.log(
+            data=input_series[0][0],
+            name="sepal length",
+            role="feature",
+            data_type="numerical",
+        )
+        mon.log(
+            input_series[0][1],
+            name="sepal width",
+            role="feature",
+            data_type="numerical",
+        )
+        mon.log(
+            input_series[0][2],
+            name="petal length",
+            role="feature",
+            data_type="numerical",
+        )
+        mon.log(
+            input_series[0][3],
+            name="petal width",
+            role="feature",
+            data_type="numerical",
+        )
+        result = await iris_clf_runner.predict.async_run(input_series)
+        mon.log(result[0], name="pred", role="prediction", data_type="categorical")
+    return result

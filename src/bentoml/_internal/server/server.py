21d354714db47b1caf946759150bce9319d89b16
Quan Nguyen <86090707+quandollar@users.noreply.github.com>
2022-12-08 18:18:41 -0500

    move client import location to avoid circular import error

--- src/bentoml/_internal/server/server.py
+++ src/bentoml/_internal/server/server.py
@@ -5,15 +5,16 @@ Server class for getting the Bento client and managing server process
 from __future__ import annotations
 
 import subprocess
-from bentoml.client import Client
 
 class Server:
+    
     def __init__(self, process: subprocess.Popen[bytes], host: str, port: int) -> None:
         self._process = process
         self._host = host
         self._port = port
 
-    def get_client(self) -> Client:
+    def get_client(self):
+        from bentoml.client import Client
         Client.wait_until_server_is_ready(self._host, self._port, 10)
         return Client.from_url(f"http://localhost:{self._port}")
 

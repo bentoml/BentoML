0024e6fd14ca3aec5599c2389ff8e326aeec066c
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-02-06 23:24:11 -0800

    fix: sorting
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- src/bentoml/client.py
+++ src/bentoml/client.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import json
 import typing as t
 import asyncio
-import functools
 import logging
+import functools
 from abc import ABC
 from abc import abstractmethod
 from http.client import HTTPConnection

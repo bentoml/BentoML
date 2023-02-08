64ab218cfacd3ffd9ea320460bf535d1c9f41d5e
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-02-08 09:56:42 -0800

    fix: increase check_delay to address exception in arbiter callback
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- src/bentoml/_internal/utils/circus/__init__.py
+++ src/bentoml/_internal/utils/circus/__init__.py
@@ -53,5 +53,9 @@ def create_standalone_arbiter(watchers: list[Watcher], **kwargs: t.Any) -> Arbit
                 watchers,
                 endpoint=f"tcp://127.0.0.1:{endpoint_port}",
                 pubsub_endpoint=f"tcp://127.0.0.1:{pubsub_port}",
+                # NOTE: increase check delay to 5s to avoid
+                # ConfclitError raised when arbiter is running arbiter_stop command
+                # See https://github.com/bentoml/BentoML/pull/3540.
+                check_delay=5,
                 **kwargs,
             )

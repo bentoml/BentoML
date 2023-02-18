fadb5e102ac6b3d58f070c1e391ffc1499c679ae
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-02-17 20:54:15 -0800

    chore: export imports
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- src/bentoml/flax.py
+++ src/bentoml/flax.py
@@ -1,7 +1,7 @@
 from ._internal.frameworks.flax import get
 from ._internal.frameworks.flax import load_model
 from ._internal.frameworks.flax import save_model
-from ._internal.frameworks.flax import FlaxOptions as ModelOptions  # type: ignore # noqa
+from ._internal.frameworks.flax import FlaxOptions as ModelOptions
 from ._internal.frameworks.flax import get_runnable
 
-__all__ = ["load_model", "save_model", "get", "get_runnable"]
+__all__ = ["load_model", "save_model", "get", "get_runnable", "ModelOptions"]

648c78af7e40d9ce319c11e87bbc01d7519d0b38
Chaoyu <paranoyang@gmail.com>
2023-02-21 23:53:27 -0800

    update

---
+++ examples/fraud-detection-benchmark/fastapi_main_load_model.py
@@ -0,0 +1,27 @@
+import io
+import bentoml
+import pandas as pd
+import numpy as np
+
+from fastapi import FastAPI, Request
+from sample import sample_input
+
+model_tag = "ieee-fraud-detection-lg:latest"
+preprocessor = bentoml.xgboost.get(model_tag).custom_objects['preprocessor']
+fraud_model = bentoml.xgboost.load_model(model_tag)
+
+app = FastAPI()
+
+@app.post("/is_fraud")
+async def is_fraud(request: Request):
+    body = await request.body()
+    input_df = pd.read_json(io.BytesIO(body), dtype=True, orient="records")
+    input_df = input_df.astype(sample_input.dtypes)
+    input_features = preprocessor.transform(input_df)
+    results = fraud_model.predict_proba(input_features)
+    predictions = np.argmax(results, axis=1)  # 0 is not fraud, 1 is fraud
+    return {
+        "is_fraud": list(map(bool, predictions)),
+        "is_fraud_prob": results[:,1].tolist()
+    }
+

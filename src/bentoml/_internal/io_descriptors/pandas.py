7278959399081905c01b23710c135999c89446b8
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-02-22 22:16:56 -0800

    fix: make sure not to override user set values for from_sample
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- src/bentoml/_internal/io_descriptors/pandas.py
+++ src/bentoml/_internal/io_descriptors/pandas.py
@@ -427,8 +427,10 @@ class PandasDataFrame(
                 raise InvalidArgument(
                     f"Failed to create a 'pd.DataFrame' from sample {sample}: {e}"
                 ) from None
-        self._shape = sample.shape
-        self._columns = [str(i) for i in list(sample.columns)]
+        if self.shape is None:
+            self._shape = sample.shape
+        if self.columns is None:
+            self._columns = [str(i) for i in list(sample.columns)]
         if self._dtype is None:
             self._dtype = True  # infer dtype automatically
         return sample
@@ -930,8 +932,10 @@ class PandasSeries(
         """
         if not isinstance(sample, pd.Series):
             sample = pd.Series(sample)
-        self._dtype = sample.dtype
-        self._shape = sample.shape
+        if self._dtype is None:
+            self._dtype = sample.dtype
+        if self._shape is None:
+            self._shape = sample.shape
         return sample
 
     def input_type(self) -> LazyType[ext.PdSeries]:

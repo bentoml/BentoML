e7678a8090b07f5b44287a8905eb5e2634eff3d1
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-05-10 09:09:53 -0700

    feat(client): ensure backward-compatible client factory
    added auto-generated docs per function
    
    add supports for client.kwargs parsing
    
    ```python
    client.predict(input_series=np.array([1,2,3,4]))
    ```
    
    update protocol interface and use asyncio.run during destructor
    
    fix(client): create a client type protocol
    
    Move common to main metaclass
    
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- src/bentoml/_internal/service/openapi/__init__.py
+++ src/bentoml/_internal/service/openapi/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import typing as t
+import inspect
 from http import HTTPStatus
 from typing import TYPE_CHECKING
 from functools import lru_cache
@@ -28,7 +29,6 @@ from .specification import Components
 from .specification import OpenAPISpecification
 
 if TYPE_CHECKING:
-
     from .. import Service
     from ..inference_api import InferenceAPI
 
@@ -98,6 +98,14 @@ def generate_service_components(svc: Service) -> Components:
     return Components(**components)
 
 
+def get_func_kwds(api: InferenceAPI) -> list[str]:
+    sig = inspect.signature(api.func)
+    # We probably want to pop out ctx from the signature.
+    return (
+        list(sig.parameters)[:-1] if len(sig.parameters) == 2 else list(sig.parameters)
+    )
+
+
 def generate_spec(svc: Service, *, openapi_version: str = "3.0.2"):
     """Generate a OpenAPI specification for a service."""
     mounted_app_paths = {}
@@ -162,6 +170,7 @@ def generate_spec(svc: Service, *, openapi_version: str = "3.0.2"):
                         "consumes": [api.input.mime_type],
                         "produces": [api.output.mime_type],
                         "x-bentoml-name": api.name,
+                        "x-bentoml-func-kwds": get_func_kwds(api),
                         "summary": str(api),
                         "description": api.doc or "",
                         "requestBody": api.input.openapi_request_body(),

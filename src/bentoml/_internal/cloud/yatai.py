64991514c675f8c484018e00d231836033387b81
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-06-07 18:59:31 -0400

    chore(compression): disable accordingly
    follow #3933 and #3935
    
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- src/bentoml/_internal/cloud/yatai.py
+++ src/bentoml/_internal/cloud/yatai.py
@@ -197,7 +197,7 @@ class YataiClient(BaseCloudClient):
 
         with io.BytesIO() as tar_io:
             with self.spin(text=f'Creating tar archive for bento "{bento.tag}"..'):
-                with tarfile.open(fileobj=tar_io, mode="w:gz") as tar:
+                with tarfile.open(fileobj=tar_io, mode="w:") as tar:
 
                     def filter_(
                         tar_info: tarfile.TarInfo,
@@ -525,7 +525,7 @@ class YataiClient(BaseCloudClient):
                     f'[bold green]Finished downloading all bento "{_tag}" files'
                 )
                 tar_file.seek(0, 0)
-                tar = tarfile.open(fileobj=tar_file, mode="r:gz")
+                tar = tarfile.open(fileobj=tar_file, mode="r")
                 with self.spin(text=f'Extracting bento "{_tag}" tar file'):
                     with fs.open_fs("temp://") as temp_fs:
                         for member in tar.getmembers():
@@ -648,7 +648,7 @@ class YataiClient(BaseCloudClient):
 
         with io.BytesIO() as tar_io:
             with self.spin(text=f'Creating tar archive for model "{model.tag}"..'):
-                with tarfile.open(fileobj=tar_io, mode="w:gz") as tar:
+                with tarfile.open(fileobj=tar_io, mode="w:") as tar:
                     tar.add(model.path, arcname="./")
             tar_io.seek(0, 0)
             with self.spin(text=f'Start uploading model "{model.tag}"..'):
@@ -940,7 +940,7 @@ class YataiClient(BaseCloudClient):
                 f'[bold green]Finished downloading model "{_tag}" files'
             )
             tar_file.seek(0, 0)
-            tar = tarfile.open(fileobj=tar_file, mode="r:gz")
+            tar = tarfile.open(fileobj=tar_file, mode="r")
             with self.spin(text=f'Extracting model "{_tag}" tar file'):
                 with fs.open_fs("temp://") as temp_fs:
                     for member in tar.getmembers():

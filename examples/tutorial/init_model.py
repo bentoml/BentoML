b9617a7ffe534bb1f47deb8d99f87a28e1d1d432
Sauyon Lee <git@sjle.co>
2023-07-13 17:49:51 -0700

    fix black include regex

--- examples/tutorial/init_model.py
+++ examples/tutorial/init_model.py
@@ -5,9 +5,7 @@ import bentoml
 pipe = transformers.pipeline("text-classification")
 
 bentoml.transformers.save_model(
-  "text-classification-pipe",
-  pipe,
-  signatures={
-    "__call__": {"batchable": True}  # Enable dynamic batching for model
-  }
+    "text-classification-pipe",
+    pipe,
+    signatures={"__call__": {"batchable": True}},  # Enable dynamic batching for model
 )

8ca43bed1faa64a53d9057469396ca0a927b8918
Zhao Shenyang <dev@zsy.im>
2023-08-18 07:10:41 +0800

    fix import

--- src/bentoml/_internal/frameworks/diffusers_runners/__init__.py
+++ src/bentoml/_internal/frameworks/diffusers_runners/__init__.py
@@ -8,16 +8,13 @@ from typing import TYPE_CHECKING
 try:
     import diffusers
     import torch
-    from diffusers.loaders import LoraLoaderMixin
-    from diffusers.loaders import TextualInversionLoaderMixin
-    from diffusers.utils.import_utils import is_accelerate_available
-    from diffusers.utils.import_utils import is_torch_version
-    from diffusers.utils.import_utils import is_xformers_available
+    from diffusers.loaders import LoraLoaderMixin as LoraLoaderMixin
+    from diffusers.loaders import TextualInversionLoaderMixin as TextualInversionLoaderMixin
 except ImportError:  # pragma: no cover
     raise MissingDependencyException(
         "'diffusers' and 'transformers' is required in order to use module 'bentoml.diffusers_runners', install diffusers and its dependencies with 'pip install --upgrade diffusers transformers accelerate'. For more information, refer to https://github.com/huggingface/diffusers",
     )
 
 
-from . import stable_diffusion
-from . import stable_diffusion_xl
+from . import stable_diffusion as stable_diffusion
+from . import stable_diffusion_xl as stable_diffusion_xl

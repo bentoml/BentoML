268c9b87cbda42679c66cd74efbf5275ee7f07f0
Frost Ming <me@frostming.com>
2023-10-11 10:22:12 +0800

    feat: batch inference prototype
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/bentoml/_internal/runner/runnable.py
+++ src/bentoml/_internal/runner/runnable.py
@@ -139,8 +139,15 @@ class RunnableMethod(t.Generic[T, P, R]):
     _bentoml_runnable_method: None = None
 
     def __get__(self, obj: T, _: t.Type[T] | None = None) -> t.Callable[P, R]:
-        def method(*args: P.args, **kwargs: P.kwargs) -> R:
-            return self.func(obj, *args, **kwargs)
+        if is_async_callable(self.func):
+
+            async def method(*args: P.args, **kwargs: P.kwargs) -> R:
+                return await self.func(obj, *args, **kwargs)
+
+        else:
+
+            def method(*args: P.args, **kwargs: P.kwargs) -> R:
+                return self.func(obj, *args, **kwargs)
 
         return method
 

4d8a0e762f8d44a1cb2ba4f431d411958aaf4319
Frost Ming <me@frostming.com>
2023-11-08 18:01:22 +0800

    fix: use explicit api to get clients
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/bentoml_io/__init__.py
+++ src/bentoml_io/__init__.py
@@ -1,3 +1,11 @@
+from bentoml._internal.utils.pkg import pkg_version_info
+
+if (ver := pkg_version_info("pydantic")) < (2,):
+    raise ImportError(
+        f"bentoml_io runs on pydantic>=2.0.0, but the you have {'.'.join(map(str, ver))}. "
+        "Please upgrade it"
+    )
+
 from ._pydantic import add_custom_preparers
 from .api import api as api
 from .servable import Servable as Servable

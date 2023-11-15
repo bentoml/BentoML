899cd759718655eca2119eeb6b706976c8fbf370
Jinyang Liu <liujinyang@bigtiger.me>
2023-11-15 22:04:49 +0800

    refactor: webui refactor FormField component

--- webui/src/components/FormField.tsx
+++ webui/src/components/FormField.tsx
@@ -1,7 +1,8 @@
 import { useCallback } from 'react'
-import { Input } from 'baseui/input'
-import { Checkbox } from 'baseui/checkbox'
 import type { DataType } from '../types'
+import Input from './form/Input'
+import InputNumber from './form/InputNumber'
+import Checkbox from './form/Checkbox'
 import JSONInput from './form/JSONInput'
 
 interface IFieldProps<T> {
@@ -19,24 +20,31 @@ function renderExample(examples?: unknown[]) {
 
 export default function FormField<T = unknown>({ value, schema, onChange }: IFieldProps<T>) {
   const placeholder = renderExample(schema.examples)
-
-  const handleChange = useCallback((e: unknown) => {
-    const { target } = e as React.ChangeEvent<HTMLInputElement>
-    onChange?.((target.type === 'checkbox' ? target.checked : target.value) as T)
-  }, [onChange])
+  const handleChange = useCallback((val: unknown) => onChange?.(val as T), [onChange])
 
   switch (schema.type) {
-    case 'string':
-      return <Input value={value as string} placeholder={placeholder} onChange={handleChange} />
+    case 'integer':
     case 'number':
-      return <Input value={value as string} placeholder={placeholder} onChange={handleChange} type="number" />
+      return (
+        <InputNumber
+          step={schema.type === 'integer' ? 1 : 0.01}
+          value={value as unknown as number}
+          maximum={schema.maximum}
+          minimum={schema.minimum}
+          exclusiveMaximum={schema.exclusiveMaximum}
+          exclusiveMinimum={schema.exclusiveMinimum}
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+      )
     case 'boolean':
-      return <Checkbox checked={value as boolean} onChange={handleChange} />
+      return <Checkbox checked={value as unknown as boolean} onChange={handleChange} />
     case 'object':
     case 'tensor':
     case 'dataframe':
-      return <JSONInput value={value as string} onChange={v => onChange?.(v as T)} />
+      return <JSONInput value={value as unknown as string} onChange={handleChange} />
+    case 'string':
     default:
-      return <Input value={value as string} placeholder={placeholder} onChange={handleChange} />
+      return <Input value={value as unknown as string} placeholder={placeholder} onChange={handleChange} />
   }
 }

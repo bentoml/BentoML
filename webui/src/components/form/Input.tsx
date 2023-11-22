86c0a0bea7957992578ed9930f9064b5c0d687cd
Jinyang Liu <liujinyang@bigtiger.me>
2023-11-22 09:36:23 +0800

    refactor: form component and add file component (#4296)
    * fix: webui InputNumber range check fails when min or max equals 0
    
    * refactor: webui refactor some component about form
    
    * feat: webui add integer feature for InputNumber
    
    * feat: webui update function of mount app function
    
    * feat: webui form support array type and object type
    
    * fix: webui InputNumebr both max and min lack the dep step
    
    * feat: webui add file component

--- webui/src/components/form/Input.tsx
+++ webui/src/components/form/Input.tsx
@@ -1,12 +1,15 @@
 import type { InputProps } from 'baseui/input'
+import { connect } from '@formily/react'
 import { Input as BaseUIInput } from 'baseui/input'
 
 export interface IInputProps extends Omit<InputProps, 'onChange'> {
   onChange?: (value: string) => void
 }
 
-export default function Input({ onChange, ...restProps }: IInputProps) {
+export function Input({ onChange, ...restProps }: IInputProps) {
   return (
     <BaseUIInput onChange={e => onChange?.(e.target.value)} {...restProps} />
   )
 }
+
+export default connect(Input)
--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { JSONSchemaProvider } from './components/JSONSchema.tsx'
+import type { IAPISchema } from './types'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function mount(schema: IAPISchema) {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <JSONSchemaProvider value={schema}>
+        <App />
+      </JSONSchemaProvider>
+    </React.StrictMode>,
+  )
+}
+
+fetch('/schema.json')
+  .then(res => res.json())
+  .then(mount)
+  .catch((err) => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <div>
+          Error:
+          {err.message}
+        </div>
+      </React.StrictMode>,
+    )
+  })

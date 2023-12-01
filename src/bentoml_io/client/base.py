2e28a76d074359a988468e2aea810d441162825e
Frost Ming <me@frostming.com>
2023-12-01 15:52:04 +0800

    feat: Python code generation
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/bentoml_io/client/base.py
+++ src/bentoml_io/client/base.py
@@ -8,7 +8,7 @@ T = t.TypeVar("T")
 
 class AbstractClient(abc.ABC):
     @abc.abstractmethod
-    def call(self, name: str, *args: t.Any, **kwargs: t.Any) -> t.Any:
+    def call(self, __name: str, /, *args: t.Any, **kwargs: t.Any) -> t.Any:
         """Call a service method by its name.
         It takes the same arguments as the service method.
         """
--- webui/src/components/code/CURL.tsx
+++ webui/src/components/code/CURL.tsx
@@ -2,25 +2,7 @@ import isEmpty from 'lodash/isEmpty'
 import { convert, hasFileInSchema } from '../../hooks/useQuery'
 import type { TObject } from '../../types'
 import Code from './Base'
-
-interface ICURLCodeProps {
-  values: object
-  schema?: TObject
-  path?: string
-}
-
-/**
- * Formats a JSON object into a string with custom indentation.
- * @param json - The JSON object to be formatted.
- * @param indent - The number of spaces used for indentation. Defaults to 4.
- * @returns A string representation of the JSON object with custom indentation.
- */
-function formatJSON(json: object, indent = 4) {
-  return JSON.stringify(json, null, indent)
-    .split('\n')
-    .map((line, index) => (index === 0 ? '' : ' '.repeat(indent)) + line)
-    .join('\n')
-}
+import { formatJSON, IClientProps } from './Base'
 
 /**
  * Formats a file or an array of files into a string representation.
@@ -31,17 +13,16 @@ function formatJSON(json: object, indent = 4) {
 function formatFiles(files: { [field: string]: File | File[] }, indent = 4) {
   if (isEmpty(files))
     return ''
-  const context = Object.entries(files)
+  const content = Object.entries(files)
     // if the entry is an array of files, format each file in the array.
     // otherwise, format the single file.
     .flatMap(([key, value]) => Array.isArray(value)
       ? value.map(item => `-F '${key}=@${item.name}'`)
       : [`-F '${key}=@${value.name}'`],
     )
-    .map(line => `${' '.repeat(indent)}${line}`)
-    .join('\n')
+    .join(` \\\n${' '.repeat(indent)}`)
 
-  return `\n${context}`
+  return content
 }
 
 function generateCode(data: object, path = '/', schema?: TObject) {
@@ -50,8 +31,8 @@ function generateCode(data: object, path = '/', schema?: TObject) {
     const { nonFileFields, fileFields } = convert(data)
 
     return `$ curl -s -X POST \\
-    -H "Content-Type: multipart/form-data" \\
-    -F '__data=${formatJSON(nonFileFields, 4)}' \\ ${formatFiles(fileFields, 4)}
+    -F '__data=${formatJSON(nonFileFields, 4)}' \\
+    ${formatFiles(fileFields, 4)} \\
     http://localhost:3000${path}`
   }
   else {
@@ -62,11 +43,11 @@ function generateCode(data: object, path = '/', schema?: TObject) {
   }
 }
 
-function CURL({ path, values, schema }: ICURLCodeProps) {
+function CURL({ path, values, schema }: IClientProps) {
   return (
     // values is proxy object, so it must rendered every time
     // otherwise it will fail to update
-    <Code>{generateCode(values, path, schema)}</Code>
+    <Code language='bash'>{generateCode(values, path, schema)}</Code>
   )
 }
 

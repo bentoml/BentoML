23fb1e9998297594c535569403155d8a1df8e179
bojiang <bojiang_@outlook.com>
2023-12-05 10:09:24 +0800

    fix: add __main__ to quickstart

--- examples/quickstart/service.py
+++ examples/quickstart/service.py
@@ -2,7 +2,7 @@ import bentoml_io as bentoml
 import numpy as np
 
 
-@bentoml.service(resources={"num_cpus": 1})
+@bentoml.service(resources={"num_cpus": 1, "gpu_type": "T4"}, traffic={"timeout": 10})
 class IrisClassifier:
     iris_model = bentoml.models.get("iris_clf:latest")
 
@@ -14,3 +14,7 @@ class IrisClassifier:
     @bentoml.api
     def classify(self, input_series: np.ndarray) -> np.ndarray:
         return self.model.predict(input_series)
+
+
+if __name__ == "__main__":
+    IrisClassifier.serve_http()

36d45280081b65de035f103094d1dcd46648c36c
Jinyang Liu <liujinyang@bigtiger.me>
2023-12-06 07:01:54 +0800

    feat: add preview feature for output

--- webui/src/components/InferenceForm.tsx
+++ webui/src/components/InferenceForm.tsx
@@ -1,20 +1,19 @@
-import type { FC, Key } from 'react'
-import { createElement, useMemo, useState } from 'react'
-import { useStyletron } from 'baseui'
+import type { Key, ReactElement } from 'react'
+import { createElement, useEffect, useMemo, useState } from 'react'
 import { Select } from 'baseui/select'
-import { FormControl } from 'baseui/form-control'
 import { createForm } from '@formily/core'
 import { FormConsumer } from '@formily/react'
 import { FlexGrid, FlexGridItem } from 'baseui/flex-grid'
-import { Tab, Tabs } from 'baseui/tabs'
 import useCurrentPath from '../hooks/useCurrentPath'
 import { transformData, useFormSubmit, useSchema } from '../hooks/useQuery'
+import { Tab, Tabs } from './Tabs'
 import Form from './form/Form'
 import FormField, { generateFormSchema } from './form/FormField'
 import Submit from './form/Submit'
 import type { IClientProps } from './code/Base'
 import CURL from './code/CURL'
 import Python from './code/Python'
+import Output from './Output'
 
 const codeExamples = {
   Python,
@@ -22,30 +21,31 @@ const codeExamples = {
 }
 
 export default function InferenceForm() {
-  const [css, theme] = useStyletron()
   const data = useSchema()
   const { currentRoute, setCurrentPath } = useCurrentPath()
   const [activeTab, setActiveTab] = useState<Key>('0')
-  const [result, setResult] = useState<object | string>()
-  const [error, setError] = useState<string | undefined>()
+  const [result, setResult] = useState<any>()
+  const [error, setError] = useState<Error>()
   const form = useMemo(() => createForm({}), [currentRoute])
-  const submit = useFormSubmit(form, currentRoute?.input)
+  const submit = useFormSubmit(form, currentRoute)
   const formSchema = generateFormSchema(currentRoute?.input)
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    if (!currentRoute)
-      return
-    const resp = await submit(currentRoute.route)
-    if (resp.status >= 400) {
-      const e = await resp.text()
-      setError(`Error: ${resp.status} ${e}`)
+    try {
+      const res = await submit()
+
+      setResult(res)
     }
-    else {
-      setError(undefined)
-      setResult(await resp.json())
+    catch (err) {
+      setError(err as Error)
     }
   }
+
+  useEffect(() => {
+    setResult(undefined)
+    setError(undefined)
+  }, [currentRoute, setResult, setError])
 
   return (
     <>
@@ -76,59 +76,51 @@ export default function InferenceForm() {
               <Tabs
                 activeKey={activeTab}
                 onChange={({ activeKey }) => setActiveTab(activeKey)}
-                overrides={{
-                  TabBar: {
-                    props: {
-                      className: css({
-                        backgroundColor: 'transparent!important',
-                        paddingLeft: '0!important',
-                        paddingRight: '0!important',
-                        borderBottomWidth: '1px',
-                        borderBottomStyle: 'solid',
-                        borderBottomColor: `${theme.colors.borderOpaque}`,
-                      }),
-                    },
-                  },
-                  TabContent: {
-                    props: {
-                      className: css({
-                        paddingLeft: '0!important',
-                        paddingRight: '0!important',
-                      }),
-                    },
-                  },
-                }}
               >
                 <Tab title="Form">
                   <FormField schema={formSchema} />
                   <Submit>Submit</Submit>
                 </Tab>
-                <>
-                  {
-                    Object.entries(codeExamples).map(([title, comp]) => (
-                      <Tab title={title} key={title}>
-                        <FormConsumer>
-                          {() => createElement(comp as FC<IClientProps>, {
-                            path: currentRoute.route,
-                            values: transformData(form.values, currentRoute.input),
-                            schema: currentRoute.input,
-                          })}
-                        </FormConsumer>
-                      </Tab>
-                    ))
-                  }
-                </>
+                {
+                  Object.entries(codeExamples).map(([title, comp]) => (
+                    <Tab title={title} key={title}>
+                      <FormConsumer>
+                        {() => createElement<IClientProps>(comp, {
+                          path: currentRoute.route,
+                          values: transformData(form.values, currentRoute.input),
+                          schema: currentRoute.input,
+                        })}
+                      </FormConsumer>
+                    </Tab> as ReactElement<any, any>
+                  ))
+                }
               </Tabs>
             </Form>
           </FlexGridItem>
           <FlexGridItem>
-            <FormControl label="Result" error={error}>
-              <div>{JSON.stringify(result)}</div>
-            </FormControl>
+            <Tabs activeKey="0">
+              <Tab title="Result">
+                {error
+                  ? (
+                    <div style={{ color: 'red' }}>
+                      Error:
+                      {' '}
+                      {error.message}
+                    </div>
+                    )
+                  : result
+                    ? (
+                      <Output
+                        result={result}
+                        schema={currentRoute.output}
+                      />
+                      )
+                    : null}
+              </Tab>
+            </Tabs>
           </FlexGridItem>
         </FlexGrid>
       )}
-      
     </>
   )
 }
---
+++ webui/src/components/Output.tsx
@@ -0,0 +1,65 @@
+import { useMemo } from 'react'
+import { FormControl } from 'baseui/form-control'
+import type { DataType } from '../types'
+import { hasFileInSchema } from '../hooks/useQuery'
+import BaseCode from './code/Base'
+import BasePreview, { Download as BaseDownload } from './preview/Base'
+import ImagePreview, { Download as ImageDownload } from './preview/Image'
+import AudioPlayer from './preview/Audio'
+import VideoPlayer from './preview/Video'
+
+interface IOutputProps {
+  result: any
+  schema: DataType
+}
+
+function Output({ result, schema }: IOutputProps) {
+  const hasFiles = useMemo(() => hasFileInSchema({ schema }), [schema])
+
+  if (!hasFiles) {
+    return (
+      <FormControl label={schema.title}>
+        <BaseCode language="json">
+          {JSON.stringify(result, null, 2)}
+        </BaseCode>
+      </FormControl>
+    )
+  }
+  else if (schema.type === 'file') {
+    switch (schema.format) {
+      case 'audio':
+        return (
+          <AudioPlayer files={[result]}>
+            {file => <BaseDownload value={file} />}
+          </AudioPlayer>
+        )
+      case 'video':
+        return (
+          <VideoPlayer files={[result]}>
+            {file => <BaseDownload value={file} />}
+          </VideoPlayer>
+        )
+      case 'image':
+        return (
+          <ImagePreview value={result}>
+            <ImageDownload value={result} />
+          </ImagePreview>
+        )
+      default:
+        return (
+          <BasePreview value={result}>
+            <BaseDownload value={result} />
+          </BasePreview>
+        )
+    }
+  }
+  else {
+    return (
+      <div style={{ color: 'red' }}>
+        Error: Preview not supported for the output type
+      </div>
+    )
+  }
+}
+
+export default Output

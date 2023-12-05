5ea0c8c378bc44d8c5d0244a0ea0ac5ee0a2e085
bojiang <bojiang_@outlook.com>
2023-12-05 11:12:50 +0800

    revert: __main__ in quickstart

--- examples/quickstart/service.py
+++ examples/quickstart/service.py
@@ -2,7 +2,7 @@ import bentoml_io as bentoml
 import numpy as np
 
 
-@bentoml.service(resources={"num_cpus": 1, "gpu_type": "T4"}, traffic={"timeout": 10})
+@bentoml.service(resources={"memory": "512MiB"})
 class IrisClassifier:
     iris_model = bentoml.models.get("iris_clf:latest")
 
@@ -14,7 +14,3 @@ class IrisClassifier:
     @bentoml.api
     def classify(self, input_series: np.ndarray) -> np.ndarray:
         return self.model.predict(input_series)
-
-
-if __name__ == "__main__":
-    IrisClassifier.serve_http()

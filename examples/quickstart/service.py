7f2367feb3eb120bbd30e2ceedaa41cd81e39474
Tianxin Dong <fog@bentoml.com>
2023-12-08 11:48:21 +0800

    fix: example and bento config (#4324)
    fix: fix example and bento config
    
    Signed-off-by: FogDong <fog@bentoml.com>

--- examples/quickstart/service.py
+++ examples/quickstart/service.py
@@ -2,14 +2,14 @@ import bentoml_io as bentoml
 import numpy as np
 
 
-@bentoml.service(resources={"memory": "512MiB"})
+@bentoml.service(resources={"cpu": "200m", "memory": "512Mi"})
 class Preprocessing:
     @bentoml.api
     def preprocess(self, input_series: np.ndarray) -> np.ndarray:
         return input_series
 
 
-@bentoml.service(resources={"memory": "512MiB"})
+@bentoml.service(resources={"cpu": "200m", "memory": "512Mi"})
 class IrisClassifier:
     iris_model = bentoml.models.get("iris_clf:latest")
     preprocessing = bentoml.depends(Preprocessing)

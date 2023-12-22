6e0bb46cb3f2e6f81daa5bc9e8c92e84fffda4db
Frost Ming <me@frostming.com>
2023-12-22 10:16:27 +0800

    feat(ui): header and theme options
    Signed-off-by: Frost Ming <me@frostming.com>

--- webui/src/index.tsx
+++ webui/src/index.tsx
@@ -4,16 +4,23 @@ import App from './App'
 import { JSONSchemaProvider } from './components/JSONSchema'
 import type { IAPISchema } from './types'
 import type { IMountOptions } from './hooks/useMountOptions'
-import { OptionProvider } from './hooks/useMountOptions'
+import { OptionProvider, defaultOptions } from './hooks/useMountOptions'
 
-export function mount(schema: IAPISchema, element: HTMLElement, options?: IMountOptions) {
+export function BentoMLUI(props: { schema: IAPISchema, options?: Partial<IMountOptions> }) {
+  const mergedOptions = { ...defaultOptions, ...(props.options ?? {}) }
+  return (
+    <OptionProvider value={mergedOptions}>
+      <JSONSchemaProvider value={props.schema}>
+        <App />
+      </JSONSchemaProvider>
+    </OptionProvider>
+  )
+}
+
+export function mount(schema: IAPISchema, element: HTMLElement, options?: Partial<IMountOptions>) {
   ReactDOM.createRoot(element).render(
     <React.StrictMode>
-      <OptionProvider value={options ?? {}}>
-        <JSONSchemaProvider value={schema}>
-          <App />
-        </JSONSchemaProvider>
-      </OptionProvider>
+      <BentoMLUI schema={schema} options={options} />
     </React.StrictMode>,
   )
 }

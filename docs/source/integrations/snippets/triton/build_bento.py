2ea0ddee02264a4501d289c8b2d82af41e877a0a
Sherlock Xu <65327072+Sherlock113@users.noreply.github.com>
2024-01-17 10:30:57 +0800

    docs: Clean 1.2 docs (#4407)
    * Clean 1.2 docs
    
    Signed-off-by: Sherlock113 <sherlockxu07@gmail.com>
    
    * ci: auto fixes from pre-commit.ci
    
    For more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Sherlock113 <sherlockxu07@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/source/integrations/snippets/triton/build_bento.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    import bentoml
-
-    bentoml.bentos.build(
-        "service:svc",
-        include=["/model_repository", "/data/*.png", "service.py"],
-        exclude=["/__pycache__", "/venv"],
-        docker={"base_image": "nvcr.io/nvidia/tritonserver:22.12-py3"},
-    )

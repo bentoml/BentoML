dddb6e6f69dead060721e43e1f753d792aa61f1c
bojiang <bojiang_@outlook.com>
2024-02-19 14:08:18 +0800

    chore(cloud cli): rename cluster to region

--- src/bentoml_cli/deployment.py
+++ src/bentoml_cli/deployment.py
@@ -625,7 +625,7 @@ def add_deployment_command(cli: click.Group) -> None:
             table.add_column("created_at")
             table.add_column("Bento")
             table.add_column("Status")
-            table.add_column("Cluster")
+            table.add_column("Region")
             for info in d_list:
                 table.add_row(
                     info.name,

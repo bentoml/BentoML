ab6e5a70deae53e2d42cebf10cf0bdae66efe1ac
Alex Liu <83865371+lycheel1@users.noreply.github.com>
2024-04-08 13:48:13 +0000

    docs: added multi model load and store example (#4575)
    * doc: added multi model load and store example
    
    * ci: auto fixes from pre-commit.ci
    
    For more information, see https://pre-commit.ci
    
    * docs: update service.py for model loading and storing example
    
    * docs: update for model load and store example
    
    * docs: fix code formatting for model load and store example
    
    * ci: auto fixes from pre-commit.ci
    
    For more information, see https://pre-commit.ci
    
    * docs: added gpu recommmendation
    
    * docs: minor fix on formatting for model loading and storing example
    
    * docs: improved readme for model load and store example
    
    * ci: auto fixes from pre-commit.ci
    
    For more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ examples/model_load_store_lora/import_model.py
@@ -0,0 +1,18 @@
+import shutil
+
+from diffusers import StableDiffusionPipeline
+
+import bentoml
+
+model_id = "CompVis/stable-diffusion-v1-4"
+pipeline = StableDiffusionPipeline.from_pretrained(model_id, use_safetensors=True)
+
+local_lora = "./lora"
+with bentoml.models.create(
+    name="sd",
+) as model_ref:
+    local_diffusion_path = model_ref.path_of("diffusion")
+    local_lora_path = model_ref.path_of("lora")
+    pipeline.save_pretrained(local_diffusion_path)
+    shutil.copytree(local_lora, local_lora_path, dirs_exist_ok=True)
+    print(f"Model saved: {model_ref}")

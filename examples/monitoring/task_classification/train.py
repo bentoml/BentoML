528d78c9c97dbcbc704b4ed903b234c3b53a65e8
Zhao Shenyang <dev@zsy.im>
2024-04-29 17:24:24 +0800

    add back old monitoring example for now

---
+++ examples/monitoring/task_classification/train.py
@@ -0,0 +1,21 @@
+import logging
+
+from sklearn import datasets
+from sklearn import svm
+
+import bentoml
+
+logging.basicConfig(level=logging.WARN)
+
+if __name__ == "__main__":
+    # Load training data
+    iris = datasets.load_iris()
+    X, y = iris.data, iris.target
+
+    # Model Training
+    clf = svm.SVC()
+    clf.fit(X, y)
+
+    # Save model to BentoML local model store
+    saved_model = bentoml.sklearn.save_model("iris_clf", clf)
+    print(f"Model saved: {saved_model}")

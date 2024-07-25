44db8aea95deae0715ac5443fd20858bbf308bf4
jsamanta_ucd <jaydeep.samanta@ucd.ie>
2024-07-25 15:54:36 +0100

    update service.py

--- examples/custom_runner/arima_model/service.py
+++ examples/custom_runner/arima_model/service.py
@@ -4,6 +4,8 @@ import bentoml
 from bentoml.io import JSON
 from bentoml.io import NumpyNdarray
 
+arima_model = bentoml.picklable_model.get("arima_forecast_model:latest")
+
 # Custom runner for ARIMA model
 class ARIMAForecastRunnable(bentoml.Runnable):
     SUPPORTED_RESOURCES = ("cpu",)
@@ -11,23 +13,22 @@ class ARIMAForecastRunnable(bentoml.Runnable):
 
     def __init__(self):
         super().__init__()
-        self.model = bentoml.picklable_model.get("arima_forecast_model:latest")
+        self.model = arima_model
     
     @bentoml.Runnable.method(batchable=False)
-    def forecast(self, values):
+    def forecast(self, values_to_forecast):
         # Load trained arima model from bentoml
         arima_model = self.model.load_model()
         model_fit = arima_model.fit()
-        y_pred = model_fit.forecast(int(values))
-        return y_pred
+        predictions = model_fit.forecast(int(values_to_forecast))
+        return predictions
 
 
 arima_forecast_runner = bentoml.Runner(ARIMAForecastRunnable)
 
-svc = bentoml.Service("arima_model_forecast", runners=[arima_forecast_runner])
+svc = bentoml.Service("arima_model_forecast", runners=[arima_forecast_runner], models=[arima_model])
 
 @svc.api(input=NumpyNdarray(dtype="int"), output=JSON())
 def predict_forecast(input_data: np.ndarray):
-    print(input_data)
     return arima_forecast_runner.forecast.run(input_data[0])
 

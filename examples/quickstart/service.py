aad1671bfdf961e81644a155f4a29c51a8b0b8d2
Chaoyu <chaoyu@bentoml.com>
2024-08-07 15:52:53 -0700

    Update service.py

--- examples/quickstart/service.py
+++ examples/quickstart/service.py
@@ -1,29 +1,20 @@
 from __future__ import annotations
 
-from transformers import pipeline
-
 import bentoml
-
-EXAMPLE_INPUT = "Breaking News: In an astonishing turn of events, the small \
-town of Willow Creek has been taken by storm as local resident Jerry Thompson's cat, \
-Whiskers, performed what witnesses are calling a 'miraculous and gravity-defying leap.' \
-Eyewitnesses report that Whiskers, an otherwise unremarkable tabby cat, jumped \
-a record-breaking 20 feet into the air to catch a fly. The event, which took \
-place in Thompson's backyard, is now being investigated by scientists for potential \
-breaches in the laws of physics. Local authorities are considering a town festival \
-to celebrate what is being hailed as 'The Leap of the Century."
-
+from typing import List
 
 @bentoml.service(
-    resources={"cpu": "2"},
-    traffic={"timeout": 10},
+    resources={"cpu": "4"}
 )
 class Summarization:
     def __init__(self) -> None:
-        # Load model into pipeline
-        self.pipeline = pipeline("summarization")
+        import torch
+        from transformers import pipeline
 
-    @bentoml.api
-    def summarize(self, text: str = EXAMPLE_INPUT) -> str:
-        result = self.pipeline(text)
-        return result[0]["summary_text"]
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.pipeline = pipeline('summarization', device=device)
+
+    @bentoml.api(batchable=True)
+    def summarize(self, texts: List[str]) -> List[str]:
+        results = self.pipeline(texts)
+        return [item['summary_text'] for item in results]

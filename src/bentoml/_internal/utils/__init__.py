7aa96eaa364798ebefd178a5d7edee0a86cfbe3c
Frost Ming <me@frostming.com>
2025-02-26 16:31:59 +0800

    fix: override
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/bentoml/_internal/utils/__init__.py
+++ src/bentoml/_internal/utils/__init__.py
@@ -525,13 +525,13 @@ def is_jupyter() -> bool:  # pragma: no cover
         return False  # Other type (?)
 
 
-def deep_merge(
-    base: dict[t.Any, t.Any], update: dict[t.Any, t.Any]
-) -> dict[t.Any, t.Any]:
+def deep_merge(base: t.Any, update: t.Any) -> dict[t.Any, t.Any]:
     """Merge two dictionaries recursively in place. The base dict will be updated."""
+    if not isinstance(base, dict) or not isinstance(update, dict):
+        return update
     for k, v in update.items():
         if k in base and isinstance(base[k], dict) and isinstance(v, dict):
-            deep_merge(base[k], v)
+            base[k] = deep_merge(base[k], v)
         else:
             base[k] = v
     return base

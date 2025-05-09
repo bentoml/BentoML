debd40ffb70274912fa88f73c8483b9c2f63a9bc
Frost Ming <me@frostming.com>
2025-05-09 09:37:27 +0800

    chore: minor fix, wrong message (#5352)
    * chore: minor fix, wrong message
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * fix: remove unused code
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    ---------
    
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/_bentoml_impl/docker.py
+++ src/_bentoml_impl/docker.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import importlib
-import os
 import shlex
 import typing as t
 
@@ -13,7 +12,6 @@ from jinja2 import FileSystemLoader
 from bentoml._internal.bento.bento import ImageInfo
 from bentoml._internal.container.generate import DEFAULT_BENTO_ENVS
 from bentoml._internal.container.generate import expands_bento_path
-from bentoml._internal.container.generate import resolve_package_versions
 from bentoml._internal.container.generate import to_bento_field
 from bentoml._internal.container.generate import to_options_field
 
@@ -25,11 +23,6 @@ def get_templates_variables(
 
     bento_envs = {**DEFAULT_BENTO_ENVS, **bento_envs}
     options = attrs.asdict(image)
-    requirement_file = bento_fs.getsyspath("env/python/requirements.txt")
-    if os.path.exists(requirement_file):
-        python_packages = resolve_package_versions(requirement_file)
-    else:
-        python_packages = {}
     return {
         **{to_options_field(k): v for k, v in options.items()},
         **{to_bento_field(k): v for k, v in bento_envs.items()},

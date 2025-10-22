189058fa6ea46062379de365bfcbf9a7c267ca53
Frost Ming <me@frostming.com>
2025-10-22 13:31:05 +0800

    fix: update cuda base image and base image docs
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/bentoml/_internal/container/frontend/dockerfile/__init__.py
+++ src/bentoml/_internal/container/frontend/dockerfile/__init__.py
@@ -98,7 +98,7 @@ CONTAINER_METADATA: dict[str, dict[str, t.Any]] = {
             "supported_architectures": SUPPORTED_ARCHITECTURES,
         },
         "cuda": {
-            "image": "nvidia/cuda:{spec_version}-cudnn8-runtime-ubuntu20.04",
+            "image": "nvidia/cuda:{spec_version}-cudnn-runtime-ubuntu24.04",
             "supported_architectures": ["amd64", "arm64"],
         },
         "miniconda": {

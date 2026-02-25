3263ffda11cb6af7009c45c77392e4f4d51ecfb5
Frost Ming <mianghong@gmail.com>
2026-02-25 08:34:01 +0800

    Revert "fix: resolve AnyIO NoEventLoopError when calling sync API from async …"
    This reverts commit 97e246fc2bdb4cfbf4eb2d2aaf80e430e8e3b66f.

--- src/_bentoml_impl/client/proxy2.py
+++ src/_bentoml_impl/client/proxy2.py
@@ -18,7 +18,6 @@ from urllib.parse import urlparse
 
 import aiohttp
 import anyio.from_thread
-import anyio.lowlevel
 import attr
 from multidict import CIMultiDict
 
@@ -626,15 +625,7 @@ class SyncClient(AbstractClient):
     def run_async(
         callable: t.Callable[P, t.Awaitable[T]], *args: P.args, **kwargs: P.kwargs
     ) -> T:
-        try:
-            # Try to get the current AnyIO token if we're in an AnyIO context
-            token = anyio.lowlevel.current_token()
-            return anyio.from_thread.run(
-                functools.partial(callable, *args, **kwargs), token=token
-            )
-        except LookupError:
-            # No AnyIO context found, use standard from_thread.run
-            return anyio.from_thread.run(functools.partial(callable, *args, **kwargs))
+        return anyio.from_thread.run(functools.partial(callable, *args, **kwargs))
 
     def call(self, __name: str, /, *args: t.Any, **kwargs: t.Any) -> t.Any:
         coro = self._async_client.call(__name, *args, **kwargs)

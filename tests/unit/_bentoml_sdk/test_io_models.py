1dbb554bef82823f5e1c7808e4c4e8311ad92b97
hasnaat <hasnaat.hussain.2@gmail.com>
2026-06-28 14:10:01 +0500

    fix(sdk): check generic args length on bare iterator return annotations to avoid IndexError
    Signed-off-by: Hasnaat Hussain <hasnaat.hussain.2@gmail.com>

---
+++ tests/unit/_bentoml_sdk/test_io_models.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import typing as t
+from _bentoml_sdk.io_models import IODescriptor
+
+def test_from_output_with_bare_iterator():
+    def bare_iterator_fn() -> t.Iterator:
+        yield 1
+
+    descriptor = IODescriptor.from_output(bare_iterator_fn)
+    assert descriptor is not None
+
+
+def test_from_output_with_bare_generator():
+    def bare_generator_fn() -> t.Generator:
+        yield 1
+
+    descriptor = IODescriptor.from_output(bare_generator_fn)
+    assert descriptor is not None
